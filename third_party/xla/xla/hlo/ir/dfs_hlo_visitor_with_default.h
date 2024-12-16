0773356df64a819d5bcadb2f54548902087fc618
Eugene Zhulenev <ezhulenev@google.com>
2024-12-16 11:25:25 -0800

    [xla] Add LiteralPool and LiteralCanonicalizer to share constant literals between HLO modules
    This change saves a lot of host memory from duplicate constant literals in instantiated HLO modules.
    
    PiperOrigin-RevId: 706775155

--- third_party/xla/xla/hlo/ir/dfs_hlo_visitor_with_default.h
+++ third_party/xla/xla/hlo/ir/dfs_hlo_visitor_with_default.h
@@ -380,6 +380,7 @@ class DfsHloRewriteVisitor : public DfsHloVisitorWithDefault {
 
   // Mark the computation as having changed.
   void MarkAsChanged() { changed_ = true; }
+  void MarkAsChanged(bool changed) { changed_ |= changed; }
 
  private:
   bool changed_ = false;

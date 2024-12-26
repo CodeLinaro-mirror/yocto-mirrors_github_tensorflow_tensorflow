fc0e5f95b2ba20ab0bb8fe2e7a0623c66cbe734c
Eugene Zhulenev <ezhulenev@google.com>
2024-12-26 13:12:11 -0800

    [xla:cpu] Use optimize memcpy function in SortThunk
    We know all possible sizes of copied elements and by using explicit switch and compile time constants we can inline memcpy implementation optimized for small sizes.
    ```
    name                                                                               old cpu/op   new cpu/op   delta
    BM_Sort1D/input_size:1000/num_inputs:1/is_stable:0/sort_ascending:1/process_time   11.4µs ± 2%  11.4µs ± 2%  -0.52%  (p=0.000 n=77+74)
    BM_Sort1D/input_size:1000/num_inputs:2/is_stable:0/sort_ascending:1/process_time    114µs ± 2%   105µs ± 2%  -7.91%  (p=0.000 n=76+76)
    BM_Sort1D/input_size:1000/num_inputs:8/is_stable:0/sort_ascending:1/process_time    284µs ± 2%   256µs ± 2%  -9.71%  (p=0.000 n=73+73)
    BM_Sort1D/input_size:1000/num_inputs:16/is_stable:0/sort_ascending:1/process_time   553µs ± 2%   565µs ± 2%  +2.08%  (p=0.000 n=76+74)
    BM_Sort1D/input_size:1000/num_inputs:32/is_stable:0/sort_ascending:1/process_time  3.28ms ± 2%  2.99ms ± 2%  -8.70%  (p=0.000 n=75+74)
    BM_Sort1D/input_size:1000/num_inputs:1/is_stable:0/sort_ascending:0/process_time   88.6µs ± 2%  84.1µs ± 2%  -5.02%  (p=0.000 n=80+72)
    BM_Sort1D/input_size:1000/num_inputs:2/is_stable:0/sort_ascending:0/process_time    114µs ± 2%   105µs ± 2%  -7.95%  (p=0.000 n=75+75)
    BM_Sort1D/input_size:1000/num_inputs:8/is_stable:0/sort_ascending:0/process_time    285µs ± 3%   257µs ± 2%  -9.84%  (p=0.000 n=74+75)
    BM_Sort1D/input_size:1000/num_inputs:16/is_stable:0/sort_ascending:0/process_time   554µs ± 2%   565µs ± 2%  +2.00%  (p=0.000 n=76+77)
    BM_Sort1D/input_size:1000/num_inputs:32/is_stable:0/sort_ascending:0/process_time  3.28ms ± 2%  2.99ms ± 2%  -8.75%  (p=0.000 n=75+77)
    ```
    
    PiperOrigin-RevId: 709866789

--- third_party/xla/xla/backends/cpu/runtime/while_thunk_test.cc
+++ third_party/xla/xla/backends/cpu/runtime/while_thunk_test.cc
@@ -26,15 +26,15 @@ limitations under the License.
 #include "xla/backends/cpu/runtime/resource_use.h"
 #include "xla/backends/cpu/runtime/thunk.h"
 #include "xla/backends/cpu/runtime/thunk_testlib.h"
+#include "xla/literal_util.h"
 #include "xla/runtime/buffer_use.h"
 #include "xla/service/buffer_assignment.h"
-#include "xla/service/maybe_owning_device_memory.h"
 #include "xla/stream_executor/device_memory.h"
 #include "xla/tsl/concurrency/async_value_ref.h"
-#include "tsl/platform/env.h"
-#include "tsl/platform/statusor.h"
-#include "tsl/platform/test.h"
-#include "tsl/platform/threadpool.h"
+#include "xla/tsl/platform/env.h"
+#include "xla/tsl/platform/statusor.h"
+#include "xla/tsl/platform/test.h"
+#include "xla/tsl/platform/threadpool.h"
 
 #define EIGEN_USE_THREADS
 
@@ -161,20 +161,14 @@ class BodyThunk : public Thunk {
 TEST(WhileThunkTest, NonBlockingExecute) {
   static constexpr size_t kNumIterations = 100;
 
-  BufferAllocation pred_alloc(0, sizeof(char), 0);
-  BufferAllocation cnt_alloc(1, sizeof(int32_t), 0);
+  auto pred = LiteralUtil::CreateR0<uint8_t>(false);
+  auto cnt = LiteralUtil::CreateR0<int32_t>(0);
 
-  BufferAllocation::Slice pred_slice(&pred_alloc, 0, sizeof(char));
-  BufferAllocation::Slice cnt_slice(&cnt_alloc, 0, sizeof(int32_t));
+  BufferAllocations allocations = CreateBufferAllocations(pred, cnt);
 
-  std::vector<MaybeOwningDeviceMemory> buffers;
-  std::vector<char> predicate = {false};
-  std::vector<int32_t> counter = {0};
-
-  buffers.emplace_back(se::DeviceMemoryBase(predicate.data(), sizeof(char)));
-  buffers.emplace_back(se::DeviceMemoryBase(counter.data(), sizeof(int32_t)));
-
-  BufferAllocations allocations(buffers);
+  auto [pred_alloc, cnt_alloc] = CreateBufferAllocation(pred, cnt);
+  auto [pred_slice, cnt_slice] =
+      CreateBufferAllocationSlice(pred_alloc, cnt_alloc);
 
   ThunkSequence cond_sequence;
   cond_sequence.push_back(
@@ -200,26 +194,20 @@ TEST(WhileThunkTest, NonBlockingExecute) {
   tsl::BlockUntilReady(execute_event);
   ASSERT_FALSE(execute_event.IsError());
 
-  EXPECT_EQ(counter[0], kNumIterations);
+  EXPECT_EQ(cnt, LiteralUtil::CreateR0<int32_t>(kNumIterations));
 }
 
 TEST(WhileThunkTest, NonBlockingExecuteWithTripCount) {
   static constexpr size_t kNumIterations = 100;
 
-  BufferAllocation pred_alloc(0, sizeof(char), 0);
-  BufferAllocation cnt_alloc(1, sizeof(int32_t), 0);
+  auto pred = LiteralUtil::CreateR0<uint8_t>(false);
+  auto cnt = LiteralUtil::CreateR0<int32_t>(0);
 
-  BufferAllocation::Slice pred_slice(&pred_alloc, 0, sizeof(char));
-  BufferAllocation::Slice cnt_slice(&cnt_alloc, 0, sizeof(int32_t));
+  BufferAllocations allocations = CreateBufferAllocations(pred, cnt);
 
-  std::vector<MaybeOwningDeviceMemory> buffers;
-  std::vector<char> predicate = {false};
-  std::vector<int32_t> counter = {0};
-
-  buffers.emplace_back(se::DeviceMemoryBase(predicate.data(), sizeof(char)));
-  buffers.emplace_back(se::DeviceMemoryBase(counter.data(), sizeof(int32_t)));
-
-  BufferAllocations allocations(buffers);
+  auto [pred_alloc, cnt_alloc] = CreateBufferAllocation(pred, cnt);
+  auto [pred_slice, cnt_slice] =
+      CreateBufferAllocationSlice(pred_alloc, cnt_alloc);
 
   // We pass empty cond sequence, because we know the trip count, and check that
   // predicate value is ignored (it is initialized to false) and body executed
@@ -246,7 +234,7 @@ TEST(WhileThunkTest, NonBlockingExecuteWithTripCount) {
   tsl::BlockUntilReady(execute_event);
   ASSERT_FALSE(execute_event.IsError());
 
-  EXPECT_EQ(counter[0], kNumIterations);
+  EXPECT_EQ(cnt, LiteralUtil::CreateR0<int32_t>(kNumIterations));
 }
 
 }  // namespace

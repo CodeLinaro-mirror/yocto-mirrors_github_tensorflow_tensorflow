hexagon-geo-surv/tensorflow

Remove `RunAndCompare` functionality from `HloRunnerAgnosticTestBase`.
This functionality is now fully contained in `HloRunnerAgnosticReferenceMixin`
and therefore is no longer needed in `HloRunnerAgnosticTestBase`.

This change temporarily adds the mixin to `HloPjRtTestBase`. Next, we'll go
through all tests that extend these base classes and will move the uses of the
mixins to the leaves.

PiperOrigin-RevId: 712982674

third_party/xla/xla/tests/hlo_pjrt_test_base.cc
@@ -21,14 +21,12 @@ limitations under the License.
 
 #include "absl/log/check.h"
 #include "absl/status/statusor.h"
-#include "xla/pjrt/interpreter/interpreter_client.h"
 #include "xla/pjrt/pjrt_client.h"
 #include "xla/service/hlo_runner_interface.h"
 #include "xla/service/hlo_runner_pjrt.h"
+#include "xla/tests/hlo_pjrt_reference_mixin.h"
 #include "xla/tests/hlo_runner_agnostic_test_base.h"
 #include "xla/tests/pjrt_client_registry.h"
-#include "xla/util.h"
-#include "tsl/platform/logging.h"
 
 namespace xla {
 namespace {
@@ -52,21 +50,12 @@ std::unique_ptr<HloRunnerInterface> GetHloRunnerForTest() {
       *std::move(client), device_shape_representation_fn, device_shape_size_fn);
 }
 
-std::unique_ptr<HloRunnerInterface> GetHloRunnerForReference() {
-  return std::make_unique<HloRunnerPjRt>(
-      std::make_unique<InterpreterClient>(),
-      InterpreterClient::DeviceShapeRepresentation,
-      InterpreterClient::ShapeSizeBytes,
-      /*use_parameter_layout_on_device=*/true);
-}
-
 }  // namespace
 
 HloPjRtTestBase::HloPjRtTestBase(HloPjRtTestBaseOptions options)
-    : HloRunnerAgnosticTestBase(GetHloRunnerForTest(),
-                                GetHloRunnerForReference(),
-                                options.verifier_layout_sensitive,
-                                options.allow_mixed_precision_in_hlo_verifier,
-                                options.instruction_can_change_layout_func) {}
+    : HloPjRtReferenceMixin<HloRunnerAgnosticTestBase>(
+          GetHloRunnerForTest(), options.verifier_layout_sensitive,
+          options.allow_mixed_precision_in_hlo_verifier,
+          options.instruction_can_change_layout_func) {}
 
 }  // namespace xla

third_party/xla/xla/tests/hlo_pjrt_test_base.h
@@ -16,6 +16,7 @@ limitations under the License.
 #ifndef XLA_TESTS_HLO_PJRT_TEST_BASE_H_
 #define XLA_TESTS_HLO_PJRT_TEST_BASE_H_
 
+#include "xla/tests/hlo_pjrt_reference_mixin.h"
 #include "xla/tests/hlo_runner_agnostic_test_base.h"
 #include "xla/util.h"
 #include "xla/xla_data.pb.h"
@@ -28,9 +29,10 @@ struct HloPjRtTestBaseOptions {
   HloPredicate instruction_can_change_layout_func;
 };
 
-class HloPjRtTestBase : public HloRunnerAgnosticTestBase {
+class HloPjRtTestBase
+    : public HloPjRtReferenceMixin<HloRunnerAgnosticTestBase> {
  protected:
-  // This uses the SE interpreter backend for the reference backend and
+  // This uses the PjRt interpreter backend for the reference backend and
   // automatically finds a PjRt backend for the test backend.
   explicit HloPjRtTestBase(HloPjRtTestBaseOptions options = {});
 };

CodeLinaro-mirror/yocto-mirrors_github_tensorflow_tensorflow

third_party/xla/xla/tests/hlo_pjrt_test_base.cc
@@ -21,14 +21,12 @@ limitations under the License.
 
 #include "absl/log/check.h"
 #include "absl/status/statusor.h"
-#include "xla/pjrt/interpreter/interpreter_client.h"
 #include "xla/pjrt/pjrt_client.h"
 #include "xla/service/hlo_runner_interface.h"
 #include "xla/service/hlo_runner_pjrt.h"
+#include "xla/tests/hlo_pjrt_interpreter_reference_mixin.h"
 #include "xla/tests/hlo_runner_agnostic_test_base.h"
 #include "xla/tests/pjrt_client_registry.h"
-#include "xla/util.h"
-#include "tsl/platform/logging.h"
 
 namespace xla {
 namespace {
@@ -52,21 +50,12 @@ std::unique_ptr<HloRunnerInterface> GetHloRunnerForTest() {
       *std::move(client), device_shape_representation_fn, device_shape_size_fn);
 }
 
-std::unique_ptr<HloRunnerInterface> GetHloRunnerForReference() {
-  return std::make_unique<HloRunnerPjRt>(
-      std::make_unique<InterpreterClient>(),
-      InterpreterClient::DeviceShapeRepresentation,
-      InterpreterClient::ShapeSizeBytes,
-      /*use_parameter_layout_on_device=*/true);
-}
-
 }  // namespace
 
 HloPjRtTestBase::HloPjRtTestBase(HloPjRtTestBaseOptions options)
-    : HloRunnerAgnosticTestBase(GetHloRunnerForTest(),
-                                GetHloRunnerForReference(),
-                                options.verifier_layout_sensitive,
-                                options.allow_mixed_precision_in_hlo_verifier,
-                                options.instruction_can_change_layout_func) {}
+    : HloPjRtInterpreterReferenceMixin<HloRunnerAgnosticTestBase>(
+          GetHloRunnerForTest(), options.verifier_layout_sensitive,
+          options.allow_mixed_precision_in_hlo_verifier,
+          options.instruction_can_change_layout_func) {}
 
 }  // namespace xla

third_party/xla/xla/tests/hlo_pjrt_test_base.h
@@ -16,6 +16,7 @@ limitations under the License.
 #ifndef XLA_TESTS_HLO_PJRT_TEST_BASE_H_
 #define XLA_TESTS_HLO_PJRT_TEST_BASE_H_
 
+#include "xla/tests/hlo_pjrt_interpreter_reference_mixin.h"
 #include "xla/tests/hlo_runner_agnostic_test_base.h"
 #include "xla/util.h"
 #include "xla/xla_data.pb.h"
@@ -28,9 +29,10 @@ struct HloPjRtTestBaseOptions {
   HloPredicate instruction_can_change_layout_func;
 };
 
-class HloPjRtTestBase : public HloRunnerAgnosticTestBase {
+class HloPjRtTestBase
+    : public HloPjRtInterpreterReferenceMixin<HloRunnerAgnosticTestBase> {
  protected:
-  // This uses the SE interpreter backend for the reference backend and
+  // This uses the PjRt interpreter backend for the reference backend and
   // automatically finds a PjRt backend for the test backend.
   explicit HloPjRtTestBase(HloPjRtTestBaseOptions options = {});
 };
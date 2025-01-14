c5803d2efac5609cb7c86a55a3417106a2ead03e
Abhinav Gunjal <agunjal@google.com>
2025-01-14 13:57:26 -0800

    [Shardy] Remove StableHLO -> MHLO -> StableHLO extra step as flatten-tuple and prepare-for-export passes are now migrated to StableHLO.
    PiperOrigin-RevId: 715519888

--- third_party/xla/xla/mlir_hlo/stablehlo_ext/transforms/stablehlo_flatten_tuple.cpp
+++ third_party/xla/xla/mlir_hlo/stablehlo_ext/transforms/stablehlo_flatten_tuple.cpp
@@ -139,10 +139,19 @@ class StablehloFlattenTuplePass
     MLIRContext *context = &getContext();
     RewritePatternSet patterns(context);
     patterns.add<FlattenCustomCallOp>(context);
-    if (failed(applyPatternsGreedily(getOperation(), std::move(patterns)))) {
+    config.useTopDownTraversal = true;
+    config.enableRegionSimplification =
+        mlir::GreedySimplifyRegionLevel::Disabled;
+    config.fold = false;
+    config.cseConstants = false;
+    if (failed(applyPatternsGreedily(getOperation(), std::move(patterns),
+                                     config))) {
       signalPassFailure();
     }
   }
+
+ private:
+  GreedyRewriteConfig config;
 };
 
 static PassRegistration<StablehloFlattenTuplePass> pass;

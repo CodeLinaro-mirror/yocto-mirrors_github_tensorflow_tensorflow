hexagon-geo-surv/tensorflow

OpenCL tensor buffer for litert Support Lock and Unlock, instantiate MLD cl environment as singleton instance. Added CompileModel CPU test with OpenCL Tensorbuffers as inputs and outputs.
Reverts changelist 546034127

PiperOrigin-RevId: 707331674

tensorflow/lite/experimental/litert/c/litert_tensor_buffer_test.cc
@@ -27,6 +27,7 @@
 #include "tensorflow/lite/experimental/litert/runtime/event.h"
 #include "tensorflow/lite/experimental/litert/runtime/fastrpc_buffer.h"  // IWYU pragma: keep
 #include "tensorflow/lite/experimental/litert/runtime/ion_buffer.h"  // IWYU pragma: keep
+#include "tensorflow/lite/experimental/litert/runtime/opencl_buffer.h"
 
 namespace {
 constexpr const float kTensorData[] = {10, 20, 30, 40};
@@ -330,6 +331,63 @@ TEST(TensorBuffer, Event) {
   ASSERT_EQ(LiteRtHasTensorBufferEvent(tensor_buffer, &has_event),
             kLiteRtStatusOk);
   EXPECT_FALSE(has_event);
+
+  LiteRtDestroyTensorBuffer(tensor_buffer);
+}
+
+TEST(TensorBuffer, OpenCL) {
+// MSAN does not support GPU tests.
+#if defined(MEMORY_SANITIZER)
+  GTEST_SKIP() << "GPU tests are not supported In msan";
+#endif
+
+  if (!litert::internal::OpenCLBuffer::IsSupported()) {
+    GTEST_SKIP() << "OpenCL buffers are not supported on this platform; "
+                    "skipping the test";
+  }
+
+  constexpr auto kTensorBufferType = kLiteRtTensorBufferTypeOpenCl;
+
+  LiteRtTensorBuffer tensor_buffer;
+  ASSERT_EQ(
+      LiteRtCreateManagedTensorBuffer(kTensorBufferType, &kTensorType,
+                                      sizeof(kTensorData), &tensor_buffer),
+      kLiteRtStatusOk);
+
+  LiteRtTensorBufferType buffer_type;
+  ASSERT_EQ(LiteRtGetTensorBufferType(tensor_buffer, &buffer_type),
+            kLiteRtStatusOk);
+  ASSERT_EQ(buffer_type, kTensorBufferType);
+
+  LiteRtRankedTensorType tensor_type;
+  ASSERT_EQ(LiteRtGetTensorBufferTensorType(tensor_buffer, &tensor_type),
+            kLiteRtStatusOk);
+  ASSERT_EQ(tensor_type.element_type, kLiteRtElementTypeFloat32);
+  ASSERT_EQ(tensor_type.layout.rank, 1);
+  ASSERT_EQ(tensor_type.layout.dimensions[0], kTensorType.layout.dimensions[0]);
+  ASSERT_EQ(tensor_type.layout.strides, nullptr);
+
+  size_t size;
+  ASSERT_EQ(LiteRtGetTensorBufferSize(tensor_buffer, &size), kLiteRtStatusOk);
+  ASSERT_EQ(size, sizeof(kTensorData));
+
+  size_t offset;
+  ASSERT_EQ(LiteRtGetTensorBufferOffset(tensor_buffer, &offset),
+            kLiteRtStatusOk);
+  ASSERT_EQ(offset, 0);
+
+  void* host_mem_addr;
+  ASSERT_EQ(
+      LiteRtLockTensorBuffer(tensor_buffer, &host_mem_addr, /*event=*/nullptr),
+      kLiteRtStatusOk);
+  std::memcpy(host_mem_addr, kTensorData, sizeof(kTensorData));
+  ASSERT_EQ(LiteRtUnlockTensorBuffer(tensor_buffer), kLiteRtStatusOk);
+
+  ASSERT_EQ(
+      LiteRtLockTensorBuffer(tensor_buffer, &host_mem_addr, /*event=*/nullptr),
+      kLiteRtStatusOk);
+  ASSERT_EQ(std::memcmp(host_mem_addr, kTensorData, sizeof(kTensorData)), 0);
+  ASSERT_EQ(LiteRtUnlockTensorBuffer(tensor_buffer), kLiteRtStatusOk);
 
   LiteRtDestroyTensorBuffer(tensor_buffer);
 }

tensorflow/lite/experimental/litert/runtime/compiled_model_test.cc
@@ -26,13 +26,13 @@
 #include "absl/strings/string_view.h"
 #include "absl/types/span.h"
 #include "tensorflow/lite/experimental/litert/c/litert_common.h"
-#include "tensorflow/lite/experimental/litert/c/litert_compiled_model_options.h"
 #include "tensorflow/lite/experimental/litert/c/litert_model.h"
 #include "tensorflow/lite/experimental/litert/c/litert_tensor_buffer.h"
 #include "tensorflow/lite/experimental/litert/cc/litert_expected.h"
 #include "tensorflow/lite/experimental/litert/cc/litert_tensor_buffer.h"
 #include "tensorflow/lite/experimental/litert/cc/litert_tensor_buffer_requirements.h"
 #include "tensorflow/lite/experimental/litert/core/model/model.h"
+#include "tensorflow/lite/experimental/litert/runtime/opencl_buffer.h"
 #include "tensorflow/lite/experimental/litert/runtime/tensor_buffer.h"
 #include "tensorflow/lite/experimental/litert/test/common.h"
 #include "tensorflow/lite/experimental/litert/test/testdata/simple_model_test_vectors.h"
@@ -288,5 +288,92 @@ TEST(CompiledModelTest, UseAhwbBuffer) {
   LiteRtDestroyModel(model);
 }
 
+TEST(CompiledModelTest, UseOpenCLBuffer) {
+  // MSAN does not support GPU tests.
+#if defined(MEMORY_SANITIZER)
+  GTEST_SKIP() << "GPU tests are not supported In msan";
+#endif
+
+  if (!litert::internal::OpenCLBuffer::IsSupported()) {
+    GTEST_SKIP() << "OpenCL buffers are not supported on this platform; "
+                    "skipping the test";
+  }
+  auto path = testing::GetTestFilePath(kModelFileName);
+  LiteRtModel model;
+  ASSERT_EQ(LiteRtCreateModelFromFile(path.c_str(), &model), kLiteRtStatusOk);
+
+  auto res_compiled_model =
+      LiteRtCompiledModelT::Create(model, kLiteRtHwAccelatorNone);
+  ASSERT_TRUE(res_compiled_model) << "Failed to initialize CompiledModel";
+  auto& compiled_model = **res_compiled_model;
+
+  auto signatures = model->Signatures();
+  ASSERT_EQ(signatures.size(), 1);
+  auto signature_key = signatures[0]->Key();
+  EXPECT_EQ(signature_key, LiteRtSignatureT::kDefaultSignatureKey);
+
+  auto input_buffers_res =
+      CreateInputBuffers(model, signature_key, kLiteRtTensorBufferTypeOpenCl,
+                         sizeof(float) * kTestInput0Size);
+  EXPECT_TRUE(input_buffers_res);
+  auto input_buffers = std::move(*input_buffers_res);
+
+  auto output_buffers_res =
+      CreateOutputBuffers(model, signature_key, kLiteRtTensorBufferTypeOpenCl,
+                          sizeof(float) * kTestOutputSize);
+  EXPECT_TRUE(output_buffers_res);
+  auto output_buffers = std::move(*output_buffers_res);
+
+  // Fill model inputs.
+  auto input_names = signatures[0]->InputNames();
+  EXPECT_EQ(input_names.size(), 2);
+  EXPECT_EQ(input_names.at(0), "arg0");
+  EXPECT_EQ(input_names.at(1), "arg1");
+  auto& input_0_buffer = input_buffers[0];
+  EXPECT_EQ(input_0_buffer->buffer_type(), kLiteRtTensorBufferTypeOpenCl);
+  {
+    TensorBuffer opencl_buffer(input_0_buffer, /*owned=*/false);
+    opencl_buffer.Write<float>(
+        absl::MakeConstSpan(kTestInput0Tensor, kTestInput0Size));
+  }
+  auto& input_1_buffer = input_buffers[1];
+  {
+    TensorBuffer opencl_buffer(input_1_buffer, /*owned=*/false);
+    opencl_buffer.Write<float>(
+        absl::MakeConstSpan(kTestInput1Tensor, kTestInput1Size));
+  }
+
+  // Execute model.
+  compiled_model.Run(signature_key, input_buffers, output_buffers);
+
+  // Check model output.
+  auto output_names = signatures[0]->OutputNames();
+  EXPECT_EQ(output_names.size(), 1);
+  EXPECT_EQ(output_names.at(0), "tfl.add");
+  {
+    void* host_mem_addr;
+    ASSERT_EQ(LiteRtLockTensorBuffer(output_buffers[0], &host_mem_addr,
+                                     /*event=*/nullptr),
+              kLiteRtStatusOk);
+    auto output = absl::MakeSpan(static_cast<const float*>(host_mem_addr),
+                                 kTestOutputSize);
+    for (auto i = 0; i < kTestOutputSize; ++i) {
+      ABSL_LOG(INFO) << output[i] << "\t" << kTestOutputTensor[i];
+    }
+    EXPECT_THAT(output, Pointwise(FloatNear(1e-5), kTestOutputTensor));
+
+    ASSERT_EQ(LiteRtUnlockTensorBuffer(output_buffers[0]), kLiteRtStatusOk);
+  }
+
+  // Since Buffers in LiteRtTensorBuffer, we need to destroy them explicitly.
+  for (auto& input_buffer : input_buffers) {
+    LiteRtDestroyTensorBuffer(input_buffer);
+  }
+  for (auto& output_buffer : output_buffers) {
+    LiteRtDestroyTensorBuffer(output_buffer);
+  }
+
+  LiteRtDestroyModel(model);
+}
 }  // namespace
 }  // namespace litert

tensorflow/lite/experimental/litert/runtime/tensor_buffer.h
@@ -16,15 +16,23 @@
 #define TENSORFLOW_LITE_EXPERIMENTAL_LITERT_RUNTIME_TENSOR_BUFFER_H_
 
 #include <atomic>
+#include <cstddef>
+#include <cstdint>
 #include <memory>
 #include <optional>
 #include <type_traits>
+#include <utility>
 #include <variant>
+#include <vector>
 
 #include "absl/types/span.h"
-#include "tensorflow/lite/experimental/litert/c/litert_common.h"
+#include "third_party/opencl_headers/CL/cl.h"
+#include "tensorflow/lite/experimental/litert/c/litert_event.h"
+#include "tensorflow/lite/experimental/litert/c/litert_layout.h"
+#include "tensorflow/lite/experimental/litert/c/litert_model.h"
 #include "tensorflow/lite/experimental/litert/c/litert_tensor_buffer.h"
 #include "tensorflow/lite/experimental/litert/cc/litert_expected.h"
+#include "tensorflow/lite/experimental/litert/runtime/opencl_buffer.h"
 
 class LiteRtTensorBufferT {
  public:
@@ -65,6 +73,10 @@ class LiteRtTensorBufferT {
       size_t fastrpc_buffer_offset,
       LiteRtFastRpcDeallocator deallocator = nullptr);
 
+  static litert::Expected<Ptr> CreateFromOpenCLBuffer(
+      const LiteRtRankedTensorType& tensor_type, cl_mem buffer,
+      size_t opencl_buffer_size, LiteRtOpenCLDeallocator deallocator = nullptr);
+
   static litert::Expected<Ptr> CreateManaged(
       LiteRtTensorBufferType buffer_type,
       const LiteRtRankedTensorType& tensor_type, size_t buffer_size);
@@ -92,6 +104,7 @@ class LiteRtTensorBufferT {
   litert::Expected<std::pair<void*, int>> GetIonBuffer();
   litert::Expected<std::pair<void*, int>> GetDmaBufBuffer();
   litert::Expected<std::pair<void*, int>> GetFastRpcBuffer();
+  litert::Expected<litert::internal::OpenCLBuffer*> GetOpenCLBuffer();
 
   litert::Expected<void*> Lock(LiteRtEvent event = nullptr);
   litert::Expected<void> Unlock();
@@ -164,6 +177,9 @@ class LiteRtTensorBufferT {
   static litert::Expected<Ptr> CreateManagedFastRpcBuffer(
       const LiteRtRankedTensorType& tensor_type, size_t buffer_size);
 
+  static litert::Expected<Ptr> CreateManagedOpenCLBuffer(
+      const LiteRtRankedTensorType& tensor_type, size_t buffer_size);
+
   litert::Expected<void> IsValid();
 
   LiteRtRankedTensorType tensor_type_;
@@ -172,7 +188,8 @@ class LiteRtTensorBufferT {
   LiteRtTensorBufferType buffer_type_;
   size_t buffer_size_;
   size_t buffer_offset_;
-  std::variant<HostBuffer, AhwbBuffer, IonBuffer, DmaBufBuffer, FastRpcBuffer>
+  std::variant<HostBuffer, AhwbBuffer, IonBuffer, DmaBufBuffer, FastRpcBuffer,
+               litert::internal::OpenCLBuffer>
       buffer_;
   std::optional<LiteRtEvent> event_;
   mutable std::atomic_int_fast32_t ref_;

CodeLinaro-mirror/yocto-mirrors_github_tensorflow_tensorflow

tensorflow/lite/experimental/litert/c/litert_tensor_buffer_test.cc
@@ -27,6 +27,7 @@
 #include "tensorflow/lite/experimental/litert/runtime/event.h"
 #include "tensorflow/lite/experimental/litert/runtime/fastrpc_buffer.h"  // IWYU pragma: keep
 #include "tensorflow/lite/experimental/litert/runtime/ion_buffer.h"  // IWYU pragma: keep
+#include "tensorflow/lite/experimental/litert/runtime/open_cl_buffer.h"
 
 namespace {
 constexpr const float kTensorData[] = {10, 20, 30, 40};
@@ -330,6 +331,63 @@ TEST(TensorBuffer, Event) {
   ASSERT_EQ(LiteRtHasTensorBufferEvent(tensor_buffer, &has_event),
             kLiteRtStatusOk);
   EXPECT_FALSE(has_event);
+
+  LiteRtDestroyTensorBuffer(tensor_buffer);
+}
+
+TEST(TensorBuffer, OpenCL) {
+// MSAN does not support GPU tests.
+#if defined(MEMORY_SANITIZER)
+  GTEST_SKIP() << "GPU tests are not supported In msan";
+#endif
+
+  if (!litert::internal::OpenClBuffer::IsSupported()) {
+    GTEST_SKIP() << "OpenCL buffers are not supported on this platform; "
+                    "skipping the test";
+  }
+
+  constexpr auto kTensorBufferType = kLiteRtTensorBufferTypeOpenCl;
+
+  LiteRtTensorBuffer tensor_buffer;
+  ASSERT_EQ(
+      LiteRtCreateManagedTensorBuffer(kTensorBufferType, &kTensorType,
+                                      sizeof(kTensorData), &tensor_buffer),
+      kLiteRtStatusOk);
+
+  LiteRtTensorBufferType buffer_type;
+  ASSERT_EQ(LiteRtGetTensorBufferType(tensor_buffer, &buffer_type),
+            kLiteRtStatusOk);
+  ASSERT_EQ(buffer_type, kTensorBufferType);
+
+  LiteRtRankedTensorType tensor_type;
+  ASSERT_EQ(LiteRtGetTensorBufferTensorType(tensor_buffer, &tensor_type),
+            kLiteRtStatusOk);
+  ASSERT_EQ(tensor_type.element_type, kLiteRtElementTypeFloat32);
+  ASSERT_EQ(tensor_type.layout.rank, 1);
+  ASSERT_EQ(tensor_type.layout.dimensions[0], kTensorType.layout.dimensions[0]);
+  ASSERT_EQ(tensor_type.layout.strides, nullptr);
+
+  size_t size;
+  ASSERT_EQ(LiteRtGetTensorBufferSize(tensor_buffer, &size), kLiteRtStatusOk);
+  ASSERT_EQ(size, sizeof(kTensorData));
+
+  size_t offset;
+  ASSERT_EQ(LiteRtGetTensorBufferOffset(tensor_buffer, &offset),
+            kLiteRtStatusOk);
+  ASSERT_EQ(offset, 0);
+
+  void* host_mem_addr;
+  ASSERT_EQ(
+      LiteRtLockTensorBuffer(tensor_buffer, &host_mem_addr, /*event=*/nullptr),
+      kLiteRtStatusOk);
+  std::memcpy(host_mem_addr, kTensorData, sizeof(kTensorData));
+  ASSERT_EQ(LiteRtUnlockTensorBuffer(tensor_buffer), kLiteRtStatusOk);
+
+  ASSERT_EQ(
+      LiteRtLockTensorBuffer(tensor_buffer, &host_mem_addr, /*event=*/nullptr),
+      kLiteRtStatusOk);
+  ASSERT_EQ(std::memcmp(host_mem_addr, kTensorData, sizeof(kTensorData)), 0);
+  ASSERT_EQ(LiteRtUnlockTensorBuffer(tensor_buffer), kLiteRtStatusOk);
 
   LiteRtDestroyTensorBuffer(tensor_buffer);
 }

tensorflow/lite/experimental/litert/runtime/compiled_model_test.cc
@@ -26,13 +26,13 @@
 #include "absl/strings/string_view.h"
 #include "absl/types/span.h"
 #include "tensorflow/lite/experimental/litert/c/litert_common.h"
-#include "tensorflow/lite/experimental/litert/c/litert_compiled_model_options.h"
 #include "tensorflow/lite/experimental/litert/c/litert_model.h"
 #include "tensorflow/lite/experimental/litert/c/litert_tensor_buffer.h"
 #include "tensorflow/lite/experimental/litert/cc/litert_expected.h"
 #include "tensorflow/lite/experimental/litert/cc/litert_tensor_buffer.h"
 #include "tensorflow/lite/experimental/litert/cc/litert_tensor_buffer_requirements.h"
 #include "tensorflow/lite/experimental/litert/core/model/model.h"
+#include "tensorflow/lite/experimental/litert/runtime/open_cl_buffer.h"
 #include "tensorflow/lite/experimental/litert/runtime/tensor_buffer.h"
 #include "tensorflow/lite/experimental/litert/test/common.h"
 #include "tensorflow/lite/experimental/litert/test/testdata/simple_model_test_vectors.h"
@@ -288,5 +288,92 @@ TEST(CompiledModelTest, UseAhwbBuffer) {
   LiteRtDestroyModel(model);
 }
 
+TEST(CompiledModelTest, UseOpenCLBuffer) {
+  // MSAN does not support GPU tests.
+#if defined(MEMORY_SANITIZER)
+  GTEST_SKIP() << "GPU tests are not supported In msan";
+#endif
+
+  if (!litert::internal::OpenClBuffer::IsSupported()) {
+    GTEST_SKIP() << "OpenCL buffers are not supported on this platform; "
+                    "skipping the test";
+  }
+  auto path = testing::GetTestFilePath(kModelFileName);
+  LiteRtModel model;
+  ASSERT_EQ(LiteRtCreateModelFromFile(path.c_str(), &model), kLiteRtStatusOk);
+
+  auto res_compiled_model =
+      LiteRtCompiledModelT::Create(model, kLiteRtHwAccelatorNone);
+  ASSERT_TRUE(res_compiled_model) << "Failed to initialize CompiledModel";
+  auto& compiled_model = **res_compiled_model;
+
+  auto signatures = model->Signatures();
+  ASSERT_EQ(signatures.size(), 1);
+  auto signature_key = signatures[0]->Key();
+  EXPECT_EQ(signature_key, LiteRtSignatureT::kDefaultSignatureKey);
+
+  auto input_buffers_res =
+      CreateInputBuffers(model, signature_key, kLiteRtTensorBufferTypeOpenCl,
+                         sizeof(float) * kTestInput0Size);
+  EXPECT_TRUE(input_buffers_res);
+  auto input_buffers = std::move(*input_buffers_res);
+
+  auto output_buffers_res =
+      CreateOutputBuffers(model, signature_key, kLiteRtTensorBufferTypeOpenCl,
+                          sizeof(float) * kTestOutputSize);
+  EXPECT_TRUE(output_buffers_res);
+  auto output_buffers = std::move(*output_buffers_res);
+
+  // Fill model inputs.
+  auto input_names = signatures[0]->InputNames();
+  EXPECT_EQ(input_names.size(), 2);
+  EXPECT_EQ(input_names.at(0), "arg0");
+  EXPECT_EQ(input_names.at(1), "arg1");
+  auto& input_0_buffer = input_buffers[0];
+  EXPECT_EQ(input_0_buffer->buffer_type(), kLiteRtTensorBufferTypeOpenCl);
+  {
+    TensorBuffer opencl_buffer(input_0_buffer, /*owned=*/false);
+    opencl_buffer.Write<float>(
+        absl::MakeConstSpan(kTestInput0Tensor, kTestInput0Size));
+  }
+  auto& input_1_buffer = input_buffers[1];
+  {
+    TensorBuffer opencl_buffer(input_1_buffer, /*owned=*/false);
+    opencl_buffer.Write<float>(
+        absl::MakeConstSpan(kTestInput1Tensor, kTestInput1Size));
+  }
+
+  // Execute model.
+  compiled_model.Run(signature_key, input_buffers, output_buffers);
+
+  // Check model output.
+  auto output_names = signatures[0]->OutputNames();
+  EXPECT_EQ(output_names.size(), 1);
+  EXPECT_EQ(output_names.at(0), "tfl.add");
+  {
+    void* host_mem_addr;
+    ASSERT_EQ(LiteRtLockTensorBuffer(output_buffers[0], &host_mem_addr,
+                                     /*event=*/nullptr),
+              kLiteRtStatusOk);
+    auto output = absl::MakeSpan(static_cast<const float*>(host_mem_addr),
+                                 kTestOutputSize);
+    for (auto i = 0; i < kTestOutputSize; ++i) {
+      ABSL_LOG(INFO) << output[i] << "\t" << kTestOutputTensor[i];
+    }
+    EXPECT_THAT(output, Pointwise(FloatNear(1e-5), kTestOutputTensor));
+
+    ASSERT_EQ(LiteRtUnlockTensorBuffer(output_buffers[0]), kLiteRtStatusOk);
+  }
+
+  // Since Buffers in LiteRtTensorBuffer, we need to destroy them explicitly.
+  for (auto& input_buffer : input_buffers) {
+    LiteRtDestroyTensorBuffer(input_buffer);
+  }
+  for (auto& output_buffer : output_buffers) {
+    LiteRtDestroyTensorBuffer(output_buffer);
+  }
+
+  LiteRtDestroyModel(model);
+}
 }  // namespace
 }  // namespace litert

tensorflow/lite/experimental/litert/runtime/tensor_buffer.h
@@ -16,17 +16,24 @@
 #define TENSORFLOW_LITE_EXPERIMENTAL_LITERT_RUNTIME_TENSOR_BUFFER_H_
 
 #include <atomic>
+#include <cstddef>
 #include <cstdint>
 #include <memory>
 #include <optional>
 #include <type_traits>
+#include <utility>
 #include <variant>
 #include <vector>
 
 #include "absl/types/span.h"
+#include "third_party/opencl_headers/CL/cl.h"
 #include "tensorflow/lite/experimental/litert/c/litert_common.h"
+#include "tensorflow/lite/experimental/litert/c/litert_event.h"
+#include "tensorflow/lite/experimental/litert/c/litert_layout.h"
+#include "tensorflow/lite/experimental/litert/c/litert_model.h"
 #include "tensorflow/lite/experimental/litert/c/litert_tensor_buffer.h"
 #include "tensorflow/lite/experimental/litert/cc/litert_expected.h"
+#include "tensorflow/lite/experimental/litert/runtime/open_cl_buffer.h"
 
 class LiteRtTensorBufferT {
  public:
@@ -67,6 +74,10 @@ class LiteRtTensorBufferT {
       size_t fastrpc_buffer_offset,
       LiteRtFastRpcDeallocator deallocator = nullptr);
 
+  static litert::Expected<Ptr> CreateFromOpenClBuffer(
+      const LiteRtRankedTensorType& tensor_type, cl_mem buffer,
+      size_t opencl_buffer_size, LiteRtOpenClDeallocator deallocator = nullptr);
+
   static litert::Expected<Ptr> CreateManaged(
       LiteRtTensorBufferType buffer_type,
       const LiteRtRankedTensorType& tensor_type, size_t buffer_size);
@@ -94,6 +105,7 @@ class LiteRtTensorBufferT {
   litert::Expected<std::pair<void*, int>> GetIonBuffer();
   litert::Expected<std::pair<void*, int>> GetDmaBufBuffer();
   litert::Expected<std::pair<void*, int>> GetFastRpcBuffer();
+  litert::Expected<litert::internal::OpenClBuffer*> GetOpenClBuffer();
 
   litert::Expected<void*> Lock(LiteRtEvent event = nullptr);
   litert::Expected<void> Unlock();
@@ -166,6 +178,9 @@ class LiteRtTensorBufferT {
   static litert::Expected<Ptr> CreateManagedFastRpcBuffer(
       const LiteRtRankedTensorType& tensor_type, size_t buffer_size);
 
+  static litert::Expected<Ptr> CreateManagedOpenClBuffer(
+      const LiteRtRankedTensorType& tensor_type, size_t buffer_size);
+
   litert::Expected<void> IsValid();
 
   LiteRtRankedTensorType tensor_type_;
@@ -174,7 +189,8 @@ class LiteRtTensorBufferT {
   LiteRtTensorBufferType buffer_type_;
   size_t buffer_size_;
   size_t buffer_offset_;
-  std::variant<HostBuffer, AhwbBuffer, IonBuffer, DmaBufBuffer, FastRpcBuffer>
+  std::variant<HostBuffer, AhwbBuffer, IonBuffer, DmaBufBuffer, FastRpcBuffer,
+               litert::internal::OpenClBuffer>
       buffer_;
   std::optional<LiteRtEvent> event_;
   mutable std::atomic_int_fast32_t ref_;
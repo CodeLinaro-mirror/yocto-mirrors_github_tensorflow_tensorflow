b4489c2dfd2cb7a3ae754a95c8d75b71f56a6e74
Luke Boyer <lukeboyer@google.com>
2025-01-27 13:01:13 -0800

    Unify metadata storage with unified buffer management.
    PiperOrigin-RevId: 720278893

--- tensorflow/lite/experimental/litert/core/model/model_test.cc
+++ tensorflow/lite/experimental/litert/core/model/model_test.cc
@@ -27,6 +27,7 @@
 #include "tensorflow/lite/experimental/litert/c/litert_model.h"
 #include "tensorflow/lite/experimental/litert/c/litert_op_code.h"
 #include "tensorflow/lite/experimental/litert/cc/litert_buffer_ref.h"
+#include "tensorflow/lite/experimental/litert/core/model/buffer_manager.h"
 #include "tensorflow/lite/experimental/litert/core/util/flatbuffer_tools.h"
 #include "tensorflow/lite/experimental/litert/test/test_macros.h"
 #include "tensorflow/lite/schema/schema_generated.h"
@@ -55,20 +56,6 @@ TEST(ModelTest, MetadataDNE) {
   LiteRtModelT model;
   auto res = model.FindMetadata("FOO");
   ASSERT_FALSE(res.HasValue());
-}
-
-TEST(ModelTest, PopMetadata) {
-  static constexpr absl::string_view kMetadata = "VALUE";
-  static constexpr absl::string_view kKey = "KEY";
-
-  LiteRtModelT model;
-  LITERT_ASSERT_STATUS_OK(model.PushMetadata(kKey, kMetadata));
-
-  auto popped_metadata = model.PopMetadata(kKey);
-  ASSERT_TRUE(popped_metadata);
-  EXPECT_EQ(popped_metadata->StrView(), kMetadata);
-
-  EXPECT_FALSE(model.FindMetadata(kKey));
 }
 
 TEST(ModelTest, EmplaceSubgraph) {
@@ -265,12 +252,63 @@ TEST(ModelQuantizationTypeTest, MakePerChannel) {
   EXPECT_THAT(zeros, ElementsAreArray(kZero));
 }
 
-TEST(ModelWeightsTest, WeightsFromBuf) {
+TEST(ModelWeightsTest, EmptyWeights) {
+  LiteRtWeightsT weights;
+  EXPECT_EQ(weights.Buffer().Size(), 0);
+}
+
+TEST(ModelWeightsTest, WeightsWithExternalBufferManager) {
+  static constexpr absl::string_view kData = "some_data";
+  BufferManager manager;
+
+  LiteRtWeightsT weights;
+  weights.SetBufferManager(&manager);
+
+  BufferRef<uint8_t> buf(kData.data(), kData.size());
+  SetWeightsFromUnownedBuffer(weights, buf);
+
+  EXPECT_EQ(manager.GetBuffer(weights.GetBufferId())->StrView(), kData);
+  EXPECT_EQ(weights.Buffer().StrView(), kData);
+}
+
+TEST(ModelWeightsTest, WeightsFromUnownedBuffer) {
   static constexpr absl::string_view kData = "some_data";
 
   LiteRtWeightsT weights;
-  weights.SetFromBuf(BufferRef<uint8_t>(kData.data(), kData.size()));
-  EXPECT_EQ(weights.Buf().StrView(), kData);
+  BufferRef<uint8_t> buf(kData.data(), kData.size());
+  SetWeightsFromUnownedBuffer(weights, buf);
+
+  EXPECT_EQ(weights.Buffer().StrView(), kData);
+}
+
+TEST(ModelWeightsTest, WeightsFromOwnedBuffer) {
+  static constexpr absl::string_view kData = "some_data";
+
+  LiteRtWeightsT weights;
+
+  OwningBufferRef<uint8_t> buf(kData);
+  SetWeightsFromUnownedBuffer(weights, std::move(buf));
+
+  EXPECT_EQ(weights.Buffer().StrView(), kData);
+}
+
+TEST(ModelWeightsTest, OverwriteBuffer) {
+  static constexpr absl::string_view kData = "some_data";
+  static constexpr absl::string_view kData2 = "some_data2";
+
+  LiteRtWeightsT weights;
+
+  {
+    OwningBufferRef<uint8_t> buf(kData);
+    SetWeightsFromOwnedBuffer(weights, std::move(buf));
+  }
+
+  {
+    OwningBufferRef<uint8_t> buf(kData2);
+    SetWeightsFromOwnedBuffer(weights, std::move(buf));
+  }
+
+  EXPECT_EQ(weights.Buffer().StrView(), kData2);
 }
 
 TEST(ModelTensorTest, Name) {

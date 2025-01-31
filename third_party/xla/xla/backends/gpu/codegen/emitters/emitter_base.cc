03ceb1da64e92602f66b66377893167680dd62cc
Emilio Cota <ecg@google.com>
2025-01-31 07:24:30 -0800

    [xla:emitters] move ConvertPureCallOpsPass to xla/codegen/emitters
    Will be shared with the CPU pipeline.
    
    PiperOrigin-RevId: 721765872

--- third_party/xla/xla/backends/gpu/codegen/emitters/emitter_base.cc
+++ third_party/xla/xla/backends/gpu/codegen/emitters/emitter_base.cc
@@ -613,7 +613,7 @@ void AddLoopTransformationPasses(mlir::OpPassManager& pm,
 
 void AddLoweringPasses(mlir::OpPassManager& pm,
                        const se::DeviceDescription& device) {
-  pm.addNestedPass<FuncOp>(CreateConvertPureCallOpsPass());
+  pm.addNestedPass<FuncOp>(emitters::CreateConvertPureCallOpsPass());
   pm.addPass(emitters::CreateLowerTensorsPass(device));
   pm.addPass(mlir::createConvertComplexToStandardPass());
   pm.addPass(CreateMergePointersToSameSlicePass());
@@ -623,7 +623,7 @@ void AddLoweringPasses(mlir::OpPassManager& pm,
   pm.addPass(mlir::createCanonicalizerPass());
   pm.addPass(mlir::createCSEPass());
   pm.addNestedPass<FuncOp>(emitters::CreateSimplifyArithPass());
-  pm.addPass(CreateSimplifyAffinePass());
+  pm.addPass(emitters::CreateSimplifyAffinePass());
   pm.addPass(CreateConvertIndexTypePass());
   // simplify-affine lowers most affine.apply ops, but if it can't prove a
   // division or modulo is unsigned, affine.apply ops will remain.
--- third_party/xla/xla/codegen/emitters/transforms/passes.h
+++ third_party/xla/xla/codegen/emitters/transforms/passes.h
@@ -27,6 +27,7 @@ namespace emitters {
 #define GEN_PASS_DECL
 #include "xla/codegen/emitters/transforms/passes.h.inc"
 
+std::unique_ptr<mlir::Pass> CreateConvertPureCallOpsPass();
 std::unique_ptr<mlir::Pass> CreateEraseDeadFunctionsPass();
 std::unique_ptr<mlir::Pass> CreateExpandFloatOpsPass();
 std::unique_ptr<mlir::Pass> CreateFlattenTensorsPass();
@@ -41,6 +42,7 @@ std::unique_ptr<mlir::Pass> CreateLowerToLLVMPass(
 std::unique_ptr<mlir::Pass> CreateLowerToLLVMPass(
     const stream_executor::DeviceDescription& device_description);
 std::unique_ptr<mlir::Pass> CreatePropagateSliceIndicesPass();
+std::unique_ptr<mlir::Pass> CreateSimplifyAffinePass();
 std::unique_ptr<mlir::Pass> CreateSimplifyArithPass();
 
 #define GEN_PASS_REGISTRATION

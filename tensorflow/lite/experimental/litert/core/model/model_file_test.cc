1b1c48533384bf4c3e344e9cf51fc8512c233c02
Terry Heo <terryheo@google.com>
2025-02-19 13:02:02 -0800

    litert: Create default signature if model doesn't have it
    Signature is needed for CompiledModel implementation. This fix generates a default
    signature if model doesn't have it.
    
    PiperOrigin-RevId: 728784478

--- tensorflow/lite/experimental/litert/core/model/model_file_test.cc
+++ tensorflow/lite/experimental/litert/core/model/model_file_test.cc
@@ -22,11 +22,17 @@
 #include <utility>
 #include <vector>
 
+// schema/mutable/schema_generated.h and schema/schema_generated.h (included
+// through flatbuffer_tools.h via model.h) have the same #ifdef, thus this line
+// need to be put at the top to ensure we get the "mutable" version.
+#if 1
+#include "tensorflow/compiler/mlir/lite/schema/mutable/schema_generated.h"
+#endif
+
 #include <gmock/gmock.h>  // IWYU pragma: keep
 #include <gtest/gtest.h>
 #include "absl/strings/string_view.h"
 #include "absl/types/span.h"
-#include "tensorflow/compiler/mlir/lite/schema/mutable/schema_generated.h"
 #include "tensorflow/lite/experimental/litert/c/litert_common.h"
 #include "tensorflow/lite/experimental/litert/c/litert_model.h"
 #include "tensorflow/lite/experimental/litert/c/litert_op_code.h"
@@ -200,6 +206,19 @@ TEST(ModelLoadTest, WithSignature) {
       litert_model.FindSignature(LiteRtSignatureT::kDefaultSignatureKey);
   ASSERT_TRUE(signature);
 
+  EXPECT_EQ(signature->get().InputNames().size(), 1);
+  EXPECT_EQ(signature->get().OutputNames().size(), 1);
+  EXPECT_EQ(&signature->get().GetSubgraph(), litert_model.MainSubgraph());
+}
+
+TEST(ModelLoadTest, NoSignature) {
+  auto model = *Model::CreateFromFile(testing::GetTfliteFilePath(
+      "java/demo/app/src/main/assets/mobilenet_v1_1.0_224.tflite"));
+  ASSERT_TRUE(model);
+  auto& litert_model = *model.Get();
+  auto signature =
+      litert_model.FindSignature(LiteRtSignatureT::kDefaultSignatureKey);
+  ASSERT_TRUE(signature);
   EXPECT_EQ(signature->get().InputNames().size(), 1);
   EXPECT_EQ(signature->get().OutputNames().size(), 1);
   EXPECT_EQ(&signature->get().GetSubgraph(), litert_model.MainSubgraph());

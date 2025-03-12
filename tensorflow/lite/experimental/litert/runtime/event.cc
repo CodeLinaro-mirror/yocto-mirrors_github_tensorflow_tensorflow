65e60ab2afe08e32605ce119fc3ec7d33b6c0034
Terry Heo <terryheo@google.com>
2025-03-12 14:05:54 -0700

    litert: Add OpenCL event support
    - Added LiteRtEventType
    - Added OpenCL event creation methods
    - Added Signal method
    
    PiperOrigin-RevId: 736259437

--- tensorflow/lite/experimental/litert/runtime/event.cc
+++ tensorflow/lite/experimental/litert/runtime/event.cc
@@ -20,43 +20,65 @@
 #include <cstdint>
 
 #include "tensorflow/lite/experimental/litert/c/litert_common.h"
+#include "tensorflow/lite/experimental/litert/c/litert_event_type.h"
 #include "tensorflow/lite/experimental/litert/cc/litert_expected.h"
+#include "tensorflow/lite/experimental/litert/runtime/environment.h"
 
 #if LITERT_HAS_SYNC_FENCE_SUPPORT
 #include <poll.h>
 #include <unistd.h>
 #endif  // LITERT_HAS_SYNC_FENCE_SUPPORT
+#if LITERT_HAS_OPENCL_SUPPORT
+#include <CL/cl.h>
+#endif  // LITERT_HAS_OPENCL_SUPPORT
 
 using litert::Error;
 using litert::Expected;
 
 Expected<void> LiteRtEventT::Wait(int64_t timeout_in_ms) {
+  if (type == LiteRtEventTypeSyncFenceFd) {
 #if LITERT_HAS_SYNC_FENCE_SUPPORT
-  struct pollfd fds = {
-      .fd = fd,
-      .events = POLLIN,
-  };
+    struct pollfd fds = {
+        .fd = fd,
+        .events = POLLIN,
+    };
 
-  int ret;
-  do {
-    ret = ::poll(&fds, 1, timeout_in_ms);
-    if (ret == 1) {
-      break;
-    } else if (ret == 0) {
-      return Error(kLiteRtStatusErrorTimeoutExpired, "Timeout expired");
+    int ret;
+    do {
+      ret = ::poll(&fds, 1, timeout_in_ms);
+      if (ret == 1) {
+        break;
+      } else if (ret == 0) {
+        return Error(kLiteRtStatusErrorTimeoutExpired, "Timeout expired");
+      }
+    } while (ret == -1 && (errno == EINTR || errno == EAGAIN));
+
+    if (ret < 0) {
+      return Error(kLiteRtStatusErrorRuntimeFailure, "Error waiting for fence");
     }
-  } while (ret == -1 && (errno == EINTR || errno == EAGAIN));
 
-  if (ret < 0) {
-    return Error(kLiteRtStatusErrorRuntimeFailure, "Error waiting for fence");
-  }
-
-  return {};
+    return {};
 
 #else
-  return Error(kLiteRtStatusErrorUnsupported,
-               "LiteRtEventWait not implemented for this platform");
+    return Error(kLiteRtStatusErrorUnsupported,
+                 "LiteRtEventWait not implemented for this platform");
 #endif
+  } else if (type == LiteRtEventTypeOpenCl) {
+#if LITERT_HAS_OPENCL_SUPPORT
+    cl_int res = clWaitForEvents(/*num_events=*/1,
+                                 /*event_list=*/&opencl_event);
+    if (res != CL_SUCCESS) {
+      return Error(kLiteRtStatusErrorRuntimeFailure,
+                   "Error waiting for OpenCL event");
+    }
+    return {};
+#else
+    return Error(kLiteRtStatusErrorUnsupported,
+                 "LiteRtEventWait not implemented for this platform");
+#endif
+  } else {
+    return Error(kLiteRtStatusErrorInvalidArgument, "Invalid event type");
+  }
 }
 
 #if LITERT_HAS_SYNC_FENCE_SUPPORT
@@ -69,8 +91,42 @@ inline bool IsFdValid(int fd) {
 
 LiteRtEventT::~LiteRtEventT() {
 #if LITERT_HAS_SYNC_FENCE_SUPPORT
-  if (owns_fd && IsFdValid(fd)) {
+  if (type == LiteRtEventTypeSyncFenceFd && owns_fd && IsFdValid(fd)) {
     ::close(fd);
   }
 #endif
+}
+
+Expected<void> LiteRtEventT::Signal() {
+#if LITERT_HAS_OPENCL_SUPPORT
+  if (type == LiteRtEventTypeOpenCl) {
+    cl_int res = clSetUserEventStatus(opencl_event, CL_COMPLETE);
+    if (res != CL_SUCCESS) {
+      return Error(kLiteRtStatusErrorRuntimeFailure,
+                   "Error signaling OpenCL event");
+    }
+    return {};
+  }
+#endif
+  return Error(kLiteRtStatusErrorInvalidArgument,
+               "The event trigger is not supported");
+}
+
+LiteRtEventT* LiteRtEventT::CreateManaged(LiteRtEventType type) {
+#if LITERT_HAS_OPENCL_SUPPORT
+  if (type == LiteRtEventTypeOpenCl) {
+    auto& env = litert::internal::EnvironmentSingleton::GetInstance();
+    cl_int errcode_ret;
+    cl_event user_event =
+        clCreateUserEvent(env.getContext()->context(), &errcode_ret);
+    if (errcode_ret != CL_SUCCESS) {
+      return nullptr;
+    }
+    return new LiteRtEventT{
+        .type = LiteRtEventTypeOpenCl,
+        .opencl_event = user_event,
+    };
+  }
+#endif
+  return nullptr;
 }

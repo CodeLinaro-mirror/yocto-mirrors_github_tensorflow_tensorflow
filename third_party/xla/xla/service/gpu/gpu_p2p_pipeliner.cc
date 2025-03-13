sdfjlasdjf/xla

[XLA:GPU] Fix control dependency generation for async collectives, send/recv
PiperOrigin-RevId: 736204963

xla/service/gpu/gpu_p2p_pipeliner.cc
@@ -309,13 +309,19 @@ static std::vector<HloInstruction*> GetSendRecvStartInstructions(
   return start_instructions;
 }
 
-static absl::Nullable<HloInstruction*> GetSendRecvDoneInstructions(
+static absl::Nullable<HloInstruction*> GetSendRecvStartInstructionOrSelf(
+    absl::Nonnull<HloInstruction*> instr) {
+  HloInstruction* send_recv_start = GetSendRecvStartInstruction(instr);
+  return send_recv_start != nullptr ? send_recv_start : instr;
+}
+
+static absl::Nullable<HloInstruction*> GetSendRecvDoneInstructionOrSelf(
     absl::Nonnull<HloInstruction*> rotated_instr) {
   auto it = absl::c_find_if(rotated_instr->users(), [](HloInstruction* user) {
     return user->opcode() == HloOpcode::kRecvDone ||
            user->opcode() == HloOpcode::kSendDone;
   });
-  return it != rotated_instr->users().end() ? *it : nullptr;
+  return it != rotated_instr->users().end() ? *it : rotated_instr;
 }
 
 // Post-process rotated send/recv ops to add control dependencies with
@@ -349,11 +355,11 @@ static absl::Status PostProcessRotatedSendRecvOps(
          FindAllConflictingCollectives(parent, {rotated_instr})) {
       if (rotated_send_recvs_set.contains(conflicting_collective)) continue;
       num_conflicting_collectives++;
-      HloInstruction* new_control_dependency =
-          GetSendRecvDoneInstructions(rotated_instr);
-      CHECK_NE(new_control_dependency, nullptr);
-      TF_RETURN_IF_ERROR(conflicting_collective->AddControlDependencyTo(
-          new_control_dependency));
+      conflicting_collective =
+          GetSendRecvDoneInstructionOrSelf(conflicting_collective);
+      rotated_instr = GetSendRecvStartInstructionOrSelf(rotated_instr);
+      TF_RETURN_IF_ERROR(
+          conflicting_collective->AddControlDependencyTo(rotated_instr));
       VLOG(5) << "Adding control dependency from "
               << conflicting_collective->ToShortString() << " to "
               << rotated_instr->ToShortString() << "\n";

CodeLinaro-mirror/yocto-mirrors_github_tensorflow_tensorflow

third_party/xla/xla/service/gpu/gpu_p2p_pipeliner.cc
@@ -287,7 +287,8 @@ struct PeeledHloInstructionInfo {
 
 // Finds the start instruction for send/recv where send/recv-done are chosen to
 // be pipelined.
-static HloInstruction* GetSendRecvStartInstruction(HloInstruction* instr) {
+static absl::Nullable<HloInstruction*> GetSendRecvStartInstruction(
+    absl::Nonnull<HloInstruction*> instr) {
   if (instr->opcode() == HloOpcode::kRecv ||
       instr->opcode() == HloOpcode::kSend) {
     return instr;
@@ -299,23 +300,30 @@ static HloInstruction* GetSendRecvStartInstruction(HloInstruction* instr) {
   return nullptr;
 }
 
-static std::vector<HloInstruction*> GetSendRecvStartInstructions(
-    const std::vector<HloInstruction*>& instructions) {
-  std::vector<HloInstruction*> start_instructions;
-  for (HloInstruction* instr : instructions) {
-    HloInstruction* start_instr = GetSendRecvStartInstruction(instr);
+static std::vector<absl::Nonnull<HloInstruction*>> GetSendRecvStartInstructions(
+    const std::vector<absl::Nonnull<HloInstruction*>>& instructions) {
+  std::vector<absl::Nonnull<HloInstruction*>> start_instructions;
+  for (absl::Nonnull<HloInstruction*> instr : instructions) {
+    absl::Nullable<HloInstruction*> start_instr =
+        GetSendRecvStartInstruction(instr);
     if (start_instr != nullptr) start_instructions.push_back(start_instr);
   }
   return start_instructions;
 }
 
-static absl::Nullable<HloInstruction*> GetSendRecvDoneInstructions(
+static absl::Nonnull<HloInstruction*> GetSendRecvStartInstructionOrSelf(
+    absl::Nonnull<HloInstruction*> instr) {
+  HloInstruction* send_recv_start = GetSendRecvStartInstruction(instr);
+  return send_recv_start != nullptr ? send_recv_start : instr;
+}
+
+static absl::Nonnull<HloInstruction*> GetSendRecvDoneInstructionOrSelf(
     absl::Nonnull<HloInstruction*> rotated_instr) {
   auto it = absl::c_find_if(rotated_instr->users(), [](HloInstruction* user) {
     return user->opcode() == HloOpcode::kRecvDone ||
            user->opcode() == HloOpcode::kSendDone;
   });
-  return it != rotated_instr->users().end() ? *it : nullptr;
+  return it != rotated_instr->users().end() ? *it : rotated_instr;
 }
 
 // Post-process rotated send/recv ops to add control dependencies with
@@ -323,7 +331,7 @@ static absl::Nullable<HloInstruction*> GetSendRecvDoneInstructions(
 static absl::Status PostProcessRotatedSendRecvOps(
     const std::vector<HloInstruction*>& rotated) {
   // Find the start instructions for send/recv.
-  std::vector<HloInstruction*> rotated_send_recvs =
+  std::vector<absl::Nonnull<HloInstruction*>> rotated_send_recvs =
       GetSendRecvStartInstructions(rotated);
 
   VLOG(5) << "Post-processing rotated send/recv ops:";
@@ -334,7 +342,7 @@ static absl::Status PostProcessRotatedSendRecvOps(
   }
 
   // Convert to set for faster lookup.
-  absl::flat_hash_set<HloInstruction*> rotated_send_recvs_set(
+  absl::flat_hash_set<absl::Nonnull<HloInstruction*>> rotated_send_recvs_set(
       rotated_send_recvs.begin(), rotated_send_recvs.end());
 
   // Add control dependencies from conflicting collectives to rotated send/recv
@@ -349,11 +357,11 @@ static absl::Status PostProcessRotatedSendRecvOps(
          FindAllConflictingCollectives(parent, {rotated_instr})) {
       if (rotated_send_recvs_set.contains(conflicting_collective)) continue;
       num_conflicting_collectives++;
-      HloInstruction* new_control_dependency =
-          GetSendRecvDoneInstructions(rotated_instr);
-      CHECK_NE(new_control_dependency, nullptr);
-      TF_RETURN_IF_ERROR(conflicting_collective->AddControlDependencyTo(
-          new_control_dependency));
+      conflicting_collective =
+          GetSendRecvDoneInstructionOrSelf(conflicting_collective);
+      rotated_instr = GetSendRecvStartInstructionOrSelf(rotated_instr);
+      TF_RETURN_IF_ERROR(
+          conflicting_collective->AddControlDependencyTo(rotated_instr));
       VLOG(5) << "Adding control dependency from "
               << conflicting_collective->ToShortString() << " to "
               << rotated_instr->ToShortString() << "\n";
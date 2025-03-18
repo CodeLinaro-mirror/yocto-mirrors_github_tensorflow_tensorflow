acb92e0f91de9cc1809aa59478241418b8971ff4
A. Unique TensorFlower <gardener@tensorflow.org>
2025-03-17 20:52:36 -0700

    Add auxiliary methods to the pjrt async client.
    PiperOrigin-RevId: 737850423

--- third_party/xla/xla/pjrt/gpu/tfrt/tfrt_gpu_client.h
+++ third_party/xla/xla/pjrt/gpu/tfrt/tfrt_gpu_client.h
@@ -312,6 +312,14 @@ class TfrtGpuClient final : public PjRtClient {
   absl::StatusOr<std::unique_ptr<PjRtLoadedExecutable>> CompileAndLoad(
       mlir::ModuleOp mlir_module, CompileOptions options) override;
 
+  absl::StatusOr<std::unique_ptr<PjRtBuffer>> CreateUninitializedBuffer(
+      const Shape& shape, PjRtMemorySpace* memory_space) override;
+
+  absl::StatusOr<std::unique_ptr<PjRtLoadedExecutable>>
+  LoadSerializedExecutable(absl::string_view serialized,
+                           std::optional<CompileOptions> options,
+                           const LoadOptions& load_options) override;
+
   absl::StatusOr<std::unique_ptr<PjRtBuffer>> CreateErrorBuffer(
       absl::Status error, const Shape& shape, PjRtMemorySpace* memory) override;
 
@@ -660,6 +668,8 @@ class TfrtGpuExecutable final : public PjRtLoadedExecutable {
 
   bool IsDeleted() override { return executables_.empty(); }
 
+  absl::StatusOr<std::string> SerializeExecutable() const override;
+
   absl::StatusOr<CompileOptions> GetCompileOptions() const override {
     return compile_options_;
   }
@@ -734,6 +744,12 @@ class TfrtGpuExecutable final : public PjRtLoadedExecutable {
   // The unoptimized (unsharded) HloModule. Primarily used for debugging.
   std::optional<InputHloSnapshotBits> input_hlo_snapshot_bits_;
 };
+
+absl::StatusOr<std::unique_ptr<TfrtGpuBuffer>> AllocateTfrtGpuDestinationBuffer(
+    const Shape& on_device_shape,
+    absl::InlinedVector<tsl::AsyncValueRef<GpuEvent>, 4> definition_events,
+    TfrtGpuDevice* device, TfrtGpuClient* client,
+    PjRtMemorySpace* memory_space);
 
 }  // namespace xla
 

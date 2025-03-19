669d949a4daa6a9a53849e80967a6b7b540b1b8c
Mikhail Goncharov <goncharov@google.com>
2025-03-19 08:31:27 -0700

    [XLA:GPU] new fusion kind for nested dot fusions
    To prevent priority fusion from modifying the construct produced by nest_gemm_fusion pass.  For example priority fusion may insert back bitcasts that we have carefully extracted before.
    
    PiperOrigin-RevId: 738409653

--- third_party/xla/xla/service/gpu/hlo_fusion_analysis.cc
+++ third_party/xla/xla/service/gpu/hlo_fusion_analysis.cc
@@ -233,7 +233,7 @@ HloFusionAnalysis::EmitterFusionKind HloFusionAnalysis::GetEmitterFusionKind()
   }
 
   if (fusion_backend_config_.kind() == kTritonFusionKind ||
-      fusion_backend_config_.kind() == kTritonGemmFusionKind) {
+      fusion_backend_config_.kind() == kTritonNestedGemmFusionKind) {
     return EmitterFusionKind::kTriton;
   }
 
--- third_party/xla/xla/service/gpu/ir_emission_utils.h
+++ third_party/xla/xla/service/gpu/ir_emission_utils.h
@@ -81,6 +81,11 @@ inline constexpr absl::string_view kTritonFusionKind = "__triton";
 // Fusions that use Triton have FusionBackendConfig.kind equal to this string.
 inline constexpr absl::string_view kTritonGemmFusionKind = "__triton_gemm";
 
+// Generic fusions that use Triton have FusionBackendConfig.kind equal to this
+// string. Used for fusions that implement a dot expressed as nested fusions.
+inline constexpr absl::string_view kTritonNestedGemmFusionKind =
+    "__triton_nested_gemm_fusion";
+
 inline constexpr absl::string_view kCuDnnFusionKind = "__cudnn$fusion";
 
 // Fusions that can be emitted using a dynamic memcpy. A dynamic memcpy depends

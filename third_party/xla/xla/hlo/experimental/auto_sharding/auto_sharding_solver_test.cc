CodeLinaro-mirror/le_external_github.com_tensorflow_tensorflow

Changes a misleading function name and reduces its usage.
`protobuf_util::ProtobufEquals(p1, p2)` sounds like comparing two protos for equality, but in reality it's comparing the serialized strings of the protos. Proto serialization isn't guaranteed to be stable, and may contain fields we don't care about in case the proto definition is subsequently extended. Therefore its behavior is dangerous and should be used only when we intend to compare serialized strings.

Rename it to `HaveSameSerialization()` to avoid misleading the readers.

Also replaces its uses in tests with `EqualsProto()`, which has a deterministic semantics and generates far better failure messages.

PiperOrigin-RevId: 740418850

third_party/xla/xla/hlo/experimental/auto_sharding/auto_sharding_solver_test.cc
@@ -26,6 +26,7 @@ limitations under the License.
 #include "xla/hlo/experimental/auto_sharding/auto_sharding_strategy.h"
 #include "tsl/platform/platform.h"
 #include "tsl/platform/statusor.h"
+#include "third_party/xls/common/proto_test_utils.h"
 
 namespace xla {
 namespace spmd {
@@ -870,7 +871,7 @@ TEST(ScaleRequest, ScalesProperly) {
   AddCosts(expected_request.mutable_communication_costs(), expected_d);
   AddCosts(expected_request.mutable_resharding_costs(), expected_r);
   expected_request.mutable_coeff_limit()->set_coeff(1e7);
-  EXPECT_THAT(request, ::testing::EqualsProto(expected_request));
+  EXPECT_THAT(request, ::xls::proto_testing::EqualsProto(expected_request));
 }
 
 TEST(ScaleRequest, SkipsScaling) {
@@ -921,7 +922,7 @@ TEST(ScaleRequest, SkipsScaling) {
   AddCosts(expected_request.mutable_communication_costs(), expected_d);
   AddCosts(expected_request.mutable_resharding_costs(), expected_r);
   expected_request.mutable_coeff_limit()->set_coeff(1e7);
-  EXPECT_THAT(request, ::testing::EqualsProto(expected_request));
+  EXPECT_THAT(request, ::xls::proto_testing::EqualsProto(expected_request));
 }
 
 TEST(MinimumMemoryBudgetRequired, HandlesLiveMatrix) {

third_party/xla/xla/hlo/parser/hlo_parser_test.cc
@@ -46,7 +46,6 @@ limitations under the License.
 #include "xla/hlo/testlib/verified_hlo_module.h"
 #include "xla/layout.h"
 #include "xla/layout_util.h"
-#include "xla/protobuf_util.h"
 #include "xla/service/hlo_module_config.h"
 #include "xla/service/pattern_matcher.h"
 #include "xla/shape.h"
@@ -58,6 +57,7 @@ limitations under the License.
 #include "xla/tsl/platform/test.h"
 #include "xla/window_util.h"
 #include "xla/xla_data.pb.h"
+#include "third_party/xls/common/proto_test_utils.h"
 
 namespace xla {
 namespace {
@@ -67,6 +67,7 @@ namespace m = ::xla::match;
 using ::absl::string_view;
 using ::testing::ElementsAre;
 using ::testing::HasSubstr;
+using ::xls::proto_testing::EqualsProto;
 
 struct TestData {
   std::string test_name;
@@ -5831,8 +5832,7 @@ TEST_F(HloParserTest, TranscendentalAccuracyMode) {
   TF_ASSERT_OK_AND_ASSIGN(auto module,
                           ParseAndReturnUnverifiedModule(hlo_string));
   auto* unary = module->entry_computation()->root_instruction();
-  EXPECT_TRUE(protobuf_util::ProtobufEquals(unary->result_accuracy(),
-                                            expected_result_accuracy));
+  EXPECT_THAT(unary->result_accuracy(), EqualsProto(expected_result_accuracy));
 }
 
 TEST_F(HloParserTest, TranscendentalAccuracyModeError) {
@@ -5868,8 +5868,7 @@ TEST_F(HloParserTest, TranscendentalAccuracyRtol) {
   TF_ASSERT_OK_AND_ASSIGN(auto module,
                           ParseAndReturnUnverifiedModule(hlo_string));
   auto* unary = module->entry_computation()->root_instruction();
-  EXPECT_TRUE(protobuf_util::ProtobufEquals(unary->result_accuracy(),
-                                            expected_result_accuracy));
+  EXPECT_THAT(unary->result_accuracy(), EqualsProto(expected_result_accuracy));
 }
 
 TEST_F(HloParserTest, TranscendentalResultAccuracyInvalidName) {
@@ -5913,9 +5912,9 @@ TEST_F(HloParserTest, TranscendentalAccuracyNoConfig) {
                           ParseAndReturnUnverifiedModule(hlo_string));
   ResultAccuracy default_result_accuracy;
   default_result_accuracy.set_mode(ResultAccuracy::DEFAULT);
-  EXPECT_TRUE(protobuf_util::ProtobufEquals(
+  EXPECT_THAT(
       module->entry_computation()->root_instruction()->result_accuracy(),
-      default_result_accuracy));
+      EqualsProto(default_result_accuracy));
 }
 
 TEST_F(HloParserTest, TranscendentalAccuracyInvalidOp) {

third_party/xla/xla/service/gpu/hlo_fusion_analysis_test.cc
@@ -16,7 +16,6 @@ limitations under the License.
 
 #include <gtest/gtest.h>
 #include "xla/hlo/utils/hlo_traversal.h"
-#include "xla/protobuf_util.h"
 #include "xla/service/gpu/backend_configs.pb.h"
 #include "xla/service/gpu/gpu_device_info_for_tests.h"
 #include "xla/service/gpu/ir_emission_utils.h"
@@ -24,9 +23,12 @@ limitations under the License.
 #include "xla/stream_executor/device_description.pb.h"
 #include "xla/tests/hlo_test_base.h"
 #include "tsl/platform/statusor.h"
+#include "third_party/xls/common/proto_test_utils.h"
 
 namespace xla::gpu {
 namespace {
+
+using ::xls::proto_testing::EqualsProto;
 
 class HloFusionAnalysisTest : public HloTestBase {};
 
@@ -407,9 +409,8 @@ TEST_F(HloFusionAnalysisTest,
   auto* root = module->entry_computation()->root_instruction();
   auto analysis = HloFusionAnalysis::Create(*root, device_info);
 
-  EXPECT_TRUE(
-      protobuf_util::ProtobufEquals(analysis.fusion_backend_config(),
-                                    FusionBackendConfig::default_instance()));
+  EXPECT_THAT(analysis.fusion_backend_config(),
+              EqualsProto(FusionBackendConfig::default_instance()));
 }
 
 TEST_F(HloFusionAnalysisTest,
@@ -435,9 +436,8 @@ TEST_F(HloFusionAnalysisTest,
   auto* producer = consumer->operand(0);
   auto analysis = HloFusionAnalysis::Create(*producer, *consumer, device_info);
 
-  EXPECT_TRUE(
-      protobuf_util::ProtobufEquals(analysis.fusion_backend_config(),
-                                    FusionBackendConfig::default_instance()));
+  EXPECT_THAT(analysis.fusion_backend_config(),
+              EqualsProto(FusionBackendConfig::default_instance()));
 }
 
 }  // namespace

third_party/xla/xla/tests/replay_test.cc
@@ -20,7 +20,6 @@ limitations under the License.
 #include "xla/hlo/builder/xla_builder.h"
 #include "xla/hlo/builder/xla_computation.h"
 #include "xla/literal.h"
-#include "xla/protobuf_util.h"
 #include "xla/service/hlo.pb.h"
 #include "xla/shape_util.h"
 #include "xla/tests/client_library_test_base.h"
@@ -28,9 +27,12 @@ limitations under the License.
 #include "xla/tests/test_macros.h"
 #include "xla/xla_data.pb.h"
 #include "tsl/platform/test.h"
+#include "third_party/xls/common/proto_test_utils.h"
 
 namespace xla {
 namespace {
+
+using ::xls::proto_testing::EqualsProto;
 
 class ReplayTest : public ClientLibraryTestBase {};
 
@@ -52,8 +54,8 @@ TEST_F(ReplayTest, TwoPlusTwoReplay) {
       client_->GetComputationShape(computation).value();
   std::unique_ptr<ProgramShape> replayed_shape =
       client_->GetComputationShape(replayed).value();
-  ASSERT_TRUE(protobuf_util::ProtobufEquals(original_shape->ToProto(),
-                                            replayed_shape->ToProto()));
+  ASSERT_THAT(replayed_shape->ToProto(),
+              EqualsProto(original_shape->ToProto()));
 
   // Run it.
   Literal literal =
@@ -84,8 +86,8 @@ XLA_TEST_F(ReplayTest, XPlusYReplayWithParameters) {
       client_->GetComputationShape(computation).value();
   std::unique_ptr<ProgramShape> replayed_shape =
       client_->GetComputationShape(replayed).value();
-  ASSERT_TRUE(protobuf_util::ProtobufEquals(original_shape->ToProto(),
-                                            replayed_shape->ToProto()));
+  ASSERT_THAT(replayed_shape->ToProto(),
+              EqualsProto(original_shape->ToProto()));
 
   // Run it.
   std::unique_ptr<GlobalData> x_data =
@@ -128,8 +130,8 @@ TEST_F(ReplayTest, MapPlusTwoOverR1) {
       client_->GetComputationShape(computation).value();
   std::unique_ptr<ProgramShape> replayed_shape =
       client_->GetComputationShape(replayed).value();
-  ASSERT_TRUE(protobuf_util::ProtobufEquals(original_shape->ToProto(),
-                                            replayed_shape->ToProto()));
+  ASSERT_THAT(replayed_shape->ToProto(),
+              EqualsProto(original_shape->ToProto()));
 
   // Run it.
   Literal literal =

third_party/xla/xla/tsl/distributed_runtime/coordination/coordination_service_test.cc
@@ -45,17 +45,18 @@ limitations under the License.
 #include "xla/tsl/protobuf/coordination_config.pb.h"
 #include "xla/tsl/protobuf/coordination_service.pb.h"
 #include "tsl/platform/random.h"
+#include "third_party/xls/common/proto_test_utils.h"
 
 namespace tsl {
 namespace {
 using ::testing::Each;
 using ::testing::Eq;
-using ::testing::EqualsProto;
 using ::testing::HasSubstr;
 using ::testing::IsEmpty;
 using ::testing::UnorderedElementsAre;
 using ::testing::UnorderedPointwise;
 using ::testing::status::StatusIs;
+using ::xls::proto_testing::EqualsProto;
 
 using tensorflow::CoordinatedJob;
 using tensorflow::CoordinatedTask;

CodeLinaro-mirror/yocto-mirrors_github_tensorflow_tensorflow

third_party/xla/xla/hlo/experimental/auto_sharding/auto_sharding_solver_test.cc
@@ -24,6 +24,7 @@ limitations under the License.
 #include "absl/status/status.h"
 #include "xla/hlo/experimental/auto_sharding/auto_sharding.pb.h"
 #include "xla/hlo/experimental/auto_sharding/auto_sharding_strategy.h"
+#include "xla/tsl/util/proto/proto_matchers.h"
 #include "tsl/platform/platform.h"
 #include "tsl/platform/statusor.h"
 
@@ -870,7 +871,7 @@ TEST(ScaleRequest, ScalesProperly) {
   AddCosts(expected_request.mutable_communication_costs(), expected_d);
   AddCosts(expected_request.mutable_resharding_costs(), expected_r);
   expected_request.mutable_coeff_limit()->set_coeff(1e7);
-  EXPECT_THAT(request, ::testing::EqualsProto(expected_request));
+  EXPECT_THAT(request, ::tsl::proto_testing::EqualsProto(expected_request));
 }
 
 TEST(ScaleRequest, SkipsScaling) {
@@ -921,7 +922,7 @@ TEST(ScaleRequest, SkipsScaling) {
   AddCosts(expected_request.mutable_communication_costs(), expected_d);
   AddCosts(expected_request.mutable_resharding_costs(), expected_r);
   expected_request.mutable_coeff_limit()->set_coeff(1e7);
-  EXPECT_THAT(request, ::testing::EqualsProto(expected_request));
+  EXPECT_THAT(request, ::tsl::proto_testing::EqualsProto(expected_request));
 }
 
 TEST(MinimumMemoryBudgetRequired, HandlesLiveMatrix) {

third_party/xla/xla/hlo/parser/hlo_parser_test.cc
@@ -46,7 +46,6 @@ limitations under the License.
 #include "xla/hlo/testlib/verified_hlo_module.h"
 #include "xla/layout.h"
 #include "xla/layout_util.h"
-#include "xla/protobuf_util.h"
 #include "xla/service/hlo_module_config.h"
 #include "xla/service/pattern_matcher.h"
 #include "xla/shape.h"
@@ -56,6 +55,7 @@ limitations under the License.
 #include "xla/tsl/platform/status_matchers.h"
 #include "xla/tsl/platform/statusor.h"
 #include "xla/tsl/platform/test.h"
+#include "xla/tsl/util/proto/proto_matchers.h"
 #include "xla/window_util.h"
 #include "xla/xla_data.pb.h"
 
@@ -67,6 +67,7 @@ namespace m = ::xla::match;
 using ::absl::string_view;
 using ::testing::ElementsAre;
 using ::testing::HasSubstr;
+using ::tsl::proto_testing::EqualsProto;
 
 struct TestData {
   std::string test_name;
@@ -5831,8 +5832,7 @@ TEST_F(HloParserTest, TranscendentalAccuracyMode) {
   TF_ASSERT_OK_AND_ASSIGN(auto module,
                           ParseAndReturnUnverifiedModule(hlo_string));
   auto* unary = module->entry_computation()->root_instruction();
-  EXPECT_TRUE(protobuf_util::ProtobufEquals(unary->result_accuracy(),
-                                            expected_result_accuracy));
+  EXPECT_THAT(unary->result_accuracy(), EqualsProto(expected_result_accuracy));
 }
 
 TEST_F(HloParserTest, TranscendentalAccuracyModeError) {
@@ -5868,8 +5868,7 @@ TEST_F(HloParserTest, TranscendentalAccuracyRtol) {
   TF_ASSERT_OK_AND_ASSIGN(auto module,
                           ParseAndReturnUnverifiedModule(hlo_string));
   auto* unary = module->entry_computation()->root_instruction();
-  EXPECT_TRUE(protobuf_util::ProtobufEquals(unary->result_accuracy(),
-                                            expected_result_accuracy));
+  EXPECT_THAT(unary->result_accuracy(), EqualsProto(expected_result_accuracy));
 }
 
 TEST_F(HloParserTest, TranscendentalResultAccuracyInvalidName) {
@@ -5913,9 +5912,9 @@ TEST_F(HloParserTest, TranscendentalAccuracyNoConfig) {
                           ParseAndReturnUnverifiedModule(hlo_string));
   ResultAccuracy default_result_accuracy;
   default_result_accuracy.set_mode(ResultAccuracy::DEFAULT);
-  EXPECT_TRUE(protobuf_util::ProtobufEquals(
+  EXPECT_THAT(
       module->entry_computation()->root_instruction()->result_accuracy(),
-      default_result_accuracy));
+      EqualsProto(default_result_accuracy));
 }
 
 TEST_F(HloParserTest, TranscendentalAccuracyInvalidOp) {

third_party/xla/xla/service/gpu/hlo_fusion_analysis_test.cc
@@ -16,17 +16,19 @@ limitations under the License.
 
 #include <gtest/gtest.h>
 #include "xla/hlo/utils/hlo_traversal.h"
-#include "xla/protobuf_util.h"
 #include "xla/service/gpu/backend_configs.pb.h"
 #include "xla/service/gpu/gpu_device_info_for_tests.h"
 #include "xla/service/gpu/ir_emission_utils.h"
 #include "xla/stream_executor/device_description.h"
 #include "xla/stream_executor/device_description.pb.h"
 #include "xla/tests/hlo_test_base.h"
+#include "xla/tsl/util/proto/proto_matchers.h"
 #include "tsl/platform/statusor.h"
 
 namespace xla::gpu {
 namespace {
+
+using ::tsl::proto_testing::EqualsProto;
 
 class HloFusionAnalysisTest : public HloTestBase {};
 
@@ -407,9 +409,8 @@ TEST_F(HloFusionAnalysisTest,
   auto* root = module->entry_computation()->root_instruction();
   auto analysis = HloFusionAnalysis::Create(*root, device_info);
 
-  EXPECT_TRUE(
-      protobuf_util::ProtobufEquals(analysis.fusion_backend_config(),
-                                    FusionBackendConfig::default_instance()));
+  EXPECT_THAT(analysis.fusion_backend_config(),
+              EqualsProto(FusionBackendConfig::default_instance()));
 }
 
 TEST_F(HloFusionAnalysisTest,
@@ -435,9 +436,8 @@ TEST_F(HloFusionAnalysisTest,
   auto* producer = consumer->operand(0);
   auto analysis = HloFusionAnalysis::Create(*producer, *consumer, device_info);
 
-  EXPECT_TRUE(
-      protobuf_util::ProtobufEquals(analysis.fusion_backend_config(),
-                                    FusionBackendConfig::default_instance()));
+  EXPECT_THAT(analysis.fusion_backend_config(),
+              EqualsProto(FusionBackendConfig::default_instance()));
 }
 
 }  // namespace

third_party/xla/xla/tests/replay_test.cc
@@ -20,17 +20,19 @@ limitations under the License.
 #include "xla/hlo/builder/xla_builder.h"
 #include "xla/hlo/builder/xla_computation.h"
 #include "xla/literal.h"
-#include "xla/protobuf_util.h"
 #include "xla/service/hlo.pb.h"
 #include "xla/shape_util.h"
 #include "xla/tests/client_library_test_base.h"
 #include "xla/tests/literal_test_util.h"
 #include "xla/tests/test_macros.h"
+#include "xla/tsl/util/proto/proto_matchers.h"
 #include "xla/xla_data.pb.h"
 #include "tsl/platform/test.h"
 
 namespace xla {
 namespace {
+
+using ::tsl::proto_testing::EqualsProto;
 
 class ReplayTest : public ClientLibraryTestBase {};
 
@@ -52,8 +54,8 @@ TEST_F(ReplayTest, TwoPlusTwoReplay) {
       client_->GetComputationShape(computation).value();
   std::unique_ptr<ProgramShape> replayed_shape =
       client_->GetComputationShape(replayed).value();
-  ASSERT_TRUE(protobuf_util::ProtobufEquals(original_shape->ToProto(),
-                                            replayed_shape->ToProto()));
+  ASSERT_THAT(replayed_shape->ToProto(),
+              EqualsProto(original_shape->ToProto()));
 
   // Run it.
   Literal literal =
@@ -84,8 +86,8 @@ XLA_TEST_F(ReplayTest, XPlusYReplayWithParameters) {
       client_->GetComputationShape(computation).value();
   std::unique_ptr<ProgramShape> replayed_shape =
       client_->GetComputationShape(replayed).value();
-  ASSERT_TRUE(protobuf_util::ProtobufEquals(original_shape->ToProto(),
-                                            replayed_shape->ToProto()));
+  ASSERT_THAT(replayed_shape->ToProto(),
+              EqualsProto(original_shape->ToProto()));
 
   // Run it.
   std::unique_ptr<GlobalData> x_data =
@@ -128,8 +130,8 @@ TEST_F(ReplayTest, MapPlusTwoOverR1) {
       client_->GetComputationShape(computation).value();
   std::unique_ptr<ProgramShape> replayed_shape =
       client_->GetComputationShape(replayed).value();
-  ASSERT_TRUE(protobuf_util::ProtobufEquals(original_shape->ToProto(),
-                                            replayed_shape->ToProto()));
+  ASSERT_THAT(replayed_shape->ToProto(),
+              EqualsProto(original_shape->ToProto()));
 
   // Run it.
   Literal literal =

third_party/xla/xla/tsl/distributed_runtime/coordination/coordination_service_test.cc
@@ -44,18 +44,19 @@ limitations under the License.
 #include "xla/tsl/platform/types.h"
 #include "xla/tsl/protobuf/coordination_config.pb.h"
 #include "xla/tsl/protobuf/coordination_service.pb.h"
+#include "xla/tsl/util/proto/proto_matchers.h"
 #include "tsl/platform/random.h"
 
 namespace tsl {
 namespace {
 using ::testing::Each;
 using ::testing::Eq;
-using ::testing::EqualsProto;
 using ::testing::HasSubstr;
 using ::testing::IsEmpty;
 using ::testing::UnorderedElementsAre;
 using ::testing::UnorderedPointwise;
 using ::testing::status::StatusIs;
+using ::tsl::proto_testing::EqualsProto;
 
 using tensorflow::CoordinatedJob;
 using tensorflow::CoordinatedTask;
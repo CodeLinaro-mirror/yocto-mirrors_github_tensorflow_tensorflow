hexagon-geo-surv/tensorflow

Enable generic dot emitter for different result and input types.
PiperOrigin-RevId: 745497568

third_party/xla/xla/backends/gpu/codegen/triton/support.cc
@@ -389,9 +389,9 @@ CodegenDecision IsTritonSupportedDot(
   }
 
   // TODO(b/393299275): add support tests for mixed types.
-  if (result_type != lhs_type || result_type != rhs_type) {
+  if (lhs_type != rhs_type) {
     return CodegenDecision::Forbid(
-        "Dot operation only supports same types for the result, lhs and rhs.");
+        "Dot operation only supports same types for lhs and rhs.");
   }
 
   absl::Status status = CheckSupportedCheckDotDimensions(dot);
@@ -408,10 +408,12 @@ CodegenDecision IsTritonSupportedDot(
                      PrecisionConfig::Algorithm_Name(algorithm)));
   }
 
-  if (algorithm == PrecisionConfig::ALG_UNSET &&
-      !internal::IsResultTypeSupportedByAlgUnsetDot(result_type, gpu_version)) {
-    return CodegenDecision::Forbid(
-        "Unsupported result type for dot algorithm ALG_UNSET.");
+  if (algorithm == PrecisionConfig::ALG_UNSET) {
+    if (CodegenDecision decision = internal::AreTypesSupportedByAlgoUnsetDot(
+            lhs_type, result_type, gpu_version);
+        !decision) {
+      return decision;
+    }
   }
 
   if (CodegenDecision conversion_decision =
@@ -617,17 +619,46 @@ bool IsTritonUnsupportedOpcode(HloOpcode opcode) {
   }
 }
 
-bool IsResultTypeSupportedByAlgUnsetDot(
-    PrimitiveType result_type, const se::GpuComputeCapability& gpu_version) {
-  std::vector<PrimitiveType> supported_types = {BF16, F16, F32, F64, F8E5M2};
+CodegenDecision AreTypesSupportedByAlgoUnsetDot(
+    PrimitiveType input_type, PrimitiveType result_type,
+    const se::GpuComputeCapability& gpu_version) {
+  if (input_type == F64 && result_type != F64) {
+    return CodegenDecision::Forbid(
+        "Dot operation only supports F64 result type for F64 input type.");
+  }
 
-  if (auto* cuda_cc = std::get_if<se::CudaComputeCapability>(&gpu_version)) {
-    if (cuda_cc->IsAtLeastHopper()) {
-      supported_types.push_back(F8E4M3FN);
+  if (input_type == F8E4M3FN || result_type == F8E4M3FN) {
+    if (auto* cuda_cc = std::get_if<se::CudaComputeCapability>(&gpu_version);
+        cuda_cc && !cuda_cc->IsAtLeastHopper()) {
+      return CodegenDecision::Forbid(
+          "Dot operation for F8E4M3FN is not supported before Hopper.");
     }
   }
 
-  return absl::c_linear_search(supported_types, result_type);
+  auto supported_float_types = {BF16, F16, F32, F64, F8E5M2, F8E4M3FN};
+  if (absl::c_linear_search(supported_float_types, input_type)) {
+    return CodegenDecision::Allow();
+  }
+
+  if (input_type == S8 && result_type == S32) {
+    return CodegenDecision::Allow();
+  }
+
+  auto partially_supported_signed_types = {S8, S16, S32, S64};
+  if (absl::c_linear_search(partially_supported_signed_types, input_type)) {
+    if (absl::c_linear_search(partially_supported_signed_types, result_type)) {
+      return CodegenDecision::Forbid(
+          "Dot operation does not support these signed integer types.");
+    }
+    if (primitive_util::IsFloatingPointType(result_type)) {
+      return CodegenDecision::Forbid(
+          "Dot operation does not support floating point input and signed "
+          "integer result types.");
+    }
+    return CodegenDecision::Allow();
+  }
+
+  return CodegenDecision::Forbid("Unsupported types.");
 }
 
 }  // namespace internal

third_party/xla/xla/backends/gpu/codegen/triton/support.h
@@ -73,8 +73,9 @@ namespace internal {
 bool IsTritonUnsupportedOpcode(HloOpcode opcode);
 
 // This is exposed for testing purposes only. Do not use.
-bool IsResultTypeSupportedByAlgUnsetDot(
-    PrimitiveType result_type, const se::GpuComputeCapability& gpu_version);
+CodegenDecision AreTypesSupportedByAlgoUnsetDot(
+    PrimitiveType input_type, PrimitiveType result_type,
+    const se::GpuComputeCapability& gpu_version);
 
 }  // namespace internal
 

third_party/xla/xla/backends/gpu/codegen/triton/support_test.cc
@@ -1739,18 +1739,35 @@ INSTANTIATE_TEST_SUITE_P(ReverseSuite, ReverseTest,
 
 using DotTest = TritonSupportTest;
 
-class DotTypesTest : public DotTest,
-                     public ::testing::WithParamInterface<
-                         std::tuple<PrimitiveType, se::GpuComputeCapability>> {
+class DotTypesTest
+    : public DotTest,
+      public ::testing::WithParamInterface<
+          std::tuple<PrimitiveType, PrimitiveType, se::GpuComputeCapability>> {
+ public:
+  static std::string ParamToString(
+      const ::testing::TestParamInfo<DotTypesTest::ParamType>& data) {
+    auto [dst_type, src_type, cc] = data.param;
+    return absl::StrCat(primitive_util::LowercasePrimitiveTypeName(dst_type),
+                        "_",
+                        primitive_util::LowercasePrimitiveTypeName(src_type),
+                        "_", ComputeCapabilityToString(cc));
+  };
 };
 
 TEST_P(DotTypesTest, Dot) {
-  // Testing A[] = dot(A[], A[]).
-  // TODO(b/393299275): Add tests for cases where LHS, RHS, and result have
-  // different types. Using infra of parameterized test will not work as the
-  // number of combinations is too large.
-  auto [type, cc] = GetParam();
-  const std::string hlo_text = R"(
+  // Testing B[] = dot(A[], A[]).
+  auto [dst_type, src_type, cc] = GetParam();
+
+  ExpectedFailMode fail_mode = ExpectedFailMode::kFail;
+  if (src_type == F8E4M3FN || dst_type == F8E4M3FN) {
+    if (auto* cuda_cc = std::get_if<se::CudaComputeCapability>(&cc);
+        cuda_cc && !cuda_cc->IsAtLeastHopper()) {
+      // Hits llvm::report_fatal_error during Triton compilation.
+      fail_mode = ExpectedFailMode::kFailOrCrash;
+    }
+  }
+
+  std::string hlo_text = R"(
 flhs {
   ROOT result = $0[128,256] parameter(0)
 }
@@ -1776,20 +1793,18 @@ ENTRY triton_computation {
       }
     }
   }
-  ROOT result = $0[128,512]{1,0} dot(lhs, rhs),
+  ROOT result = $1[128,512]{1,0} dot(lhs, rhs),
     lhs_contracting_dims={1}, rhs_contracting_dims={0}
 }
 )";
+  hlo_text = absl::Substitute(
+      hlo_text, primitive_util::LowercasePrimitiveTypeName(src_type),
+      primitive_util::LowercasePrimitiveTypeName(dst_type));
 
-  ExpectedFailMode fail_mode = ExpectedFailMode::kFail;
-  if (absl::c_linear_search(std::vector{F8E5M2, F8E4M3FN, S8}, type)) {
-    fail_mode = ExpectedFailMode::kFailOrCrash;
-  }
-
-  TF_ASSERT_OK_AND_ASSIGN(
-      TestedInstruction ti,
-      ParseTemplateAndGetInstruction(hlo_text, type, HloOpcode::kDot,
-                                     /* use_nested_gemm_fusions=*/true));
+  TF_ASSERT_OK_AND_ASSIGN(TestedInstruction ti,
+                          ParseTemplateAndGetInstruction(
+                              hlo_text, PRIMITIVE_TYPE_INVALID, HloOpcode::kDot,
+                              /*use_nested_gemm_fusions=*/true));
   RunSupportTest(std::move(ti), /*output_tile_sizes=*/{16, 32}, cc, fail_mode);
 }
 
@@ -1797,8 +1812,9 @@ INSTANTIATE_TEST_SUITE_P(
     DotTestSuite, DotTypesTest,
     ::testing::Combine(
         ::testing::ValuesIn(AllOpSupportedTypes(HloOpcode::kDot)),
+        ::testing::ValuesIn(AllOpSupportedTypes(HloOpcode::kDot)),
         ::testing::ValuesIn(AllDevicesToTest())),
-    TritonSupportTestTypeAndDeviceToString);
+    DotTypesTest::ParamToString);
 
 TEST_F(DotTest, NonFusionRhs) {
   const std::string kHloTestTemplate = R"(

CodeLinaro-mirror/yocto-mirrors_github_tensorflow_tensorflow

third_party/xla/xla/backends/gpu/codegen/triton/support.cc
@@ -389,9 +389,9 @@ CodegenDecision IsTritonSupportedDot(
   }
 
   // TODO(b/393299275): add support tests for mixed types.
-  if (result_type != lhs_type || result_type != rhs_type) {
+  if (lhs_type != rhs_type) {
     return CodegenDecision::Forbid(
-        "Dot operation only supports same types for the result, lhs and rhs.");
+        "Dot operation only supports same types for lhs and rhs.");
   }
 
   absl::Status status = CheckSupportedCheckDotDimensions(dot);
@@ -408,10 +408,12 @@ CodegenDecision IsTritonSupportedDot(
                      PrecisionConfig::Algorithm_Name(algorithm)));
   }
 
-  if (algorithm == PrecisionConfig::ALG_UNSET &&
-      !internal::IsResultTypeSupportedByAlgUnsetDot(result_type, gpu_version)) {
-    return CodegenDecision::Forbid(
-        "Unsupported result type for dot algorithm ALG_UNSET.");
+  if (algorithm == PrecisionConfig::ALG_UNSET) {
+    if (CodegenDecision decision = internal::AreTypesSupportedByAlgUnsetDot(
+            lhs_type, result_type, gpu_version);
+        !decision) {
+      return decision;
+    }
   }
 
   if (CodegenDecision conversion_decision =
@@ -617,17 +619,46 @@ bool IsTritonUnsupportedOpcode(HloOpcode opcode) {
   }
 }
 
-bool IsResultTypeSupportedByAlgUnsetDot(
-    PrimitiveType result_type, const se::GpuComputeCapability& gpu_version) {
-  std::vector<PrimitiveType> supported_types = {BF16, F16, F32, F64, F8E5M2};
+CodegenDecision AreTypesSupportedByAlgUnsetDot(
+    PrimitiveType input_type, PrimitiveType result_type,
+    const se::GpuComputeCapability& gpu_version) {
+  if (input_type == F64 && result_type != F64) {
+    return CodegenDecision::Forbid(
+        "Dot operation only supports F64 result type for F64 input type.");
+  }
 
-  if (auto* cuda_cc = std::get_if<se::CudaComputeCapability>(&gpu_version)) {
-    if (cuda_cc->IsAtLeastHopper()) {
-      supported_types.push_back(F8E4M3FN);
+  if (input_type == F8E4M3FN || result_type == F8E4M3FN) {
+    if (auto* cuda_cc = std::get_if<se::CudaComputeCapability>(&gpu_version);
+        cuda_cc && !cuda_cc->IsAtLeastHopper()) {
+      return CodegenDecision::Forbid(
+          "Dot operation for F8E4M3FN is not supported before Hopper.");
     }
   }
 
-  return absl::c_linear_search(supported_types, result_type);
+  auto supported_float_types = {BF16, F16, F32, F64, F8E5M2, F8E4M3FN};
+  if (absl::c_linear_search(supported_float_types, input_type)) {
+    return CodegenDecision::Allow();
+  }
+
+  if (input_type == S8 && result_type == S32) {
+    return CodegenDecision::Allow();
+  }
+
+  auto partially_supported_signed_types = {S8, S16, S32, S64};
+  if (absl::c_linear_search(partially_supported_signed_types, input_type)) {
+    if (absl::c_linear_search(partially_supported_signed_types, result_type)) {
+      return CodegenDecision::Forbid(
+          "Dot operation does not support these signed integer types.");
+    }
+    if (primitive_util::IsFloatingPointType(result_type)) {
+      return CodegenDecision::Forbid(
+          "Dot operation does not support floating point input and signed "
+          "integer result types.");
+    }
+    return CodegenDecision::Allow();
+  }
+
+  return CodegenDecision::Forbid("Unsupported types.");
 }
 
 }  // namespace internal

third_party/xla/xla/backends/gpu/codegen/triton/support.h
@@ -73,8 +73,9 @@ namespace internal {
 bool IsTritonUnsupportedOpcode(HloOpcode opcode);
 
 // This is exposed for testing purposes only. Do not use.
-bool IsResultTypeSupportedByAlgUnsetDot(
-    PrimitiveType result_type, const se::GpuComputeCapability& gpu_version);
+CodegenDecision AreTypesSupportedByAlgUnsetDot(
+    PrimitiveType input_type, PrimitiveType result_type,
+    const se::GpuComputeCapability& gpu_version);
 
 }  // namespace internal
 

third_party/xla/xla/backends/gpu/codegen/triton/support_test.cc
@@ -1739,18 +1739,35 @@ INSTANTIATE_TEST_SUITE_P(ReverseSuite, ReverseTest,
 
 using DotTest = TritonSupportTest;
 
-class DotTypesTest : public DotTest,
-                     public ::testing::WithParamInterface<
-                         std::tuple<PrimitiveType, se::GpuComputeCapability>> {
+class DotTypesTest
+    : public DotTest,
+      public ::testing::WithParamInterface<
+          std::tuple<PrimitiveType, PrimitiveType, se::GpuComputeCapability>> {
+ public:
+  static std::string ParamToString(
+      const ::testing::TestParamInfo<DotTypesTest::ParamType>& data) {
+    auto [result_type, input_type, cc] = data.param;
+    return absl::StrCat(primitive_util::LowercasePrimitiveTypeName(result_type),
+                        "_",
+                        primitive_util::LowercasePrimitiveTypeName(input_type),
+                        "_", ComputeCapabilityToString(cc));
+  };
 };
 
 TEST_P(DotTypesTest, Dot) {
-  // Testing A[] = dot(A[], A[]).
-  // TODO(b/393299275): Add tests for cases where LHS, RHS, and result have
-  // different types. Using infra of parameterized test will not work as the
-  // number of combinations is too large.
-  auto [type, cc] = GetParam();
-  const std::string hlo_text = R"(
+  // Testing B[] = dot(A[], A[]).
+  auto [result_type, input_type, cc] = GetParam();
+
+  ExpectedFailMode fail_mode = ExpectedFailMode::kFail;
+  if (input_type == F8E4M3FN || result_type == F8E4M3FN) {
+    if (auto* cuda_cc = std::get_if<se::CudaComputeCapability>(&cc);
+        cuda_cc && !cuda_cc->IsAtLeastHopper()) {
+      // Hits llvm::report_fatal_error during Triton compilation.
+      fail_mode = ExpectedFailMode::kFailOrCrash;
+    }
+  }
+
+  std::string hlo_text = R"(
 flhs {
   ROOT result = $0[128,256] parameter(0)
 }
@@ -1776,20 +1793,18 @@ ENTRY triton_computation {
       }
     }
   }
-  ROOT result = $0[128,512]{1,0} dot(lhs, rhs),
+  ROOT result = $1[128,512]{1,0} dot(lhs, rhs),
     lhs_contracting_dims={1}, rhs_contracting_dims={0}
 }
 )";
+  hlo_text = absl::Substitute(
+      hlo_text, primitive_util::LowercasePrimitiveTypeName(input_type),
+      primitive_util::LowercasePrimitiveTypeName(result_type));
 
-  ExpectedFailMode fail_mode = ExpectedFailMode::kFail;
-  if (absl::c_linear_search(std::vector{F8E5M2, F8E4M3FN, S8}, type)) {
-    fail_mode = ExpectedFailMode::kFailOrCrash;
-  }
-
-  TF_ASSERT_OK_AND_ASSIGN(
-      TestedInstruction ti,
-      ParseTemplateAndGetInstruction(hlo_text, type, HloOpcode::kDot,
-                                     /* use_nested_gemm_fusions=*/true));
+  TF_ASSERT_OK_AND_ASSIGN(TestedInstruction ti,
+                          ParseTemplateAndGetInstruction(
+                              hlo_text, PRIMITIVE_TYPE_INVALID, HloOpcode::kDot,
+                              /*use_nested_gemm_fusions=*/true));
   RunSupportTest(std::move(ti), /*output_tile_sizes=*/{16, 32}, cc, fail_mode);
 }
 
@@ -1797,8 +1812,9 @@ INSTANTIATE_TEST_SUITE_P(
     DotTestSuite, DotTypesTest,
     ::testing::Combine(
         ::testing::ValuesIn(AllOpSupportedTypes(HloOpcode::kDot)),
+        ::testing::ValuesIn(AllOpSupportedTypes(HloOpcode::kDot)),
         ::testing::ValuesIn(AllDevicesToTest())),
-    TritonSupportTestTypeAndDeviceToString);
+    DotTypesTest::ParamToString);
 
 TEST_F(DotTest, NonFusionRhs) {
   const std::string kHloTestTemplate = R"(
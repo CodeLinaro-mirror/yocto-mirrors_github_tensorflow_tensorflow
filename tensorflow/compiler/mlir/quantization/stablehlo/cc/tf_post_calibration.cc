hexagon-geo-surv/tensorflow

Branch tf_test_passes to use TFQuantDialect
PiperOrigin-RevId: 751014099

tensorflow/compiler/mlir/quantization/stablehlo/cc/tf_post_calibration.cc
@@ -1,0 +1,67 @@
+/* Copyright 2023 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#include "tensorflow/compiler/mlir/quantization/stablehlo/cc/tf_post_calibration.h"
+
+#include <memory>
+
+#include "absl/base/nullability.h"
+#include "absl/log/die_if_null.h"
+#include "absl/status/statusor.h"
+#include "mlir/IR/BuiltinOps.h"  // from @llvm-project
+#include "mlir/Pass/PassManager.h"  // from @llvm-project
+#include "tensorflow/compiler/mlir/quantization/stablehlo/cc/config.h"
+#include "tensorflow/compiler/mlir/quantization/stablehlo/cc/tf_pass_pipeline.h"
+#include "tensorflow/compiler/mlir/quantization/stablehlo/instrumentations/save_report.h"
+#include "tensorflow/compiler/mlir/quantization/stablehlo/quantization_config.pb.h"
+#include "tensorflow/compiler/mlir/quantization/tensorflow/cc/run_passes.h"
+#include "xla/mlir_hlo/mhlo/transforms/passes.h"
+#include "xla/tsl/platform/errors.h"
+
+namespace mlir::quant::stablehlo {
+
+using ::stablehlo::quantization::GetReportFilePath;
+using ::stablehlo::quantization::PipelineConfig;
+using ::stablehlo::quantization::QuantizationConfig;
+using ::stablehlo::quantization::QuantizationSpecs;
+using ::tensorflow::quantization::RunPasses;
+
+PostCalibrationComponent::PostCalibrationComponent(
+    MLIRContext* absl_nonnull ctx)
+    : ctx_(ABSL_DIE_IF_NULL(ctx)) {}  // Crash OK
+
+absl::StatusOr<ModuleOp> PostCalibrationComponent::Run(
+    ModuleOp module_op, const QuantizationConfig& config) {
+  TF_RETURN_IF_ERROR(RunPasses(
+      kName, /*add_passes_func=*/
+      [&config](PassManager& pm) {
+        // Add instrumentation to save quantization report after quantization.
+        pm.addInstrumentation(
+            std::make_unique<SaveQuantizationReportInstrumentation>(
+                GetReportFilePath(config)));
+
+        tf_quant::stablehlo::AddPostCalibrationPasses(
+            pm, config.pipeline_config(), config.specs());
+      },
+      *ctx_, module_op));
+  return module_op;
+}
+
+void PostCalibrationComponent::AddPasses(
+    OpPassManager& pm, const QuantizationSpecs& specs,
+    const PipelineConfig& pipeline_config) const {
+  tf_quant::stablehlo::AddPostCalibrationPasses(pm, pipeline_config, specs);
+}
+
+}  // namespace mlir::quant::stablehlo

tensorflow/compiler/mlir/quantization/stablehlo/cc/tf_post_calibration.h
@@ -1,0 +1,59 @@
+/* Copyright 2023 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#ifndef TENSORFLOW_COMPILER_MLIR_QUANTIZATION_STABLEHLO_CC_TF_POST_CALIBRATION_H_
+#define TENSORFLOW_COMPILER_MLIR_QUANTIZATION_STABLEHLO_CC_TF_POST_CALIBRATION_H_
+
+#include "absl/base/nullability.h"
+#include "absl/status/statusor.h"
+#include "absl/strings/string_view.h"
+#include "mlir/IR/BuiltinOps.h"  // from @llvm-project
+#include "mlir/IR/MLIRContext.h"  // from @llvm-project
+#include "mlir/Pass/PassManager.h"  // from @llvm-project
+#include "tensorflow/compiler/mlir/quantization/stablehlo/cc/component.h"
+#include "tensorflow/compiler/mlir/quantization/stablehlo/quantization_config.pb.h"
+
+namespace mlir::quant::stablehlo {
+
+// Performs post-calibration graph transformation as part of post-training
+// static-range quantization.
+//
+// The resulting `ModuleOp` contains quantized StableHLO ops serialized in
+// `TF::XlaCallModuleOp`s. They are quantized using the statistics collected
+// after the calibration step, corresponding to each `TF::CustomAggregatorOp`s
+// in the input module op.
+class PostCalibrationComponent : public Component {
+ public:
+  // Name of the post-training quantization post-calibration step. Used for
+  // debugging purposes.
+  static constexpr absl::string_view kName = "quant_ptq_post_calibration";
+
+  explicit PostCalibrationComponent(MLIRContext* absl_nonnull ctx);
+
+  absl::StatusOr<ModuleOp> Run(
+      ModuleOp module_op,
+      const ::stablehlo::quantization::QuantizationConfig& config) override;
+
+  void AddPasses(
+      OpPassManager& pm,
+      const ::stablehlo::quantization::QuantizationSpecs& specs,
+      const ::stablehlo::quantization::PipelineConfig& pipeline_config) const;
+
+ private:
+  MLIRContext* absl_nonnull ctx_;
+};
+
+}  // namespace mlir::quant::stablehlo
+
+#endif  // TENSORFLOW_COMPILER_MLIR_QUANTIZATION_STABLEHLO_CC_TF_POST_CALIBRATION_H_

CodeLinaro-mirror/yocto-mirrors_github_tensorflow_tensorflow

tensorflow/compiler/mlir/quantization/stablehlo/cc/tf_post_calibration.cc
@@ -1,0 +1,67 @@
+/* Copyright 2023 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#include "tensorflow/compiler/mlir/quantization/stablehlo/cc/tf_post_calibration.h"
+
+#include <memory>
+
+#include "absl/base/nullability.h"
+#include "absl/log/die_if_null.h"
+#include "absl/status/statusor.h"
+#include "mlir/IR/BuiltinOps.h"  // from @llvm-project
+#include "mlir/Pass/PassManager.h"  // from @llvm-project
+#include "tensorflow/compiler/mlir/quantization/stablehlo/cc/config.h"
+#include "tensorflow/compiler/mlir/quantization/stablehlo/cc/tf_pass_pipeline.h"
+#include "tensorflow/compiler/mlir/quantization/stablehlo/instrumentations/tf_save_report.h"
+#include "tensorflow/compiler/mlir/quantization/stablehlo/quantization_config.pb.h"
+#include "tensorflow/compiler/mlir/quantization/tensorflow/cc/run_passes.h"
+#include "xla/mlir_hlo/mhlo/transforms/passes.h"
+#include "xla/tsl/platform/errors.h"
+
+namespace mlir::tf_quant::stablehlo {
+
+using ::stablehlo::quantization::GetReportFilePath;
+using ::stablehlo::quantization::PipelineConfig;
+using ::stablehlo::quantization::QuantizationConfig;
+using ::stablehlo::quantization::QuantizationSpecs;
+using ::tensorflow::quantization::RunPasses;
+
+PostCalibrationComponent::PostCalibrationComponent(
+    MLIRContext* absl_nonnull ctx)
+    : ctx_(ABSL_DIE_IF_NULL(ctx)) {}  // Crash OK
+
+absl::StatusOr<ModuleOp> PostCalibrationComponent::Run(
+    ModuleOp module_op, const QuantizationConfig& config) {
+  TF_RETURN_IF_ERROR(RunPasses(
+      kName, /*add_passes_func=*/
+      [&config](PassManager& pm) {
+        // Add instrumentation to save quantization report after quantization.
+        pm.addInstrumentation(
+            std::make_unique<SaveQuantizationReportInstrumentation>(
+                GetReportFilePath(config)));
+
+        tf_quant::stablehlo::AddPostCalibrationPasses(
+            pm, config.pipeline_config(), config.specs());
+      },
+      *ctx_, module_op));
+  return module_op;
+}
+
+void PostCalibrationComponent::AddPasses(
+    OpPassManager& pm, const QuantizationSpecs& specs,
+    const PipelineConfig& pipeline_config) const {
+  tf_quant::stablehlo::AddPostCalibrationPasses(pm, pipeline_config, specs);
+}
+
+}  // namespace mlir::tf_quant::stablehlo

tensorflow/compiler/mlir/quantization/stablehlo/cc/tf_post_calibration.h
@@ -1,0 +1,59 @@
+/* Copyright 2023 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#ifndef TENSORFLOW_COMPILER_MLIR_QUANTIZATION_STABLEHLO_CC_TF_POST_CALIBRATION_H_
+#define TENSORFLOW_COMPILER_MLIR_QUANTIZATION_STABLEHLO_CC_TF_POST_CALIBRATION_H_
+
+#include "absl/base/nullability.h"
+#include "absl/status/statusor.h"
+#include "absl/strings/string_view.h"
+#include "mlir/IR/BuiltinOps.h"  // from @llvm-project
+#include "mlir/IR/MLIRContext.h"  // from @llvm-project
+#include "mlir/Pass/PassManager.h"  // from @llvm-project
+#include "tensorflow/compiler/mlir/quantization/stablehlo/cc/component.h"
+#include "tensorflow/compiler/mlir/quantization/stablehlo/quantization_config.pb.h"
+
+namespace mlir::tf_quant::stablehlo {
+
+// Performs post-calibration graph transformation as part of post-training
+// static-range quantization.
+//
+// The resulting `ModuleOp` contains quantized StableHLO ops serialized in
+// `TF::XlaCallModuleOp`s. They are quantized using the statistics collected
+// after the calibration step, corresponding to each `TF::CustomAggregatorOp`s
+// in the input module op.
+class PostCalibrationComponent : public quant::stablehlo::Component {
+ public:
+  // Name of the post-training quantization post-calibration step. Used for
+  // debugging purposes.
+  static constexpr absl::string_view kName = "quant_ptq_post_calibration";
+
+  explicit PostCalibrationComponent(MLIRContext* absl_nonnull ctx);
+
+  absl::StatusOr<ModuleOp> Run(
+      ModuleOp module_op,
+      const ::stablehlo::quantization::QuantizationConfig& config) override;
+
+  void AddPasses(
+      OpPassManager& pm,
+      const ::stablehlo::quantization::QuantizationSpecs& specs,
+      const ::stablehlo::quantization::PipelineConfig& pipeline_config) const;
+
+ private:
+  MLIRContext* absl_nonnull ctx_;
+};
+
+}  // namespace mlir::tf_quant::stablehlo
+
+#endif  // TENSORFLOW_COMPILER_MLIR_QUANTIZATION_STABLEHLO_CC_TF_POST_CALIBRATION_H_
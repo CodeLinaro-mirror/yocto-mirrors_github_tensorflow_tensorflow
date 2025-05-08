f6abe5fd5232f1274d1df84d781e52d2c325abc6
A. Unique TensorFlower <gardener@tensorflow.org>
2025-05-08 14:24:26 -0700

    XLA:CPU: If configured, use more memory types and change the default memory type.
    The configuration option `legacy_memory_space_behavior`, which currently
    defaults to true, controls whether the old or the new behavior is followed.
    
    PiperOrigin-RevId: 756459075

--- third_party/xla/xla/pjrt/host_memory_spaces.h
+++ third_party/xla/xla/pjrt/host_memory_spaces.h
@@ -91,6 +91,39 @@ class PinnedHostMemorySpace : public PjRtMemorySpace {
   std::string to_string_;
 };
 
+// Define "device" as the default so as to unify the default memory kind name
+// between CPU, GPU, and TPU.  "device" for a CPU client corresponds to the host
+// memory.
+class CpuDeviceMemorySpace : public PjRtMemorySpace {
+ public:
+  static constexpr absl::string_view kKind = "device";
+  static const int kKindId;
+
+  CpuDeviceMemorySpace(int id, PjRtDevice* device);
+
+  PjRtClient* client() const override { return device_->client(); }
+
+  absl::Span<PjRtDevice* const> devices() const override {
+    return absl::Span<PjRtDevice* const>(&device_, device_ != nullptr ? 1 : 0);
+  }
+
+  int id() const override { return id_; }
+
+  absl::string_view kind() const override { return kKind; }
+
+  int kind_id() const override { return kKindId; }
+
+  absl::string_view DebugString() const override { return debug_string_; }
+
+  absl::string_view ToString() const override { return to_string_; }
+
+ private:
+  int id_;
+  PjRtDevice* device_ = nullptr;
+  std::string debug_string_;
+  std::string to_string_;
+};
+
 }  // namespace xla
 
 #endif  // XLA_PJRT_HOST_MEMORY_SPACES_H_

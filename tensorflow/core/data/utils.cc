hexagon-geo-surv/tensorflow

Internal change
PiperOrigin-RevId: 759800545

tensorflow/core/data/utils.cc
@@ -21,6 +21,7 @@ limitations under the License.
 
 #include "absl/container/flat_hash_map.h"
 #include "absl/status/statusor.h"
+#include "tensorflow/core/data/tf_data_file_logger_options.h"
 #include "tensorflow/core/framework/metrics.h"
 #include "tensorflow/core/protobuf/data_service.pb.h"
 
@@ -47,7 +48,7 @@ absl::StatusOr<bool> DisableCompressionAtRuntime(
   return false;
 }
 
-void LogFilenames(const std::vector<std::string>& files) {}
+void LogFilenames(const LogFilenamesOptions& options) {}
 
 }  // namespace data
 }  // namespace tensorflow

tensorflow/core/data/utils.h
@@ -18,9 +18,11 @@ limitations under the License.
 #include <memory>
 #include <optional>
 #include <string>
+#include <vector>
 
 #include "absl/container/flat_hash_map.h"
 #include "absl/status/statusor.h"
+#include "tensorflow/core/data/tf_data_file_logger_options.h"
 #include "tensorflow/core/protobuf/data_service.pb.h"
 
 namespace tensorflow {
@@ -54,7 +56,7 @@ absl::StatusOr<bool> DisableCompressionAtRuntime(
 // every call. Thread safe.
 // TODO (shushanik) Implement streamz error reporting in case the logging is not
 // successful
-void LogFilenames(const std::vector<std::string>& files);
+void LogFilenames(const LogFilenamesOptions& options);
 
 }  // namespace data
 }  // namespace tensorflow

tensorflow/core/kernels/data/fixed_length_record_dataset_op.cc
@@ -15,6 +15,7 @@ limitations under the License.
 #include "tensorflow/core/kernels/data/fixed_length_record_dataset_op.h"
 
 #include "tensorflow/core/data/name_utils.h"
+#include "tensorflow/core/data/tf_data_file_logger_options.h"
 #include "tensorflow/core/data/utils.h"
 #include "tensorflow/core/framework/dataset.h"
 #include "tensorflow/core/framework/metrics.h"
@@ -133,7 +134,9 @@ class FixedLengthRecordDatasetOp::Dataset : public DatasetBase {
         : DatasetIterator<Dataset>(params) {}
 
     absl::Status Initialize(IteratorContext* ctx) override {
-      LogFilenames(dataset()->filenames_);
+      LogFilenamesOptions log_filenames_options(dataset()->filenames_,
+                                                ctx->data_service_address());
+      LogFilenames(log_filenames_options);
       return absl::OkStatus();
     }
 
@@ -264,7 +267,9 @@ class FixedLengthRecordDatasetOp::Dataset : public DatasetBase {
         : DatasetIterator<Dataset>(params) {}
 
     absl::Status Initialize(IteratorContext* ctx) override {
-      LogFilenames(dataset()->filenames_);
+      LogFilenamesOptions log_filenames_options(dataset()->filenames_,
+                                                ctx->data_service_address());
+      LogFilenames(log_filenames_options);
       return absl::OkStatus();
     }
 

tensorflow/core/kernels/data/text_line_dataset_op.cc
@@ -15,6 +15,7 @@ limitations under the License.
 #include "tensorflow/core/kernels/data/text_line_dataset_op.h"
 
 #include "tensorflow/core/data/name_utils.h"
+#include "tensorflow/core/data/tf_data_file_logger_options.h"
 #include "tensorflow/core/data/utils.h"
 #include "tensorflow/core/framework/dataset.h"
 #include "tensorflow/core/framework/metrics.h"
@@ -101,7 +102,9 @@ class TextLineDatasetOp::Dataset : public DatasetBase {
         : DatasetIterator<Dataset>(params) {}
 
     absl::Status Initialize(IteratorContext* ctx) override {
-      LogFilenames(dataset()->filenames_);
+      LogFilenamesOptions log_filenames_options(dataset()->filenames_,
+                                                ctx->data_service_address());
+      LogFilenames(log_filenames_options);
       return absl::OkStatus();
     }
 

tensorflow/core/kernels/data/tf_record_dataset_op.cc
@@ -17,6 +17,7 @@ limitations under the License.
 #include <cstdint>
 
 #include "tensorflow/core/data/name_utils.h"
+#include "tensorflow/core/data/tf_data_file_logger_options.h"
 #include "tensorflow/core/data/utils.h"
 #include "tensorflow/core/framework/dataset.h"
 #include "tensorflow/core/framework/metrics.h"
@@ -134,7 +135,9 @@ class TFRecordDatasetOp::Dataset : public DatasetBase {
         : DatasetIterator<Dataset>(params) {}
 
     absl::Status Initialize(IteratorContext* ctx) override {
-      LogFilenames(dataset()->filenames_);
+      LogFilenamesOptions log_filenames_options(dataset()->filenames_,
+                                                ctx->data_service_address());
+      LogFilenames(log_filenames_options);
       return absl::OkStatus();
     }
 

CodeLinaro-mirror/yocto-mirrors_github_tensorflow_tensorflow

tensorflow/core/data/utils.cc
@@ -22,6 +22,7 @@ limitations under the License.
 #include "absl/container/flat_hash_map.h"
 #include "absl/status/statusor.h"
 #include "tensorflow/core/framework/metrics.h"
+#include "tensorflow/core/framework/tf_data_file_logger_options.h"
 #include "tensorflow/core/protobuf/data_service.pb.h"
 
 namespace tensorflow {
@@ -47,7 +48,7 @@ absl::StatusOr<bool> DisableCompressionAtRuntime(
   return false;
 }
 
-void LogFilenames(const std::vector<std::string>& files) {}
+void LogFilenames(const LogFilenamesOptions& options) {}
 
 }  // namespace data
 }  // namespace tensorflow

tensorflow/core/data/utils.h
@@ -18,9 +18,11 @@ limitations under the License.
 #include <memory>
 #include <optional>
 #include <string>
+#include <vector>
 
 #include "absl/container/flat_hash_map.h"
 #include "absl/status/statusor.h"
+#include "tensorflow/core/framework/tf_data_file_logger_options.h"
 #include "tensorflow/core/protobuf/data_service.pb.h"
 
 namespace tensorflow {
@@ -54,7 +56,7 @@ absl::StatusOr<bool> DisableCompressionAtRuntime(
 // every call. Thread safe.
 // TODO (shushanik) Implement streamz error reporting in case the logging is not
 // successful
-void LogFilenames(const std::vector<std::string>& files);
+void LogFilenames(const LogFilenamesOptions& options);
 
 }  // namespace data
 }  // namespace tensorflow

tensorflow/core/kernels/data/fixed_length_record_dataset_op.cc
@@ -20,6 +20,7 @@ limitations under the License.
 #include "tensorflow/core/framework/metrics.h"
 #include "tensorflow/core/framework/partial_tensor_shape.h"
 #include "tensorflow/core/framework/tensor.h"
+#include "tensorflow/core/framework/tf_data_file_logger_options.h"
 #include "tensorflow/core/lib/io/buffered_inputstream.h"
 #include "tensorflow/core/lib/io/inputbuffer.h"
 #include "tensorflow/core/lib/io/random_inputstream.h"
@@ -133,7 +134,9 @@ class FixedLengthRecordDatasetOp::Dataset : public DatasetBase {
         : DatasetIterator<Dataset>(params) {}
 
     absl::Status Initialize(IteratorContext* ctx) override {
-      LogFilenames(dataset()->filenames_);
+      LogFilenamesOptions log_filenames_options(dataset()->filenames_,
+                                                ctx->data_service_address());
+      LogFilenames(log_filenames_options);
       return absl::OkStatus();
     }
 
@@ -264,7 +267,9 @@ class FixedLengthRecordDatasetOp::Dataset : public DatasetBase {
         : DatasetIterator<Dataset>(params) {}
 
     absl::Status Initialize(IteratorContext* ctx) override {
-      LogFilenames(dataset()->filenames_);
+      LogFilenamesOptions log_filenames_options(dataset()->filenames_,
+                                                ctx->data_service_address());
+      LogFilenames(log_filenames_options);
       return absl::OkStatus();
     }
 

tensorflow/core/kernels/data/text_line_dataset_op.cc
@@ -20,6 +20,7 @@ limitations under the License.
 #include "tensorflow/core/framework/metrics.h"
 #include "tensorflow/core/framework/partial_tensor_shape.h"
 #include "tensorflow/core/framework/tensor.h"
+#include "tensorflow/core/framework/tf_data_file_logger_options.h"
 #include "tensorflow/core/lib/io/buffered_inputstream.h"
 #include "tensorflow/core/lib/io/inputbuffer.h"
 #include "tensorflow/core/lib/io/random_inputstream.h"
@@ -101,7 +102,9 @@ class TextLineDatasetOp::Dataset : public DatasetBase {
         : DatasetIterator<Dataset>(params) {}
 
     absl::Status Initialize(IteratorContext* ctx) override {
-      LogFilenames(dataset()->filenames_);
+      LogFilenamesOptions log_filenames_options(dataset()->filenames_,
+                                                ctx->data_service_address());
+      LogFilenames(log_filenames_options);
       return absl::OkStatus();
     }
 

tensorflow/core/kernels/data/tf_record_dataset_op.cc
@@ -22,6 +22,7 @@ limitations under the License.
 #include "tensorflow/core/framework/metrics.h"
 #include "tensorflow/core/framework/partial_tensor_shape.h"
 #include "tensorflow/core/framework/tensor.h"
+#include "tensorflow/core/framework/tf_data_file_logger_options.h"
 #include "tensorflow/core/lib/io/buffered_inputstream.h"
 #include "tensorflow/core/lib/io/inputbuffer.h"
 #include "tensorflow/core/lib/io/random_inputstream.h"
@@ -134,7 +135,9 @@ class TFRecordDatasetOp::Dataset : public DatasetBase {
         : DatasetIterator<Dataset>(params) {}
 
     absl::Status Initialize(IteratorContext* ctx) override {
-      LogFilenames(dataset()->filenames_);
+      LogFilenamesOptions log_filenames_options(dataset()->filenames_,
+                                                ctx->data_service_address());
+      LogFilenames(log_filenames_options);
       return absl::OkStatus();
     }
 
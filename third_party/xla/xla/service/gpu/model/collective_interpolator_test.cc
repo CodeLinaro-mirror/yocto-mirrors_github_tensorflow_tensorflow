20b9a7cf6936b12fccf8d7d93a7b2004ee4d7137
Greg Olechwierowicz <olechwierowicz@google.com>
2025-05-27 03:35:24 -0700

    [XLA:GPU] Add exact collective interpolation.
    PiperOrigin-RevId: 763714436

--- third_party/xla/xla/service/gpu/model/collective_interpolator_test.cc
+++ third_party/xla/xla/service/gpu/model/collective_interpolator_test.cc
@@ -437,6 +437,18 @@ INSTANTIATE_TEST_SUITE_P(
     CollectiveInterpolationTestInstantiation, CollectiveInterpolationTest,
     ValuesIn<ParametrizedTestCase>({
         {
+            /*test_name=*/"AR_rail_aligned_exact_nodes",
+            /*spec=*/
+            {
+                /*opcode=*/HloOpcode::kAllReduce,
+                /*comm=*/
+                GPUCommunicationType::RAIL_ALIGNED,
+                /*tensor_size=*/1024,
+                /*num_nodes=*/2,
+            },
+            /*expected_duration=*/absl::Seconds(1),
+        },
+        {
             /*test_name=*/"AR_rail_aligned_extrapolate_nodes",
             /*spec=*/
             {
@@ -483,6 +495,18 @@ INSTANTIATE_TEST_SUITE_P(
                 /*num_nodes=*/2,
             },
             /*expected_duration=*/absl::Milliseconds(1250),
+        },
+        {
+            /*test_name=*/"AR_nonrail_aligned_exact_nodes",
+            /*spec=*/
+            {
+                /*opcode=*/HloOpcode::kAllReduce,
+                /*comm=*/
+                GPUCommunicationType::NON_RAIL_ALIGNED,
+                /*tensor_size=*/1024,
+                /*num_nodes=*/2,
+            },
+            /*expected_duration=*/absl::Seconds(2),
         },
         {
             /*test_name=*/"AR_nonrail_aligned_extrapolate_nodes",
@@ -593,6 +617,18 @@ INSTANTIATE_TEST_SUITE_P(
             /*expected_duration=*/absl::Milliseconds(625),
         },
         {
+            /*test_name=*/"RS_rail_aligned_exact_nodes",
+            /*spec=*/
+            {
+                /*opcode=*/HloOpcode::kReduceScatter,
+                /*comm=*/
+                GPUCommunicationType::RAIL_ALIGNED,
+                /*tensor_size=*/1024,
+                /*num_nodes=*/2,
+            },
+            /*expected_duration=*/absl::Seconds(1),
+        },
+        {
             /*test_name=*/"RS_rail_aligned_extrapolate_nodes",
             /*spec=*/
             {
@@ -639,6 +675,18 @@ INSTANTIATE_TEST_SUITE_P(
                 /*num_nodes=*/2,
             },
             /*expected_duration=*/absl::Milliseconds(1250),
+        },
+        {
+            /*test_name=*/"RS_nonrail_aligned_exact_nodes",
+            /*spec=*/
+            {
+                /*opcode=*/HloOpcode::kReduceScatter,
+                /*comm=*/
+                GPUCommunicationType::NON_RAIL_ALIGNED,
+                /*tensor_size=*/1024,
+                /*num_nodes=*/2,
+            },
+            /*expected_duration=*/absl::Seconds(2),
         },
         {
             /*test_name=*/"RS_nonrail_aligned_extrapolate_nodes",
@@ -737,6 +785,18 @@ INSTANTIATE_TEST_SUITE_P(
             /*expected_duration=*/absl::Milliseconds(625),
         },
         {
+            /*test_name=*/"AG_rail_aligned_exact_nodes",
+            /*spec=*/
+            {
+                /*opcode=*/HloOpcode::kAllGather,
+                /*comm=*/
+                GPUCommunicationType::RAIL_ALIGNED,
+                /*tensor_size=*/1024,
+                /*num_nodes=*/2,
+            },
+            /*expected_duration=*/absl::Seconds(1),
+        },
+        {
             /*test_name=*/"AG_rail_aligned_extrapolate_nodes",
             /*spec=*/
             {
@@ -783,6 +843,18 @@ INSTANTIATE_TEST_SUITE_P(
                 /*num_nodes=*/2,
             },
             /*expected_duration=*/absl::Milliseconds(1250),
+        },
+        {
+            /*test_name=*/"AG_nonrail_aligned_exact_nodes",
+            /*spec=*/
+            {
+                /*opcode=*/HloOpcode::kAllGather,
+                /*comm=*/
+                GPUCommunicationType::NON_RAIL_ALIGNED,
+                /*tensor_size=*/1024,
+                /*num_nodes=*/2,
+            },
+            /*expected_duration=*/absl::Seconds(2),
         },
         {
             /*test_name=*/"AG_nonrail_aligned_extrapolate_nodes",

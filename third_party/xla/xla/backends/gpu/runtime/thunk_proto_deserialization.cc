43614f7276523e72bb720d6083122c3116f52976
Aliia Khasanova <aliia@google.com>
2025-06-02 05:45:10 -0700

    [XLA:GPU] Add proto serialization for ReplicaIdThunk.
    + cleaning includes
    
    PiperOrigin-RevId: 766154870

--- third_party/xla/xla/backends/gpu/runtime/thunk_proto_deserialization.cc
+++ third_party/xla/xla/backends/gpu/runtime/thunk_proto_deserialization.cc
@@ -24,6 +24,7 @@ limitations under the License.
 #include "xla/backends/gpu/runtime/conditional_thunk.h"
 #include "xla/backends/gpu/runtime/copy_thunk.h"
 #include "xla/backends/gpu/runtime/gemm_thunk.h"
+#include "xla/backends/gpu/runtime/replica_id_thunk.h"
 #include "xla/backends/gpu/runtime/sequential_thunk.h"
 #include "xla/backends/gpu/runtime/thunk.h"
 #include "xla/backends/gpu/runtime/triangular_solve_thunk.h"
@@ -89,6 +90,11 @@ absl::StatusOr<std::unique_ptr<Thunk>> DeserializeThunkProto(
     return TriangularSolveThunk::FromProto(std::move(thunk_info),
                                            thunk_proto.triangular_solve_thunk(),
                                            buffer_allocations);
+  }
+  if (thunk_proto.has_replica_id_thunk()) {
+    return ReplicaIdThunk::FromProto(std::move(thunk_info),
+                                     thunk_proto.replica_id_thunk(),
+                                     buffer_allocations);
   }
   return absl::InvalidArgumentError("Unknown thunk type found in ThunkProto.");
 }

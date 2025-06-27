CodeLinaro-mirror/le_external_github.com_tensorflow_tensorflow

Porting the grappler batch prioritization rewriter to TFRT.
PiperOrigin-RevId: 776144698

tensorflow/compiler/mlir/tfrt/transforms/passes.h
@@ -83,6 +83,7 @@ struct ReconfigBatchOpPassOptions {
   int64_t batch_timeout_micros = 0;
   llvm::ArrayRef<int64_t> allowed_batch_sizes = {};
   int64_t max_enqueued_batches = 0;
+  int64_t global_prioritization_num_threads = 0;
 };
 std::unique_ptr<mlir::OperationPass<mlir::ModuleOp>> CreateReconfigBatchOpPass(
     ReconfigBatchOpPassOptions options);

tensorflow/compiler/mlir/tfrt/translate/import_model.cc
@@ -345,6 +345,8 @@ std::unique_ptr<tensorflow::TfrtPipelineOptions> GetTfrtPipelineOptions(
   pipeline_options->cost_threshold = options.cost_threshold;
   pipeline_options->min_num_batch_threads = options.min_num_batch_threads;
   pipeline_options->min_max_enqueued_batches = options.min_max_enqueued_batches;
+  pipeline_options->global_prioritization_num_threads =
+      options.global_prioritization_num_threads;
   pipeline_options->batch_padding_policy = options.batch_padding_policy;
   pipeline_options->num_batch_threads =
       options.batch_options.num_batch_threads();

tensorflow/compiler/mlir/tfrt/translate/tfrt_compile_options.h
@@ -150,6 +150,10 @@ struct TfrtCompileOptions {
   // resources.
   int64_t min_max_enqueued_batches = 1;
 
+  // If non-zero, all models on this server are switched to use a prioritized
+  // batching function using this number of global threads.
+  int64_t global_prioritization_num_threads = 0;
+
   // The policy used by a BatchScheduler to pad (or split) batches.
   std::string batch_padding_policy;
 

CodeLinaro-mirror/yocto-mirrors_github_tensorflow_tensorflow

tensorflow/compiler/mlir/tfrt/transforms/passes.h
@@ -83,6 +83,7 @@ struct ReconfigBatchOpPassOptions {
   int64_t batch_timeout_micros = 0;
   llvm::ArrayRef<int64_t> allowed_batch_sizes = {};
   int64_t max_enqueued_batches = 0;
+  int64_t batch_queue_global_prioritization_num_threads = 0;
 };
 std::unique_ptr<mlir::OperationPass<mlir::ModuleOp>> CreateReconfigBatchOpPass(
     ReconfigBatchOpPassOptions options);

tensorflow/compiler/mlir/tfrt/translate/import_model.cc
@@ -345,6 +345,8 @@ std::unique_ptr<tensorflow::TfrtPipelineOptions> GetTfrtPipelineOptions(
   pipeline_options->cost_threshold = options.cost_threshold;
   pipeline_options->min_num_batch_threads = options.min_num_batch_threads;
   pipeline_options->min_max_enqueued_batches = options.min_max_enqueued_batches;
+  pipeline_options->batch_queue_global_prioritization_num_threads =
+      options.batch_queue_global_prioritization_num_threads;
   pipeline_options->batch_padding_policy = options.batch_padding_policy;
   pipeline_options->num_batch_threads =
       options.batch_options.num_batch_threads();

tensorflow/compiler/mlir/tfrt/translate/tfrt_compile_options.h
@@ -150,6 +150,10 @@ struct TfrtCompileOptions {
   // resources.
   int64_t min_max_enqueued_batches = 1;
 
+  // If non-zero, all models on this server are switched to use a prioritized
+  // batching function using this number of global threads.
+  int64_t batch_queue_global_prioritization_num_threads = 0;
+
   // The policy used by a BatchScheduler to pad (or split) batches.
   std::string batch_padding_policy;
 
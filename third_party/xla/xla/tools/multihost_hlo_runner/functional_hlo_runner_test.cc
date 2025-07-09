9814b3287924928c0b36cf8adbc6ed758f3681da
David Dunleavy <ddunleavy@google.com>
2025-07-09 15:44:19 -0700

    Migrate uses of `XLA_TEST_BACKEND` macros to use utilities in `xla_test_backend_predicates.h`
    PiperOrigin-RevId: 781240525

--- third_party/xla/xla/tools/multihost_hlo_runner/functional_hlo_runner_test.cc
+++ third_party/xla/xla/tools/multihost_hlo_runner/functional_hlo_runner_test.cc
@@ -21,6 +21,7 @@ limitations under the License.
 #include <string>
 #include <vector>
 
+#include "xla/tests/xla_test_backend_predicates.h"
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include "absl/log/check.h"
@@ -64,20 +65,13 @@ using ::tsl::testing::IsOkAndHolds;
 using ::tsl::testing::StatusIs;
 using HloModuleAndArguments = ::xla::FunctionalHloRunner::HloModuleAndArguments;
 
-bool IsTestingCpu() {
-#ifdef XLA_TEST_BACKEND_CPU
-  return true;
-#endif
-  return false;
-}
-
 std::string GetHloPath(std::string file_name) {
   return tsl::io::JoinPath(tsl::testing::XlaSrcRoot(), "tools",
                            "multihost_hlo_runner", "data", file_name);
 }
 
 absl::StatusOr<std::unique_ptr<xla::PjRtClient>> GetPjRtClient() {
-  if (IsTestingCpu()) {
+  if (test::DeviceTypeIs(test::kCpu)) {
     return CreateHostClient();
   }
   return CreateGpuClient({});
@@ -140,7 +134,7 @@ TEST_F(FunctionalHloRunnerTest, SingleDeviceHloWithExecutionProfile) {
 }
 
 TEST_F(FunctionalHloRunnerTest, GPUProfilerWithEmptyDumpPathReturnsError) {
-  if (IsTestingCpu()) {
+  if (test::DeviceTypeIs(test::kCpu)) {
     GTEST_SKIP() << "GPU-only test";
   }
   std::string empty_profile_dump_path = "";
@@ -150,7 +144,7 @@ TEST_F(FunctionalHloRunnerTest, GPUProfilerWithEmptyDumpPathReturnsError) {
 }
 
 TEST_F(FunctionalHloRunnerTest, GPUProfilerKeepXSpaceReturnsNonNullXSpace) {
-  if (IsTestingCpu()) {
+  if (test::DeviceTypeIs(test::kCpu)) {
     GTEST_SKIP() << "GPU-only test";
   }
   std::string profile_dump_path =
@@ -380,7 +374,7 @@ void CompileAndFilecheck(
   }
 
   // Check that the LLVM IR has been generated.
-  if (!IsTestingCpu()) {
+  if (!test::DeviceTypeIs(test::kCpu)) {
     std::vector<std::string> ir_paths;
     TF_ASSERT_OK(fs->GetMatchingPaths(fs->JoinPath(dump_dir, "*ir-no-opt.ll"),
                                       &ir_paths));
@@ -405,7 +399,8 @@ TEST_F(FunctionalHloRunnerTest, KeepLayoutsFromHloModule) {
 }
 
 TEST_F(FunctionalHloRunnerTest, AutoLayoutAssignsNonDefaultLayout) {
-  if (IsTestingCpu()) GTEST_SKIP() << "CPU doesn't support auto-layout yet.";
+  if (test::DeviceTypeIs(test::kCpu))
+    GTEST_SKIP() << "CPU doesn't support auto-layout yet.";
   FunctionalHloRunner::PreprocessingOptions preproc_options;
   preproc_options.use_layouts_from_hlo_module = true;
   CompileAndFilecheck(GetHloPath("auto_layout.hlo"),
@@ -417,7 +412,7 @@ TEST_F(FunctionalHloRunnerTest, AutoLayoutAssignsNonDefaultLayout) {
 }
 
 TEST_F(FunctionalHloRunnerTest, FixedLayoutAssignsNonDefaultLayout) {
-  if (IsTestingCpu()) {
+  if (test::DeviceTypeIs(test::kCpu)) {
     GTEST_SKIP() << "CPU doesn't support auto-layout yet.";
   }
   FunctionalHloRunner::PreprocessingOptions preproc_options;
@@ -460,7 +455,7 @@ static const char* binary_name;
 constexpr int kNumNodes = 2;
 
 TEST_F(FunctionalHloRunnerTest, ShardedAutotuningWorks) {
-  if (IsTestingCpu()) {
+  if (test::DeviceTypeIs(test::kCpu)) {
     GTEST_SKIP() << "GPU-only test.";
   }
 
@@ -565,7 +560,7 @@ TEST_F(FunctionalHloRunnerTest, PreservesAutoLayout) {
 }
 
 TEST_F(FunctionalHloRunnerTest, MakeFakeLiteralWithSameValue) {
-  if (IsTestingCpu()) {
+  if (test::DeviceTypeIs(test::kCpu)) {
     GTEST_SKIP() << "GPU-only test";
   }
 
@@ -601,7 +596,7 @@ TEST_F(FunctionalHloRunnerTest, MakeFakeLiteralWithSameValue) {
 }
 
 TEST_F(FunctionalHloRunnerTest, CanRunWithMockCollectives) {
-  if (IsTestingCpu()) {
+  if (test::DeviceTypeIs(test::kCpu)) {
     GTEST_SKIP() << "GPU-only test";
   }
   TF_ASSERT_OK_AND_ASSIGN(std::unique_ptr<xla::PjRtClient> client,
@@ -613,7 +608,7 @@ TEST_F(FunctionalHloRunnerTest, CanRunWithMockCollectives) {
 TEST_F(FunctionalHloRunnerTest, CanCreateMockClientInPjRtEnv) {
   // Tests that the GPU options are propagated correctly to initialize a mock
   // client.
-  if (IsTestingCpu()) {
+  if (test::DeviceTypeIs(test::kCpu)) {
     GTEST_SKIP() << "GPU-only test";
   }
 

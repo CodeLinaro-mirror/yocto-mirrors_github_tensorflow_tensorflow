CodeLinaro-mirror/le_external_github.com_tensorflow_tensorflow

[XLA:CPU] Refactor Intrinsic and use it in all math intrinsics.
Will rename codegen/math to codegen/intrinsic in subsequent CL.

PiperOrigin-RevId: 783049644

third_party/xla/xla/codegen/emitters/transforms/lower_xla_math_lib.cc
@@ -33,10 +33,10 @@ limitations under the License.
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 #include "xla/codegen/emitters/transforms/passes.h"
 #include "xla/codegen/math/erf.h"
+#include "xla/codegen/math/exp.h"
 #include "xla/codegen/math/fptrunc.h"
 #include "xla/codegen/math/intrinsic.h"
 #include "xla/codegen/math/log1p.h"
-#include "xla/mlir/utils/type_util.h"
 
 namespace xla {
 namespace emitters {
@@ -46,7 +46,8 @@ namespace emitters {
 
 namespace {
 
-using Intrinsic = ::xla::codegen::Intrinsic;
+using Type = ::xla::codegen::intrinsics::Type;
+// TODO(talts): Add LowerMathOpPattern based on MathFunction instances.
 
 mlir::func::FuncOp GetOrInsertDeclaration(mlir::PatternRewriter& rewriter,
                                           mlir::ModuleOp& module_op,
@@ -91,8 +92,8 @@ class LowerExpOpPattern : public mlir::OpRewritePattern<mlir::math::ExpOp> {
     mlir::ImplicitLocOpBuilder builder(op.getLoc(), rewriter);
 
     mlir::func::FuncOp xla_exp_func =
-        GetOrInsertDeclaration(rewriter, module_op_, "xla.exp.f64",
-                               rewriter.getFunctionType(op_type, op_type));
+        codegen::intrinsics::Exp::GetOrInsertDeclaration(
+            rewriter, module_op_, Type::TypeFromMlirType(op_type));
 
     // Replace math.exp with call to xla.exp.f64
     auto call_op =
@@ -114,14 +115,11 @@ class LowerLog1pPattern : public mlir::OpRewritePattern<mlir::math::Log1pOp> {
   mlir::LogicalResult matchAndRewrite(
       mlir::math::Log1pOp op, mlir::PatternRewriter& rewriter) const override {
     mlir::Type type = op.getType();
-    PrimitiveType primitive_type = ConvertMlirTypeToPrimitiveType(type);
 
     mlir::ImplicitLocOpBuilder b(op.getLoc(), rewriter);
 
-    auto log1p_decl = GetOrInsertDeclaration(
-        rewriter, module_op_,
-        codegen::math::Log1pFunctionName(1, primitive_type),
-        rewriter.getFunctionType(type, type));
+    auto log1p_decl = codegen::intrinsics::Log1p::GetOrInsertDeclaration(
+        rewriter, module_op_, Type::TypeFromMlirType(type));
     auto call_op = b.create<mlir::func::CallOp>(log1p_decl, op.getOperand());
     rewriter.replaceOp(op, call_op->getResults());
     return mlir::success();
@@ -149,9 +147,8 @@ class LowerErfPattern : public mlir::OpRewritePattern<mlir::math::ErfOp> {
       mlir::Value input_value =
           b.create<mlir::arith::ExtFOp>(f32_type, op.getOperand());
 
-      auto erf_decl = GetOrInsertDeclaration(
-          rewriter, module_op_, codegen::math::ErfFunctionName(1, F32),
-          rewriter.getFunctionType(f32_type, f32_type));
+      auto erf_decl = codegen::intrinsics::Erf::GetOrInsertDeclaration(
+          rewriter, module_op_, Type::TypeFromMlirType(f32_type));
       auto call_op = b.create<mlir::func::CallOp>(erf_decl, input_value);
 
       mlir::Value f32_result = call_op.getResult(0);
@@ -204,8 +201,11 @@ class LowerTruncF32BF16FPattern
 
     mlir::ImplicitLocOpBuilder b(op.getLoc(), rewriter);
 
-    auto f32_to_bf16_decl = Intrinsic::FpTrunc::GetOrInsertDeclaration(
-        rewriter, module_op_, Intrinsic::S(F32), Intrinsic::S(BF16));
+    Type src_type = Type::S(F32);
+    Type dst_type = Type::S(BF16);
+    auto f32_to_bf16_decl =
+        codegen::intrinsics::FpTrunc::GetOrInsertDeclaration(
+            rewriter, module_op_, src_type, dst_type);
     auto call_op =
         b.create<mlir::func::CallOp>(f32_to_bf16_decl, op.getOperand());
     rewriter.replaceOp(op, call_op->getResults());

third_party/xla/xla/codegen/math/intrinsic.cc
@@ -22,14 +22,12 @@ limitations under the License.
 
 #include "absl/status/status.h"
 #include "absl/strings/str_cat.h"
-#include "absl/strings/string_view.h"
 #include "llvm/IR/DerivedTypes.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Type.h"
-#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/IR/Builders.h"
-#include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/MLIRContext.h"
+#include "mlir/Support/LLVM.h"
 #include "xla/mlir/utils/type_util.h"
 #include "xla/primitive_util.h"
 #include "xla/service/llvm_ir/llvm_util.h"
@@ -38,7 +36,37 @@ limitations under the License.
 
 namespace xla::codegen {
 
-Intrinsic::Type::Type(PrimitiveType type, std::optional<size_t> vector_width) {
+namespace {
+std::string LowercaseLLVMPrimitiveTypeName(PrimitiveType type) {
+  std::string name = primitive_util::LowercasePrimitiveTypeName(type);
+  switch (type) {
+    case S1:
+    case S2:
+    case S4:
+    case S8:
+    case S16:
+    case S32:
+    case S64:
+      name[0] = 'i';
+      return name;
+    case U1:
+    case U2:
+    case U4:
+    case U8:
+    case U16:
+    case U32:
+    case U64:
+      name[0] = 'u';
+      return name;
+    default:
+      return name;
+  }
+}
+}  // namespace
+
+namespace intrinsics {
+
+Type::Type(PrimitiveType type, std::optional<size_t> vector_width) {
   if (vector_width) {
     emplace<1>(Vec{type, *vector_width});
   } else {
@@ -46,60 +74,57 @@ Intrinsic::Type::Type(PrimitiveType type, std::optional<size_t> vector_width) {
   }
 }
 
-template <typename R, typename Scalar, typename Vector>
-static R Visit(Scalar scalar, Vector vector, const Intrinsic::Type* type) {
-  if (auto* s = std::get_if<Intrinsic::Scalar>(type)) {
+namespace {
+template <typename R, typename ScalarFn, typename VectorFn>
+static R Visit(ScalarFn scalar, VectorFn vector, const Type* type) {
+  if (auto* s = std::get_if<Scalar>(type)) {
     return scalar(*s);
   }
-  return vector(std::get<Intrinsic::Vec>(*type));
+  return vector(std::get<Vec>(*type));
 }
+}  // namespace
 
-std::string Intrinsic::Type::name() const {
+std::string Type::name() const {
   return Visit<std::string>(
       [](const Scalar& scalar) {
-        return primitive_util::LowercasePrimitiveTypeName(scalar.type);
+        return LowercaseLLVMPrimitiveTypeName(scalar.type);
       },
       [](const Vec& vec) {
-        return absl::StrCat(
-            "v", vec.width,
-            primitive_util::LowercasePrimitiveTypeName(vec.type));
+        return absl::StrCat("v", vec.width,
+                            LowercaseLLVMPrimitiveTypeName(vec.type));
       },
       this);
 }
 
-bool Intrinsic::Type::is_scalar() const {
-  return std::holds_alternative<Scalar>(*this);
-}
+bool Type::is_scalar() const { return std::holds_alternative<Scalar>(*this); }
 
-bool Intrinsic::Type::is_vector() const {
-  return std::holds_alternative<Vec>(*this);
-}
+bool Type::is_vector() const { return std::holds_alternative<Vec>(*this); }
 
-PrimitiveType Intrinsic::Type::element_type() const {
+PrimitiveType Type::element_type() const {
   return Visit<PrimitiveType>([](const Scalar& scalar) { return scalar.type; },
                               [](const Vec& vec) { return vec.type; }, this);
 }
 
-std::optional<size_t> Intrinsic::Type::vector_width() const {
+std::optional<size_t> Type::vector_width() const {
   return Visit<std::optional<size_t>>(
       [](const Scalar& scalar) { return std::nullopt; },
       [](const Vec& vec) { return vec.width; }, this);
 }
 
-template <typename Scalar, typename Vector>
-static absl::Status VerifyTypes(Scalar scalar, Vector vector,
-                                const Intrinsic::Type& a,
-                                const Intrinsic::Type& b) {
+namespace {
+template <typename ScalarFn, typename VectorFn>
+static absl::Status VerifyTypes(ScalarFn scalar, VectorFn vector, const Type& a,
+                                const Type& b) {
   // A pair of scalar types.
-  auto* sa = std::get_if<Intrinsic::Scalar>(&a);
-  auto* sb = std::get_if<Intrinsic::Scalar>(&b);
+  auto* sa = std::get_if<Scalar>(&a);
+  auto* sb = std::get_if<Scalar>(&b);
   if (sa && sb) {
     return scalar(*sa, *sb);
   }
 
   // A pair of vector types.
-  auto* va = std::get_if<Intrinsic::Vec>(&a);
-  auto* vb = std::get_if<Intrinsic::Vec>(&b);
+  auto* va = std::get_if<Vec>(&a);
+  auto* vb = std::get_if<Vec>(&b);
   if (va && vb) {
     return vector(*va, *vb);
   }
@@ -107,8 +132,9 @@ static absl::Status VerifyTypes(Scalar scalar, Vector vector,
   return InvalidArgument("Expected types of the same kind, but got %s and %s",
                          a.name(), b.name());
 }
+}  // namespace
 
-absl::Status Intrinsic::VerifySameWidth(const Type& a, const Type& b) {
+absl::Status Type::VerifySameWidth(const Type& a, const Type& b) {
   return VerifyTypes(
       [&](const Scalar&, const Scalar&) { return absl::OkStatus(); },
       [&](const Vec& va, const Vec& vb) -> absl::Status {
@@ -122,8 +148,7 @@ absl::Status Intrinsic::VerifySameWidth(const Type& a, const Type& b) {
       a, b);
 }
 
-absl::Status Intrinsic::VerifySameWidthAndElementType(const Type& a,
-                                                      const Type& b) {
+absl::Status Type::VerifySameWidthAndElementType(const Type& a, const Type& b) {
   return VerifyTypes(
       [&](const Scalar&, const Scalar&) { return absl::OkStatus(); },
       [&](const Vec& va, const Vec& vb) -> absl::Status {
@@ -138,7 +163,7 @@ absl::Status Intrinsic::VerifySameWidthAndElementType(const Type& a,
       a, b);
 }
 
-llvm::Type* Intrinsic::TypeToIrType(Type type, llvm::LLVMContext& context) {
+llvm::Type* Type::TypeToIrType(Type type, llvm::LLVMContext& context) {
   auto* elt_type = llvm_ir::PrimitiveTypeToIrType(type.element_type(), context);
   if (auto width = type.vector_width()) {
     return llvm::VectorType::get(elt_type, *width, false);
@@ -146,7 +171,7 @@ llvm::Type* Intrinsic::TypeToIrType(Type type, llvm::LLVMContext& context) {
   return elt_type;
 }
 
-mlir::Type Intrinsic::TypeToIrType(Type type, mlir::MLIRContext& context) {
+mlir::Type Type::TypeToIrType(Type type, mlir::MLIRContext& context) {
   auto elt_type = ConvertPrimitiveTypeToMlirType(type.element_type(),
                                                  mlir::Builder(&context));
   if (auto width = type.vector_width()) {
@@ -155,23 +180,13 @@ mlir::Type Intrinsic::TypeToIrType(Type type, mlir::MLIRContext& context) {
   return elt_type.value();
 }
 
-mlir::func::FuncOp Intrinsic::GetOrInsertDeclaration(mlir::OpBuilder& b,
-                                                     mlir::ModuleOp& module,
-                                                     absl::string_view name,
-                                                     mlir::FunctionType type) {
-  // Check if the function already exists, and has the correct type.
-  if (auto func = module.lookupSymbol<mlir::func::FuncOp>(name);
-      func && func.getFunctionType() == type) {
-    return func;
+Type Type::TypeFromMlirType(mlir::Type type) {
+  if (auto vec_type = mlir::dyn_cast<mlir::VectorType>(type)) {
+    return Type(ConvertMlirTypeToPrimitiveType(vec_type.getElementType()),
+                vec_type.getShape().front());
   }
-
-  // If not found or type mismatch, create the declaration.
-  mlir::OpBuilder::InsertionGuard guard(b);
-  b.setInsertionPointToStart(module.getBody());
-
-  auto decl = b.create<mlir::func::FuncOp>(module.getLoc(), name, type);
-  decl.setPrivate();
-  return decl;
+  return Type(ConvertMlirTypeToPrimitiveType(type), std::nullopt);
 }
 
+}  // namespace intrinsics
 }  // namespace xla::codegen

CodeLinaro-mirror/yocto-mirrors_github_tensorflow_tensorflow

third_party/xla/xla/codegen/emitters/transforms/lower_xla_math_lib.cc
@@ -33,10 +33,10 @@ limitations under the License.
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 #include "xla/codegen/emitters/transforms/passes.h"
 #include "xla/codegen/math/erf.h"
+#include "xla/codegen/math/exp.h"
 #include "xla/codegen/math/fptrunc.h"
 #include "xla/codegen/math/intrinsic.h"
 #include "xla/codegen/math/log1p.h"
-#include "xla/mlir/utils/type_util.h"
 
 namespace xla {
 namespace emitters {
@@ -46,7 +46,8 @@ namespace emitters {
 
 namespace {
 
-using Intrinsic = ::xla::codegen::Intrinsic;
+using Type = ::xla::codegen::intrinsics::Type;
+// TODO(talts): Add LowerMathOpPattern based on MathFunction instances.
 
 mlir::func::FuncOp GetOrInsertDeclaration(mlir::PatternRewriter& rewriter,
                                           mlir::ModuleOp& module_op,
@@ -91,8 +92,8 @@ class LowerExpOpPattern : public mlir::OpRewritePattern<mlir::math::ExpOp> {
     mlir::ImplicitLocOpBuilder builder(op.getLoc(), rewriter);
 
     mlir::func::FuncOp xla_exp_func =
-        GetOrInsertDeclaration(rewriter, module_op_, "xla.exp.f64",
-                               rewriter.getFunctionType(op_type, op_type));
+        codegen::intrinsics::Exp::GetOrInsertDeclaration(
+            rewriter, module_op_, Type::TypeFromIrType(op_type));
 
     // Replace math.exp with call to xla.exp.f64
     auto call_op =
@@ -114,14 +115,11 @@ class LowerLog1pPattern : public mlir::OpRewritePattern<mlir::math::Log1pOp> {
   mlir::LogicalResult matchAndRewrite(
       mlir::math::Log1pOp op, mlir::PatternRewriter& rewriter) const override {
     mlir::Type type = op.getType();
-    PrimitiveType primitive_type = ConvertMlirTypeToPrimitiveType(type);
 
     mlir::ImplicitLocOpBuilder b(op.getLoc(), rewriter);
 
-    auto log1p_decl = GetOrInsertDeclaration(
-        rewriter, module_op_,
-        codegen::math::Log1pFunctionName(1, primitive_type),
-        rewriter.getFunctionType(type, type));
+    auto log1p_decl = codegen::intrinsics::Log1p::GetOrInsertDeclaration(
+        rewriter, module_op_, Type::TypeFromIrType(type));
     auto call_op = b.create<mlir::func::CallOp>(log1p_decl, op.getOperand());
     rewriter.replaceOp(op, call_op->getResults());
     return mlir::success();
@@ -149,9 +147,8 @@ class LowerErfPattern : public mlir::OpRewritePattern<mlir::math::ErfOp> {
       mlir::Value input_value =
           b.create<mlir::arith::ExtFOp>(f32_type, op.getOperand());
 
-      auto erf_decl = GetOrInsertDeclaration(
-          rewriter, module_op_, codegen::math::ErfFunctionName(1, F32),
-          rewriter.getFunctionType(f32_type, f32_type));
+      auto erf_decl = codegen::intrinsics::Erf::GetOrInsertDeclaration(
+          rewriter, module_op_, Type::TypeFromIrType(f32_type));
       auto call_op = b.create<mlir::func::CallOp>(erf_decl, input_value);
 
       mlir::Value f32_result = call_op.getResult(0);
@@ -204,8 +201,11 @@ class LowerTruncF32BF16FPattern
 
     mlir::ImplicitLocOpBuilder b(op.getLoc(), rewriter);
 
-    auto f32_to_bf16_decl = Intrinsic::FpTrunc::GetOrInsertDeclaration(
-        rewriter, module_op_, Intrinsic::S(F32), Intrinsic::S(BF16));
+    Type src_type = Type::S(F32);
+    Type dst_type = Type::S(BF16);
+    auto f32_to_bf16_decl =
+        codegen::intrinsics::FpTrunc::GetOrInsertDeclaration(
+            rewriter, module_op_, src_type, dst_type);
     auto call_op =
         b.create<mlir::func::CallOp>(f32_to_bf16_decl, op.getOperand());
     rewriter.replaceOp(op, call_op->getResults());

third_party/xla/xla/codegen/math/intrinsic.cc
@@ -22,14 +22,13 @@ limitations under the License.
 
 #include "absl/status/status.h"
 #include "absl/strings/str_cat.h"
-#include "absl/strings/string_view.h"
 #include "llvm/IR/DerivedTypes.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Type.h"
-#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "llvm/Support/Casting.h"
 #include "mlir/IR/Builders.h"
-#include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/MLIRContext.h"
+#include "mlir/Support/LLVM.h"
 #include "xla/mlir/utils/type_util.h"
 #include "xla/primitive_util.h"
 #include "xla/service/llvm_ir/llvm_util.h"
@@ -38,7 +37,37 @@ limitations under the License.
 
 namespace xla::codegen {
 
-Intrinsic::Type::Type(PrimitiveType type, std::optional<size_t> vector_width) {
+namespace {
+std::string LowercaseLLVMPrimitiveTypeName(PrimitiveType type) {
+  std::string name = primitive_util::LowercasePrimitiveTypeName(type);
+  switch (type) {
+    case S1:
+    case S2:
+    case S4:
+    case S8:
+    case S16:
+    case S32:
+    case S64:
+      name[0] = 'i';
+      return name;
+    case U1:
+    case U2:
+    case U4:
+    case U8:
+    case U16:
+    case U32:
+    case U64:
+      name[0] = 'u';
+      return name;
+    default:
+      return name;
+  }
+}
+}  // namespace
+
+namespace intrinsics {
+
+Type::Type(PrimitiveType type, std::optional<size_t> vector_width) {
   if (vector_width) {
     emplace<1>(Vec{type, *vector_width});
   } else {
@@ -46,60 +75,57 @@ Intrinsic::Type::Type(PrimitiveType type, std::optional<size_t> vector_width) {
   }
 }
 
-template <typename R, typename Scalar, typename Vector>
-static R Visit(Scalar scalar, Vector vector, const Intrinsic::Type* type) {
-  if (auto* s = std::get_if<Intrinsic::Scalar>(type)) {
+namespace {
+template <typename R, typename ScalarFn, typename VectorFn>
+static R Visit(ScalarFn scalar, VectorFn vector, const Type* type) {
+  if (auto* s = std::get_if<Scalar>(type)) {
     return scalar(*s);
   }
-  return vector(std::get<Intrinsic::Vec>(*type));
+  return vector(std::get<Vec>(*type));
 }
+}  // namespace
 
-std::string Intrinsic::Type::name() const {
+std::string Type::name() const {
   return Visit<std::string>(
       [](const Scalar& scalar) {
-        return primitive_util::LowercasePrimitiveTypeName(scalar.type);
+        return LowercaseLLVMPrimitiveTypeName(scalar.type);
       },
       [](const Vec& vec) {
-        return absl::StrCat(
-            "v", vec.width,
-            primitive_util::LowercasePrimitiveTypeName(vec.type));
+        return absl::StrCat("v", vec.width,
+                            LowercaseLLVMPrimitiveTypeName(vec.type));
       },
       this);
 }
 
-bool Intrinsic::Type::is_scalar() const {
-  return std::holds_alternative<Scalar>(*this);
-}
+bool Type::is_scalar() const { return std::holds_alternative<Scalar>(*this); }
 
-bool Intrinsic::Type::is_vector() const {
-  return std::holds_alternative<Vec>(*this);
-}
+bool Type::is_vector() const { return std::holds_alternative<Vec>(*this); }
 
-PrimitiveType Intrinsic::Type::element_type() const {
+PrimitiveType Type::element_type() const {
   return Visit<PrimitiveType>([](const Scalar& scalar) { return scalar.type; },
                               [](const Vec& vec) { return vec.type; }, this);
 }
 
-std::optional<size_t> Intrinsic::Type::vector_width() const {
+std::optional<size_t> Type::vector_width() const {
   return Visit<std::optional<size_t>>(
       [](const Scalar& scalar) { return std::nullopt; },
       [](const Vec& vec) { return vec.width; }, this);
 }
 
-template <typename Scalar, typename Vector>
-static absl::Status VerifyTypes(Scalar scalar, Vector vector,
-                                const Intrinsic::Type& a,
-                                const Intrinsic::Type& b) {
+namespace {
+template <typename ScalarFn, typename VectorFn>
+static absl::Status VerifyTypes(ScalarFn scalar, VectorFn vector, const Type& a,
+                                const Type& b) {
   // A pair of scalar types.
-  auto* sa = std::get_if<Intrinsic::Scalar>(&a);
-  auto* sb = std::get_if<Intrinsic::Scalar>(&b);
+  auto* sa = std::get_if<Scalar>(&a);
+  auto* sb = std::get_if<Scalar>(&b);
   if (sa && sb) {
     return scalar(*sa, *sb);
   }
 
   // A pair of vector types.
-  auto* va = std::get_if<Intrinsic::Vec>(&a);
-  auto* vb = std::get_if<Intrinsic::Vec>(&b);
+  auto* va = std::get_if<Vec>(&a);
+  auto* vb = std::get_if<Vec>(&b);
   if (va && vb) {
     return vector(*va, *vb);
   }
@@ -107,8 +133,9 @@ static absl::Status VerifyTypes(Scalar scalar, Vector vector,
   return InvalidArgument("Expected types of the same kind, but got %s and %s",
                          a.name(), b.name());
 }
+}  // namespace
 
-absl::Status Intrinsic::VerifySameWidth(const Type& a, const Type& b) {
+absl::Status Type::VerifySameWidth(const Type& a, const Type& b) {
   return VerifyTypes(
       [&](const Scalar&, const Scalar&) { return absl::OkStatus(); },
       [&](const Vec& va, const Vec& vb) -> absl::Status {
@@ -122,8 +149,7 @@ absl::Status Intrinsic::VerifySameWidth(const Type& a, const Type& b) {
       a, b);
 }
 
-absl::Status Intrinsic::VerifySameWidthAndElementType(const Type& a,
-                                                      const Type& b) {
+absl::Status Type::VerifySameWidthAndElementType(const Type& a, const Type& b) {
   return VerifyTypes(
       [&](const Scalar&, const Scalar&) { return absl::OkStatus(); },
       [&](const Vec& va, const Vec& vb) -> absl::Status {
@@ -138,7 +164,7 @@ absl::Status Intrinsic::VerifySameWidthAndElementType(const Type& a,
       a, b);
 }
 
-llvm::Type* Intrinsic::TypeToIrType(Type type, llvm::LLVMContext& context) {
+llvm::Type* Type::TypeToIrType(Type type, llvm::LLVMContext& context) {
   auto* elt_type = llvm_ir::PrimitiveTypeToIrType(type.element_type(), context);
   if (auto width = type.vector_width()) {
     return llvm::VectorType::get(elt_type, *width, false);
@@ -146,7 +172,7 @@ llvm::Type* Intrinsic::TypeToIrType(Type type, llvm::LLVMContext& context) {
   return elt_type;
 }
 
-mlir::Type Intrinsic::TypeToIrType(Type type, mlir::MLIRContext& context) {
+mlir::Type Type::TypeToIrType(Type type, mlir::MLIRContext& context) {
   auto elt_type = ConvertPrimitiveTypeToMlirType(type.element_type(),
                                                  mlir::Builder(&context));
   if (auto width = type.vector_width()) {
@@ -155,23 +181,23 @@ mlir::Type Intrinsic::TypeToIrType(Type type, mlir::MLIRContext& context) {
   return elt_type.value();
 }
 
-mlir::func::FuncOp Intrinsic::GetOrInsertDeclaration(mlir::OpBuilder& b,
-                                                     mlir::ModuleOp& module,
-                                                     absl::string_view name,
-                                                     mlir::FunctionType type) {
-  // Check if the function already exists, and has the correct type.
-  if (auto func = module.lookupSymbol<mlir::func::FuncOp>(name);
-      func && func.getFunctionType() == type) {
-    return func;
+Type Type::TypeFromIrType(mlir::Type type) {
+  if (auto vec_type = mlir::dyn_cast<mlir::VectorType>(type)) {
+    return Type(ConvertMlirTypeToPrimitiveType(vec_type.getElementType()),
+                vec_type.getShape().front());
   }
-
-  // If not found or type mismatch, create the declaration.
-  mlir::OpBuilder::InsertionGuard guard(b);
-  b.setInsertionPointToStart(module.getBody());
-
-  auto decl = b.create<mlir::func::FuncOp>(module.getLoc(), name, type);
-  decl.setPrivate();
-  return decl;
+  return Type(ConvertMlirTypeToPrimitiveType(type), std::nullopt);
 }
 
+Type Type::TypeFromIrType(llvm::Type* type) {
+  if (llvm::isa<llvm::VectorType>(type)) {
+    return Type(llvm_ir::PrimitiveTypeFromIrType(type),
+                llvm::cast<llvm::VectorType>(type)
+                    ->getElementCount()
+                    .getKnownMinValue());
+  }
+  return Type(llvm_ir::PrimitiveTypeFromIrType(type), std::nullopt);
+}
+
+}  // namespace intrinsics
 }  // namespace xla::codegen
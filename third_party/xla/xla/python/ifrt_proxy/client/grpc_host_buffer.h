asitab/tensorflow

IFRT proxy: Allow large data transfers via files when proxy-client and proxy-server have access to a shared filesystem.
PiperOrigin-RevId: 807866039

third_party/xla/xla/python/ifrt_proxy/client/grpc_host_buffer.h
@@ -50,6 +50,8 @@ class GrpcClientHostBufferStore : public ClientHostBufferStore {
   Future<> Delete(uint64_t handle) override;
 
  private:
+  Future<> StoreViaFile(uint64_t handle, absl::string_view data);
+
   const std::shared_ptr<grpc::GrpcIfrtService::StubInterface> stub_;
   const IfrtProxyVersion version_;
   const uint64_t session_id_;

third_party/xla/xla/python/ifrt_proxy/server/grpc_service_impl.h
@@ -77,6 +77,11 @@ class GrpcServiceImpl : public grpc::GrpcIfrtService::Service {
       const GrpcHostBufferDeleteRequest* request,
       GrpcHostBufferDeleteResponse* response) override;
 
+  ::grpc::Status HostBufferStoreViaFile(
+      ::grpc::ServerContext* context,
+      const GrpcHostBufferStoreViaFileRequest* request,
+      GrpcHostBufferStoreViaFileResponse* response) override;
+
   // Test-only method that adds a new session in the host buffer store map.
   // Returns false if the session id already exists.
   bool Test_InsertHostBufferStore(

CodeLinaro-mirror/yocto-mirrors_github_tensorflow_tensorflow

third_party/xla/xla/python/ifrt_proxy/client/grpc_host_buffer.h
@@ -50,6 +50,8 @@ class GrpcClientHostBufferStore : public ClientHostBufferStore {
   Future<> Delete(uint64_t handle) override;
 
  private:
+  Future<> StoreToDisk(uint64_t handle, absl::string_view data);
+
   const std::shared_ptr<grpc::GrpcIfrtService::StubInterface> stub_;
   const IfrtProxyVersion version_;
   const uint64_t session_id_;

third_party/xla/xla/python/ifrt_proxy/server/grpc_service_impl.h
@@ -77,6 +77,11 @@ class GrpcServiceImpl : public grpc::GrpcIfrtService::Service {
       const GrpcHostBufferDeleteRequest* request,
       GrpcHostBufferDeleteResponse* response) override;
 
+  ::grpc::Status HostBufferReadFromDisk(
+      ::grpc::ServerContext* context,
+      const GrpcHostBufferReadFromDiskRequest* request,
+      GrpcHostBufferReadFromDiskResponse* response) override;
+
   // Test-only method that adds a new session in the host buffer store map.
   // Returns false if the session id already exists.
   bool Test_InsertHostBufferStore(
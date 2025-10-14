e3a5eafd8162babc5a66663520d8fcfc54900d9f
Aliia Khasanova <aliia@google.com>
2025-10-14 08:01:24 -0700

    Add proto [de]serialization for CholeskyThunk.
    The only non-obvious part of the thunk is `solver_context_creator`, but we can retrieve it during the deserialization from `stream_executor::Platform`, which is available during runtime.
    
    PiperOrigin-RevId: 819222894

--- third_party/xla/xla/backends/gpu/runtime/cholesky_thunk.cc
+++ third_party/xla/xla/backends/gpu/runtime/cholesky_thunk.cc
@@ -30,6 +30,7 @@ limitations under the License.
 #include "xla/stream_executor/blas.h"
 #include "xla/stream_executor/device_memory.h"
 #include "xla/stream_executor/gpu_solver_context.h"
+#include "xla/stream_executor/platform/platform_object_registry.h"
 #include "xla/stream_executor/stream.h"
 #include "xla/tsl/platform/errors.h"
 #include "xla/util.h"
@@ -171,5 +172,52 @@ absl::Status CholeskyThunk::ExecuteOnStream(const ExecuteParams& params) {
   auto local_context = context.value().get();
   return RunCholesky(type_, &cholesky_params, params.stream, local_context);
 }
+
+absl::StatusOr<ThunkProto> CholeskyThunk::ToProto() const {
+  ThunkProto proto;
+  *proto.mutable_thunk_info() = thunk_info().ToProto();
+
+  CholeskyThunkProto* cholesky_thunk_proto = proto.mutable_cholesky_thunk();
+
+  auto options = cholesky_thunk_proto->mutable_options();
+  options->set_lower(uplo_ == se::blas::UpperLower::kLower);
+
+  TF_ASSIGN_OR_RETURN(*cholesky_thunk_proto->mutable_a_buffer(),
+                      a_buffer_.ToProto());
+  TF_ASSIGN_OR_RETURN(*cholesky_thunk_proto->mutable_workspace_buffer(),
+                      workspace_buffer_.ToProto());
+  TF_ASSIGN_OR_RETURN(*cholesky_thunk_proto->mutable_info_buffer(),
+                      info_buffer_.ToProto());
+  cholesky_thunk_proto->set_type(type_);
+  cholesky_thunk_proto->set_batch_size(batch_size_);
+  cholesky_thunk_proto->set_n(n_);
+  return proto;
+}
+
+absl::StatusOr<std::unique_ptr<CholeskyThunk>> CholeskyThunk::FromProto(
+    ThunkInfo thunk_info, const CholeskyThunkProto& proto,
+    absl::Span<const BufferAllocation> allocations,
+    stream_executor::Platform* platform) {
+  TF_ASSIGN_OR_RETURN(
+      BufferAllocation::Slice a_buffer,
+      BufferAllocation::Slice::FromProto(proto.a_buffer(), allocations));
+  TF_ASSIGN_OR_RETURN(BufferAllocation::Slice workspace_buffer,
+                      BufferAllocation::Slice::FromProto(
+                          proto.workspace_buffer(), allocations));
+  TF_ASSIGN_OR_RETURN(
+      BufferAllocation::Slice info_buffer,
+      BufferAllocation::Slice::FromProto(proto.info_buffer(), allocations));
+  TF_ASSIGN_OR_RETURN(
+      std::function<
+          absl::StatusOr<std::unique_ptr<stream_executor::GpuSolverContext>>()>
+          solver_creator,
+      stream_executor::PlatformObjectRegistry::GetGlobalRegistry()
+          .FindObject<stream_executor::GpuSolverContextFactory>(
+              platform->id()));
+  return std::make_unique<CholeskyThunk>(
+      thunk_info, proto.options(), a_buffer, workspace_buffer, info_buffer,
+      proto.type(), proto.batch_size(), proto.n(), solver_creator);
+}
+
 }  // namespace gpu
 }  // namespace xla
---
+++ third_party/xla/xla/backends/gpu/runtime/cholesky_thunk_test.cc
@@ -0,0 +1,80 @@
+/* Copyright 2025 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "xla/backends/gpu/runtime/cholesky_thunk.h"
+
+#include <memory>
+#include <vector>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "absl/log/check.h"
+#include "xla/backends/gpu/runtime/thunk.h"
+#include "xla/backends/gpu/runtime/thunk.pb.h"
+#include "xla/service/buffer_assignment.h"
+#include "xla/stream_executor/gpu_solver_context.h"
+#include "xla/stream_executor/platform/platform_object_registry.h"
+#include "xla/stream_executor/platform_manager.h"
+#include "xla/tests/hlo_test_base.h"
+#include "xla/tsl/platform/statusor.h"
+#include "xla/tsl/util/proto/proto_matchers.h"
+#include "xla/xla_data.pb.h"
+#include "tsl/platform/protobuf.h"
+
+namespace xla::gpu {
+namespace {
+
+using ::tsl::proto_testing::EqualsProto;
+
+class CholeskyThunkTest : public HloTestBase {};
+
+TEST_F(CholeskyThunkTest, ProtoRoundTrip) {
+  Thunk::ThunkInfo thunk_info;
+  thunk_info.profile_annotation = "cholesky";
+  CholeskyOptions options;
+  options.set_lower(true);
+  std::vector<BufferAllocation> buffer_allocations = {
+      BufferAllocation(0, 256, 0), BufferAllocation(1, 128, 0),
+      BufferAllocation(2, 4, 0)};
+  BufferAllocation::Slice a_buffer(&buffer_allocations[0], 0, 256);
+  BufferAllocation::Slice workspace_buffer(&buffer_allocations[1], 0, 128);
+  BufferAllocation::Slice info_buffer(&buffer_allocations[2], 0, 4);
+  PrimitiveType type = F32;
+  int64_t batch_size = 1;
+  int64_t n = 16;
+
+  TF_ASSERT_OK_AND_ASSIGN(
+      auto solver_creator,
+      stream_executor::PlatformObjectRegistry::GetGlobalRegistry()
+          .FindObject<stream_executor::GpuSolverContextFactory>(
+              backend().platform()->id()));
+
+  CholeskyThunk thunk(thunk_info, options, a_buffer, workspace_buffer,
+                      info_buffer, type, batch_size, n, solver_creator);
+
+  TF_ASSERT_OK_AND_ASSIGN(ThunkProto proto, thunk.ToProto());
+
+  TF_ASSERT_OK_AND_ASSIGN(
+      std::unique_ptr<CholeskyThunk> round_trip_thunk,
+      CholeskyThunk::FromProto(thunk.thunk_info(), proto.cholesky_thunk(),
+                               buffer_allocations, backend().platform()));
+
+  TF_ASSERT_OK_AND_ASSIGN(ThunkProto round_trip_proto,
+                          round_trip_thunk->ToProto());
+  EXPECT_THAT(round_trip_proto, EqualsProto(proto));
+}
+
+}  // namespace
+}  // namespace xla::gpu

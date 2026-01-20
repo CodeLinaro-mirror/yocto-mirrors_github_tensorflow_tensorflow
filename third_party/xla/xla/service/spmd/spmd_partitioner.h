000bac14263156128220143d8c1c13a46396d324
Bill Varcho <varcho@google.com>
2026-01-20 11:49:54 -0800

    [ReplicaGroupV3][Refactor][12/n] Remove create_all_to_all_with_iota_device_list from SPMFCollectiveOpsCreator.
    PiperOrigin-RevId: 858688641

--- third_party/xla/xla/service/spmd/spmd_partitioner.h
+++ third_party/xla/xla/service/spmd/spmd_partitioner.h
@@ -206,16 +206,6 @@ struct SPMDCollectiveOpsCreator {
       const CollectiveDeviceListBase& partition_subgroups, int64_t channel_id)>
       create_all_reduce;
 
-  // Function used to create a cross-partition all-reduce HLO using device list
-  // in iota format. This function is optional: if it is a nullptr, use
-  // create_all_reduce.
-  // TODO(b/316622399): Merge this and create_all_reduce into a
-  // function that uses CollectiveDeviceList.
-  std::function<HloInstruction*(
-      SpmdBuilder*, HloInstruction* operand, HloComputation* reduction,
-      const IotaReplicaGroupList& partition_group_list, int64_t channel_id)>
-      create_all_reduce_with_iota_device_list;
-
   // Function used to create a cross-partition collective-permute HLO.
   std::function<HloInstruction*(
       SpmdBuilder*, HloInstruction* operand,
@@ -229,15 +219,6 @@ struct SPMDCollectiveOpsCreator {
       const CollectiveDeviceListBase& partition_subgroups, int64_t channel_id,
       std::optional<int64_t> split_dimension)>
       create_all_to_all;
-
-  // Function used to create a cross-partition all-to-all HLO using device list
-  // in iota format. This function is optional: if it is a nullptr, use
-  // create_all_to_all.
-  std::function<HloInstruction*(
-      SpmdBuilder*, absl::Span<HloInstruction* const> operands,
-      const IotaReplicaGroupList& partition_group_list, int64_t channel_id,
-      std::optional<int64_t> split_dimension)>
-      create_all_to_all_with_iota_device_list;
 
   // Function used to create a cross-partition all-gather HLO. This is optional:
   // if it is nullptr, the partitioner will use all-reduce instead.

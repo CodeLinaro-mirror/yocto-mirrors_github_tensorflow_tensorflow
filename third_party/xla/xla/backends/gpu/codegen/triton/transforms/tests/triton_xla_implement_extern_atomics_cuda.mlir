0805232/tensorflow

PR #40460: Refactor CUDA backend for Triton_XLA atomic operations lowering (Enable ROCm Triton AllReduce 2/4)
Imported from GitHub PR https://github.com/openxla/xla/pull/40460

📝 Summary of Changes
This PR is the second PR in a series of four.

PR 1/4: https://github.com/openxla/xla/pull/40462
PR 2/4: https://github.com/openxla/xla/pull/40460 <= This PR
PR 3/4: https://github.com/openxla/xla/pull/40463
PR 4/4: https://github.com/openxla/xla/pull/40464

This PR refactors the CUDA lowering of `Triton_XLA` atomic operations.
It modifies the lowering of atomic operations `triton_xla.atomic_write`, `triton_xla.atomic_spin_wait` and `triton_xla.get_tid` to rely on `extern_elementwise` triton operations instead of inline-assembly. The extern_elementwise ops are then caught later in the triton compiler pipeline and replaced by PTX inline assembly.

🎯 Justification
This PR is part of a rework of the AllReduce triton support to facilitate the enabling of different target GPUs (starting with ROCm).
Prior to these PRs, the `triton_xla.get_tid`, `triton_xla.atomic_write` and `triton_xla.atomic_spin_wait` operations were lowered using PTX assembly. Therefore, AllReduce triton backend was only available for CUDA target.
This PR replaces the existing CUDA-only lowering strategy to lower these operations via Triton operations (`tt.extern_elementwise`), and then catch this operations, at the end of the triton pipeline.

The current PR is conservative and only delays the insertion of the same PTX inline assemble, however subsequent PRs will replace this by using LLVM target dependent intrinsics.
This unifies the lowering of triton_xla.atomic_write and triton_xla.atomic_spin_wait and triton_xla.get_tid operations for CUDA and ROCm targets.

🚀 Kind of Contribution
Please remove what does not apply: ✨ New Feature ♻️ Cleanup

🧪 Unit Tests:
This PR includes a LIT test checking the lowering of atomic operations.

🧪 Execution Tests:
The existing test xla/tests:all_reduce_e2e_test is relevant

third_party/xla/xla/backends/gpu/codegen/triton/transforms/tests/triton_xla_implement_extern_atomics_cuda.mlir
@@ -1,0 +1,154 @@
+// RUN: xla-opt %s -triton-xla-implement-extern-element-wise | FileCheck %s
+
+// Test CUDA implementation of extern_elementwise atomic functions
+// This pass operates on LLVM dialect and inlines PTX assembly implementations
+
+// Test unmasked operations
+module {
+  // CHECK-LABEL: llvm.func @test_get_thread_id
+  llvm.func @test_get_thread_id() -> i32 {
+    // CHECK-NOT: llvm.call @xla_getthreadid
+    // CHECK: [[TID:%.*]] = llvm.inline_asm {{.*}}mov.u32 $0, %tid.x;{{.*}}, "=r" : () -> i32
+    // CHECK: llvm.return [[TID]]
+    %tid = llvm.call @xla_getthreadid() : () -> i32
+    llvm.return %tid : i32
+  }
+  
+  // CHECK-LABEL: llvm.func @test_atomic_write_unmasked
+  llvm.func @test_atomic_write_unmasked(%ptr: !llvm.ptr<1>, %value: i32) -> i32 {
+    // CHECK-NOT: llvm.call @xla_atomicwrite_release_system
+    // CHECK: [[POISON:%.*]] = llvm.mlir.poison : i32
+    // CHECK: llvm.inline_asm has_side_effects "
+    // CHECK-SAME: st.global.sys.release.u32 [$0], $1;
+    // CHECK-SAME: ", "l,r" %arg0, %arg1 : (!llvm.ptr<1>, i32) -> ()
+    // CHECK: llvm.return [[POISON]]
+    %result = llvm.call @xla_atomicwrite_release_system(%ptr, %value) : (!llvm.ptr<1>, i32) -> i32
+    llvm.return %result : i32
+  }
+  
+  // CHECK-LABEL: llvm.func @test_atomic_spin_wait_unmasked
+  llvm.func @test_atomic_spin_wait_unmasked(%ptr: !llvm.ptr<1>, %expected: i32) -> i32 {
+    // CHECK-NOT: llvm.call @xla_atomicspinwait_acquire_system_lt
+    // CHECK: [[POISON:%.*]] = llvm.mlir.poison : i32
+    // CHECK: llvm.inline_asm has_side_effects "
+    // CHECK-SAME: .reg .pred %p<1>;
+    // CHECK-SAME: .reg .b32 %r<1>;
+    // CHECK-SAME: wait:
+    // CHECK-SAME: ld.global.sys.acquire.u32 %r0, [$0];
+    // CHECK-SAME: setp.lt.u32 %p0, %r0, $1;
+    // CHECK-SAME: @%p0 bra wait;
+    // CHECK-SAME: ", "l,r" %arg0, %arg1 : (!llvm.ptr<1>, i32) -> ()
+    // CHECK: llvm.return [[POISON]]
+    %result = llvm.call @xla_atomicspinwait_acquire_system_lt(%ptr, %expected) : (!llvm.ptr<1>, i32) -> i32
+    llvm.return %result : i32
+  }
+  
+  // CHECK-LABEL: llvm.func @test_atomic_spin_wait_eq
+  llvm.func @test_atomic_spin_wait_eq(%ptr: !llvm.ptr<1>, %expected: i32) -> i32 {
+    // CHECK: [[POISON:%.*]] = llvm.mlir.poison : i32
+    // CHECK: llvm.inline_asm has_side_effects "
+    // CHECK-SAME: setp.eq.u32 %p0, %r0, $1;
+    // CHECK-SAME: ", "l,r" %arg0, %arg1 : (!llvm.ptr<1>, i32) -> ()
+    // CHECK: llvm.return [[POISON]]
+    %result = llvm.call @xla_atomicspinwait_acquire_system_eq(%ptr, %expected) : (!llvm.ptr<1>, i32) -> i32
+    llvm.return %result : i32
+  }
+  
+  // CHECK-LABEL: llvm.func @test_relaxed_ordering
+  llvm.func @test_relaxed_ordering(%ptr: !llvm.ptr<1>, %value: i32) -> i32 {
+    // CHECK: llvm.inline_asm has_side_effects "
+    // CHECK-SAME: st.global.sys.relaxed.u32 [$0], $1;
+    // CHECK-SAME: ", "l,r"
+    %result = llvm.call @xla_atomicwrite_relaxed_system(%ptr, %value) : (!llvm.ptr<1>, i32) -> i32
+    llvm.return %result : i32
+  }
+  
+  // CHECK-LABEL: llvm.func @test_gpu_scope
+  llvm.func @test_gpu_scope(%ptr: !llvm.ptr<1>, %value: i32) -> i32 {
+    // CHECK: llvm.inline_asm has_side_effects "
+    // CHECK-SAME: st.global.gpu.release.u32 [$0], $1;
+    // CHECK-SAME: ", "l,r"
+    %result = llvm.call @xla_atomicwrite_release_gpu(%ptr, %value) : (!llvm.ptr<1>, i32) -> i32
+    llvm.return %result : i32
+  }
+  
+  // CHECK-LABEL: llvm.func @test_cta_scope
+  llvm.func @test_cta_scope(%ptr: !llvm.ptr<1>, %value: i32) -> i32 {
+    // CHECK: llvm.inline_asm has_side_effects "
+    // CHECK-SAME: st.global.cta.release.u32 [$0], $1;
+    // CHECK-SAME: ", "l,r"
+    %result = llvm.call @xla_atomicwrite_release_cta(%ptr, %value) : (!llvm.ptr<1>, i32) -> i32
+    llvm.return %result : i32
+  }
+  
+  // CHECK-LABEL: llvm.func @test_acquire_ordering
+  llvm.func @test_acquire_ordering(%ptr: !llvm.ptr<1>, %expected: i32) -> i32 {
+    // CHECK: llvm.inline_asm has_side_effects "
+    // CHECK-SAME: ld.global.sys.acquire.u32 %r0, [$0];
+    // CHECK-SAME: ", "l,r"
+    %result = llvm.call @xla_atomicspinwait_acquire_system_lt(%ptr, %expected) : (!llvm.ptr<1>, i32) -> i32
+    llvm.return %result : i32
+  }
+  
+  // Extern function declarations (will be removed by the pass)
+  llvm.func @xla_getthreadid() -> i32
+  llvm.func @xla_atomicwrite_release_system(!llvm.ptr<1>, i32) -> i32
+  llvm.func @xla_atomicwrite_relaxed_system(!llvm.ptr<1>, i32) -> i32
+  llvm.func @xla_atomicwrite_release_gpu(!llvm.ptr<1>, i32) -> i32
+  llvm.func @xla_atomicwrite_release_cta(!llvm.ptr<1>, i32) -> i32
+  llvm.func @xla_atomicspinwait_acquire_system_lt(!llvm.ptr<1>, i32) -> i32
+  llvm.func @xla_atomicspinwait_acquire_system_eq(!llvm.ptr<1>, i32) -> i32
+}
+
+// Test masked operations in separate module to avoid function redefinition
+module {
+  // CHECK-LABEL: llvm.func @test_atomic_write_masked
+  llvm.func @test_atomic_write_masked(%ptr: !llvm.ptr<1>, %value: i32, %mask: i32) -> i32 {
+    // CHECK-NOT: llvm.call @xla_atomicwrite_release_system
+    // CHECK: [[POISON:%.*]] = llvm.mlir.poison : i32
+    // CHECK: llvm.inline_asm has_side_effects "
+    // CHECK-SAME: .reg .pred %p<>;
+    // CHECK-SAME: setp.ne.u32 %p<>, $2, 0;
+    // CHECK-SAME: @%p<> st.global.sys.release.u32 [$0], $1;
+    // CHECK-SAME: ", "l,r,r" %arg0, %arg1, %arg2 : (!llvm.ptr<1>, i32, i32) -> ()
+    // CHECK: llvm.return [[POISON]]
+    %result = llvm.call @xla_atomicwrite_release_system(%ptr, %value, %mask) : (!llvm.ptr<1>, i32, i32) -> i32
+    llvm.return %result : i32
+  }
+  
+  // CHECK-LABEL: llvm.func @test_atomic_spin_wait_masked
+  llvm.func @test_atomic_spin_wait_masked(%ptr: !llvm.ptr<1>, %expected: i32, %mask: i32) -> i32 {
+    // CHECK-NOT: llvm.call @xla_atomicspinwait_acquire_system_lt
+    // CHECK: [[POISON:%.*]] = llvm.mlir.poison : i32
+    // CHECK: llvm.inline_asm has_side_effects "
+    // CHECK-SAME: .reg .pred %p<2>;
+    // CHECK-SAME: .reg .b32 %r<1>;
+    // CHECK-SAME: setp.ne.u32 %p0, $2, 0;
+    // CHECK-SAME: @%!p0 bra done;
+    // CHECK-SAME: wait:
+    // CHECK-SAME: ld.global.sys.acquire.u32 %r0, [$0];
+    // CHECK-SAME: setp.lt.u32 %p1, %r0, $1;
+    // CHECK-SAME: @%p1 bra wait;
+    // CHECK-SAME: done:
+    // CHECK-SAME: ", "l,r,r" %arg0, %arg1, %arg2 : (!llvm.ptr<1>, i32, i32) -> ()
+    // CHECK: llvm.return [[POISON]]
+    %result = llvm.call @xla_atomicspinwait_acquire_system_lt(%ptr, %expected, %mask) : (!llvm.ptr<1>, i32, i32) -> i32
+    llvm.return %result : i32
+  }
+  
+  // CHECK-LABEL: llvm.func @test_atomic_spin_wait_masked_eq
+  llvm.func @test_atomic_spin_wait_masked_eq(%ptr: !llvm.ptr<1>, %expected: i32, %mask: i32) -> i32 {
+    // CHECK: [[POISON:%.*]] = llvm.mlir.poison : i32
+    // CHECK: llvm.inline_asm has_side_effects "
+    // CHECK-SAME: setp.eq.u32 %p1, %r0, $1;
+    // CHECK-SAME: ", "l,r,r" %arg0, %arg1, %arg2 : (!llvm.ptr<1>, i32, i32) -> ()
+    // CHECK: llvm.return [[POISON]]
+    %result = llvm.call @xla_atomicspinwait_acquire_system_eq(%ptr, %expected, %mask) : (!llvm.ptr<1>, i32, i32) -> i32
+    llvm.return %result : i32
+  }
+  
+  // Extern function declarations for masked versions
+  llvm.func @xla_atomicwrite_release_system(!llvm.ptr<1>, i32, i32) -> i32
+  llvm.func @xla_atomicspinwait_acquire_system_lt(!llvm.ptr<1>, i32, i32) -> i32
+  llvm.func @xla_atomicspinwait_acquire_system_eq(!llvm.ptr<1>, i32, i32) -> i32
+}

CodeLinaro-mirror/yocto-mirrors_github_tensorflow_tensorflow

third_party/xla/xla/backends/gpu/codegen/triton/transforms/tests/triton_xla_implement_extern_atomics_cuda.mlir
@@ -1,0 +1,154 @@
+// RUN: xla-opt %s -triton-xla-implement-extern-element-wise | FileCheck %s
+
+// Test CUDA implementation of extern_elementwise atomic functions
+// This pass operates on LLVM dialect and inlines PTX assembly implementations
+
+// Test unmasked operations
+module {
+  // CHECK-LABEL: llvm.func @test_get_thread_id
+  llvm.func @test_get_thread_id() -> i32 {
+    // CHECK-NOT: llvm.call @xla_getthreadid
+    // CHECK: [[TID:%.*]] = llvm.inline_asm {{.*}}mov.u32 $0, %tid.x;{{.*}}, "=r" : () -> i32
+    // CHECK: llvm.return [[TID]]
+    %tid = llvm.call @xla_getthreadid() : () -> i32
+    llvm.return %tid : i32
+  }
+  
+  // CHECK-LABEL: llvm.func @test_atomic_write_unmasked
+  llvm.func @test_atomic_write_unmasked(%ptr: !llvm.ptr<1>, %value: i32) -> i32 {
+    // CHECK-NOT: llvm.call @xla_atomicwrite_release_system_nomask
+    // CHECK: [[POISON:%.*]] = llvm.mlir.poison : i32
+    // CHECK: llvm.inline_asm has_side_effects "
+    // CHECK-SAME: st.global.sys.release.u32 [$0], $1;
+    // CHECK-SAME: ", "l,r" %arg0, %arg1 : (!llvm.ptr<1>, i32) -> ()
+    // CHECK: llvm.return [[POISON]]
+    %result = llvm.call @xla_atomicwrite_release_system_nomask(%ptr, %value) : (!llvm.ptr<1>, i32) -> i32
+    llvm.return %result : i32
+  }
+  
+  // CHECK-LABEL: llvm.func @test_atomic_spin_wait_unmasked
+  llvm.func @test_atomic_spin_wait_unmasked(%ptr: !llvm.ptr<1>, %expected: i32) -> i32 {
+    // CHECK-NOT: llvm.call @xla_atomicspinwait_acquire_system_lt_nomask
+    // CHECK: [[POISON:%.*]] = llvm.mlir.poison : i32
+    // CHECK: llvm.inline_asm has_side_effects "
+    // CHECK-SAME: .reg .pred %p<1>;
+    // CHECK-SAME: .reg .b32 %r<1>;
+    // CHECK-SAME: wait:
+    // CHECK-SAME: ld.global.sys.acquire.u32 %r0, [$0];
+    // CHECK-SAME: setp.lt.u32 %p0, %r0, $1;
+    // CHECK-SAME: @%p0 bra wait;
+    // CHECK-SAME: ", "l,r" %arg0, %arg1 : (!llvm.ptr<1>, i32) -> ()
+    // CHECK: llvm.return [[POISON]]
+    %result = llvm.call @xla_atomicspinwait_acquire_system_lt_nomask(%ptr, %expected) : (!llvm.ptr<1>, i32) -> i32
+    llvm.return %result : i32
+  }
+  
+  // CHECK-LABEL: llvm.func @test_atomic_spin_wait_eq
+  llvm.func @test_atomic_spin_wait_eq(%ptr: !llvm.ptr<1>, %expected: i32) -> i32 {
+    // CHECK: [[POISON:%.*]] = llvm.mlir.poison : i32
+    // CHECK: llvm.inline_asm has_side_effects "
+    // CHECK-SAME: setp.eq.u32 %p0, %r0, $1;
+    // CHECK-SAME: ", "l,r" %arg0, %arg1 : (!llvm.ptr<1>, i32) -> ()
+    // CHECK: llvm.return [[POISON]]
+    %result = llvm.call @xla_atomicspinwait_acquire_system_eq_nomask(%ptr, %expected) : (!llvm.ptr<1>, i32) -> i32
+    llvm.return %result : i32
+  }
+  
+  // CHECK-LABEL: llvm.func @test_relaxed_ordering
+  llvm.func @test_relaxed_ordering(%ptr: !llvm.ptr<1>, %value: i32) -> i32 {
+    // CHECK: llvm.inline_asm has_side_effects "
+    // CHECK-SAME: st.global.sys.relaxed.u32 [$0], $1;
+    // CHECK-SAME: ", "l,r"
+    %result = llvm.call @xla_atomicwrite_relaxed_system_nomask(%ptr, %value) : (!llvm.ptr<1>, i32) -> i32
+    llvm.return %result : i32
+  }
+  
+  // CHECK-LABEL: llvm.func @test_gpu_scope
+  llvm.func @test_gpu_scope(%ptr: !llvm.ptr<1>, %value: i32) -> i32 {
+    // CHECK: llvm.inline_asm has_side_effects "
+    // CHECK-SAME: st.global.gpu.release.u32 [$0], $1;
+    // CHECK-SAME: ", "l,r"
+    %result = llvm.call @xla_atomicwrite_release_gpu_nomask(%ptr, %value) : (!llvm.ptr<1>, i32) -> i32
+    llvm.return %result : i32
+  }
+  
+  // CHECK-LABEL: llvm.func @test_cta_scope
+  llvm.func @test_cta_scope(%ptr: !llvm.ptr<1>, %value: i32) -> i32 {
+    // CHECK: llvm.inline_asm has_side_effects "
+    // CHECK-SAME: st.global.cta.release.u32 [$0], $1;
+    // CHECK-SAME: ", "l,r"
+    %result = llvm.call @xla_atomicwrite_release_cta_nomask(%ptr, %value) : (!llvm.ptr<1>, i32) -> i32
+    llvm.return %result : i32
+  }
+  
+  // CHECK-LABEL: llvm.func @test_acquire_ordering
+  llvm.func @test_acquire_ordering(%ptr: !llvm.ptr<1>, %expected: i32) -> i32 {
+    // CHECK: llvm.inline_asm has_side_effects "
+    // CHECK-SAME: ld.global.sys.acquire.u32 %r0, [$0];
+    // CHECK-SAME: ", "l,r"
+    %result = llvm.call @xla_atomicspinwait_acquire_system_lt_nomask(%ptr, %expected) : (!llvm.ptr<1>, i32) -> i32
+    llvm.return %result : i32
+  }
+  
+  // Extern function declarations (will be removed by the pass)
+  llvm.func @xla_getthreadid() -> i32
+  llvm.func @xla_atomicwrite_release_system_nomask(!llvm.ptr<1>, i32) -> i32
+  llvm.func @xla_atomicwrite_relaxed_system_nomask(!llvm.ptr<1>, i32) -> i32
+  llvm.func @xla_atomicwrite_release_gpu_nomask(!llvm.ptr<1>, i32) -> i32
+  llvm.func @xla_atomicwrite_release_cta_nomask(!llvm.ptr<1>, i32) -> i32
+  llvm.func @xla_atomicspinwait_acquire_system_lt_nomask(!llvm.ptr<1>, i32) -> i32
+  llvm.func @xla_atomicspinwait_acquire_system_eq_nomask(!llvm.ptr<1>, i32) -> i32
+}
+
+// Test masked operations in separate module to avoid function redefinition
+module {
+  // CHECK-LABEL: llvm.func @test_atomic_write_masked
+  llvm.func @test_atomic_write_masked(%ptr: !llvm.ptr<1>, %value: i32, %mask: i32) -> i32 {
+    // CHECK-NOT: llvm.call @xla_atomicwrite_release_system_mask
+    // CHECK: [[POISON:%.*]] = llvm.mlir.poison : i32
+    // CHECK: llvm.inline_asm has_side_effects "
+    // CHECK-SAME: .reg .pred %p<>;
+    // CHECK-SAME: setp.ne.u32 %p<>, $2, 0;
+    // CHECK-SAME: @%p<> st.global.sys.release.u32 [$0], $1;
+    // CHECK-SAME: ", "l,r,r" %arg0, %arg1, %arg2 : (!llvm.ptr<1>, i32, i32) -> ()
+    // CHECK: llvm.return [[POISON]]
+    %result = llvm.call @xla_atomicwrite_release_system_mask(%ptr, %value, %mask) : (!llvm.ptr<1>, i32, i32) -> i32
+    llvm.return %result : i32
+  }
+  
+  // CHECK-LABEL: llvm.func @test_atomic_spin_wait_masked
+  llvm.func @test_atomic_spin_wait_masked(%ptr: !llvm.ptr<1>, %expected: i32, %mask: i32) -> i32 {
+    // CHECK-NOT: llvm.call @xla_atomicspinwait_acquire_system_lt_mask
+    // CHECK: [[POISON:%.*]] = llvm.mlir.poison : i32
+    // CHECK: llvm.inline_asm has_side_effects "
+    // CHECK-SAME: .reg .pred %p<2>;
+    // CHECK-SAME: .reg .b32 %r<1>;
+    // CHECK-SAME: setp.ne.u32 %p0, $2, 0;
+    // CHECK-SAME: @%!p0 bra done;
+    // CHECK-SAME: wait:
+    // CHECK-SAME: ld.global.sys.acquire.u32 %r0, [$0];
+    // CHECK-SAME: setp.lt.u32 %p1, %r0, $1;
+    // CHECK-SAME: @%p1 bra wait;
+    // CHECK-SAME: done:
+    // CHECK-SAME: ", "l,r,r" %arg0, %arg1, %arg2 : (!llvm.ptr<1>, i32, i32) -> ()
+    // CHECK: llvm.return [[POISON]]
+    %result = llvm.call @xla_atomicspinwait_acquire_system_lt_mask(%ptr, %expected, %mask) : (!llvm.ptr<1>, i32, i32) -> i32
+    llvm.return %result : i32
+  }
+  
+  // CHECK-LABEL: llvm.func @test_atomic_spin_wait_masked_eq
+  llvm.func @test_atomic_spin_wait_masked_eq(%ptr: !llvm.ptr<1>, %expected: i32, %mask: i32) -> i32 {
+    // CHECK: [[POISON:%.*]] = llvm.mlir.poison : i32
+    // CHECK: llvm.inline_asm has_side_effects "
+    // CHECK-SAME: setp.eq.u32 %p1, %r0, $1;
+    // CHECK-SAME: ", "l,r,r" %arg0, %arg1, %arg2 : (!llvm.ptr<1>, i32, i32) -> ()
+    // CHECK: llvm.return [[POISON]]
+    %result = llvm.call @xla_atomicspinwait_acquire_system_eq_mask(%ptr, %expected, %mask) : (!llvm.ptr<1>, i32, i32) -> i32
+    llvm.return %result : i32
+  }
+  
+  // Extern function declarations for the masked operations.
+  llvm.func @xla_atomicwrite_release_system_mask(!llvm.ptr<1>, i32, i32) -> i32
+  llvm.func @xla_atomicspinwait_acquire_system_lt_mask(!llvm.ptr<1>, i32, i32) -> i32
+  llvm.func @xla_atomicspinwait_acquire_system_eq_mask(!llvm.ptr<1>, i32, i32) -> i32
+}